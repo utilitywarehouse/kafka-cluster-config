32c0ba790f8030d1ed1d14f3d262d3a39f5812a0
Foivos Filippopoulos <foivosfilip@gmail.com>
2023-05-17 16:54:27 +0100

    exp-1-merit: Split kafka-exporter ACL into explicit ones per resource

--- exp-1-merit/kafka-shared/acls.tf
+++ exp-1-merit/kafka-shared/acls.tf
@@ -1,6 +1,37 @@
-resource "kafka_acl" "kafka_exporter" {
+# kafka-exporter ACLs
+# Using resourceType = "Any" triggers kafka to complain:
+# `java.lang.IllegalArgumentException: resourceType must not be ANY`
+# Looks like we'd have to create ACLs per resource type
+resource "kafka_acl" "kafka_exporter_topic" {
   resource_name       = "*"
-  resource_type       = "Any"
+  resource_type       = "Topic"
+  acl_principal       = "User:CN=pubsub/kafka-exporter"
+  acl_host            = "*"
+  acl_operation       = "Read"
+  acl_permission_type = "Allow"
+}
+
+resource "kafka_acl" "kafka_exporter_group" {
+  resource_name       = "*"
+  resource_type       = "Group"
+  acl_principal       = "User:CN=pubsub/kafka-exporter"
+  acl_host            = "*"
+  acl_operation       = "Read"
+  acl_permission_type = "Allow"
+}
+
+resource "kafka_acl" "kafka_exporter_cluster" {
+  resource_name       = "*"
+  resource_type       = "Cluster"
+  acl_principal       = "User:CN=pubsub/kafka-exporter"
+  acl_host            = "*"
+  acl_operation       = "Read"
+  acl_permission_type = "Allow"
+}
+
+resource "kafka_acl" "kafka_exporter_transactional_id" {
+  resource_name       = "*"
+  resource_type       = "TransactionalID"
   acl_principal       = "User:CN=pubsub/kafka-exporter"
   acl_host            = "*"
   acl_operation       = "Read"
