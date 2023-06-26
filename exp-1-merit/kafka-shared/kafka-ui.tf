1fd836549d60f7d8050168da787268ac087255ac
Simon Dickson <simonhdickson@users.noreply.github.com>
2023-06-26 14:44:07 +0200

    Add kafka cluster acl

--- exp-1-merit/kafka-shared/kafka-ui.tf
+++ exp-1-merit/kafka-shared/kafka-ui.tf
@@ -15,3 +15,13 @@ resource "kafka_acl" "kafka_ui_group" {
   acl_operation       = "All"
   acl_permission_type = "Allow"
 }
+
+resource "kafka_acl" "kafka_ui_cluster" {
+  resource_name       = "kafka-cluster"
+  resource_type       = "Cluster"
+  acl_principal       = "User:CN=pubsub/kafka-ui"
+  acl_host            = "*"
+  acl_operation       = "All"
+  acl_permission_type = "Allow"
+  resource_pattern_type_filter = "Literal"
+}
