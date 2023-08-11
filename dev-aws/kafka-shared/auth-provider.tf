f416bc3034aa3dde2efbeb9c7a1b8457a38f5793
Shan Gardezi <shangardezi@gmail.com>
2023-08-11 14:32:07 +0100

    (dev/aws) auth provider topic

---
+++ dev-aws/kafka-shared/auth-provider.tf
@@ -0,0 +1,35 @@
+resource "kafka_topic" "iam_auth_provider_v1" {
+  name               = "iam-auth-provider-v1"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    # retain 100MB on each partition
+    "retention.bytes" = "104857600"
+    # keep data for 2 days
+    "retention.ms" = "172800000"
+    # allow max 1 MB for a message
+    "max.message.bytes" = "1048576"
+    "compression.type" = "zstd"
+    "cleanup.policy"   = "delete"
+  }
+}
+
+resource "kafka_acl" "iam_auth_provider_v1_access" {
+  resource_name       = "iam-auth-provider-v1"
+  resource_type       = "Topic"
+  acl_principal       = "User:CN=clubhouse/auth-provider"
+  acl_host            = "*"
+  acl_operation       = "Write"
+  acl_permission_type = "Allow"
+}
+
+resource "kafka_quota" "iam_cerbos_audit_v1_quota" {
+  entity_name               = "User:CN=clubhouse/auth-provider"
+  entity_type               = "user"
+  config = {
+    # limit producing to 5 MB/s
+    "producer_byte_rate" = "5242880"
+    # Allow 100% of CPU. More on this here: https://docs.confluent.io/kafka/design/quotas.html#request-rate-quotas
+    "request_percentage" = "100"
+  }
+}
