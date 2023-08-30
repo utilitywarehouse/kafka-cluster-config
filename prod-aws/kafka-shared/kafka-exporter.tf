a5e2802d42d797dc27f39fba1fd1bd3fc976038f
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2023-08-30 09:56:21 +0300

    kafka-shared prod-aws: added config from merit

---
+++ prod-aws/kafka-shared/kafka-exporter.tf
@@ -0,0 +1,23 @@
+# kafka-exporter ACLs
+# Using resourceType = "Any" triggers kafka to complain:
+# `java.lang.IllegalArgumentException: resourceType must not be ANY`
+# Looks like we'd have to create ACLs per resource type
+# We should need the following according to:
+# https://github.com/danielqsj/kafka_exporter/issues/40#issuecomment-832122188
+resource "kafka_acl" "kafka_exporter_topic" {
+  resource_name       = "*"
+  resource_type       = "Topic"
+  acl_principal       = "User:CN=pubsub/kafka-exporter"
+  acl_host            = "*"
+  acl_operation       = "Describe"
+  acl_permission_type = "Allow"
+}
+
+resource "kafka_acl" "kafka_exporter_group" {
+  resource_name       = "*"
+  resource_type       = "Group"
+  acl_principal       = "User:CN=pubsub/kafka-exporter"
+  acl_host            = "*"
+  acl_operation       = "Describe"
+  acl_permission_type = "Allow"
+}
