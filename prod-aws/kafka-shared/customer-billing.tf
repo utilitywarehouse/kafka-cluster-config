5f1ac7ad4554be9ced768cca4eadaad7b745a76f
Eike Spang <espang@uw.co.uk>
2023-10-17 12:13:35 +0100

    add bex.internal.bill_fulfilled topic and producer and consumer in prod

---
+++ prod-aws/kafka-shared/customer-billing.tf
@@ -0,0 +1,30 @@
+resource "kafka_topic" "invoice_fulfillment" {
+  name               = "bex.internal.bill_fulfilled"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    # keep data for 7 days
+    "retention.ms" = "604800000"
+    # allow max 1 MB for a message
+    "max.message.bytes" = "1048576"
+    "compression.type" = "zstd"
+    "cleanup.policy"   = "delete"
+  }
+}
+
+module "invoice_fulfillment_producer" {
+  source = "../../modules/producer"
+
+  topic = kafka_topic.invoice_fulfillment.name
+
+  cert_common_name = "customer-billing/invoice-fulfillment"
+}
+
+module "bills_total_api_consumer" {
+  source = "../../modules/consumer"
+
+  topic = kafka_topic.invoice_fulfillment.name
+  consumer_group = "bills-total-api-reader"
+
+  cert_common_name = "customer-billing/bills-total-api" 
+}
