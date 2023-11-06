75af51a6f31c803d086da58e0b09fac85a0789e1
Zoltán M. Bodor <Nesze@users.noreply.github.com>
2023-11-06 14:38:09 +0000

    Set up iam credentials topic [IAM-185] (#63)

---
+++ dev-aws/kafka-shared/iam_credentials.tf
@@ -0,0 +1,23 @@
+resource "kafka_topic" "iam_credentials_v1" {
+  name               = "auth-customer.iam-credentials-v1"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    # retain 100MB on each partition
+    "retention.bytes" = "104857600"
+    # keep data for 7 days
+    "retention.ms" = "604800000"
+    # allow max 1 MB for a message
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+module "iam_credentials_producer" {
+  source = "../../modules/producer"
+
+  topic = kafka_topic.iam_credentials_v1.name
+
+  cert_common_name = "auth-customer/credentials-v1"
+}
