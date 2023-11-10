1d88692f7fd68b0a9e8d4da372193cc82e564d8c
mgrzebykuw <mgrzebyk@uw.co.uk>
2023-11-10 09:27:42 +0000

    adding dlq for invoice-fulfilment

--- dev-aws/kafka-shared/customer-billing.tf
+++ dev-aws/kafka-shared/customer-billing.tf
@@ -7,8 +7,8 @@ resource "kafka_topic" "invoice_fulfillment" {
     "retention.ms" = "604800000"
     # allow max 1 MB for a message
     "max.message.bytes" = "1048576"
-    "compression.type" = "zstd"
-    "cleanup.policy"   = "delete"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
   }
 }
 
@@ -23,8 +23,39 @@ module "invoice_fulfillment_producer" {
 module "bills_total_api_consumer" {
   source = "../../modules/consumer"
 
-  topic = kafka_topic.invoice_fulfillment.name
+  topic          = kafka_topic.invoice_fulfillment.name
   consumer_group = "bills-total-api-reader"
 
-  cert_common_name = "customer-billing/bills-total-api" 
+  cert_common_name = "customer-billing/bills-total-api"
+}
+
+resource "kafka_topic" "invoice_fulfillment_deadletter" {
+  name               = "bex.internal.accountreadytobefulfilled_deadletter"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    # keep data for 7 days
+    "retention.ms" = "604800000"
+    # allow max 1 MB for a message
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+module "invoice_fulfillment_producer" {
+  source = "../../modules/producer"
+
+  topic = kafka_topic.invoice_fulfillment_deadletter.name
+
+  cert_common_name = "customer-billing/invoice-fulfillment"
+}
+
+module "invoice_fulfillment_consumer" {
+  source = "../../modules/consumer"
+
+  topic          = kafka_topic.invoice_fulfillment_deadletter.name
+  consumer_group = "bills-total-api-reader"
+
+  cert_common_name = "customer-billing/invoice-fulfillment"
 }
