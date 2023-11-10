a2e884836895abcadc90594ca7a81130b20ef2a5
mgrzebykuw <mgrzebyk@uw.co.uk>
2023-11-10 09:41:19 +0000

    updating retention period

--- dev-aws/kafka-shared/customer-billing.tf
+++ dev-aws/kafka-shared/customer-billing.tf
@@ -34,8 +34,8 @@ resource "kafka_topic" "invoice_fulfillment_deadletter" {
   replication_factor = 3
   partitions         = 10
   config = {
-    # keep data for 7 days
-    "retention.ms" = "604800000"
+    # keep data for 14 days
+    "retention.ms" = "1209600000"
     # allow max 1 MB for a message
     "max.message.bytes" = "1048576"
     "compression.type"  = "zstd"
