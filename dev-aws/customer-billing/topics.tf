f89b1c470d88f51031a252de4dd2bd6f4193bec8
Eike Spang <espang@uw.co.uk>
2023-11-22 11:03:59 +0000

    update topics

--- dev-aws/customer-billing/topics.tf
+++ dev-aws/customer-billing/topics.tf
@@ -4,327 +4,430 @@ resource "kafka_topic" "finance-invoice-model" {
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 100MB for a message
     "max.message.bytes" = "104857600"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "invoice-fulfilment-event" {
   name               = "invoice-fulfilment-event"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 100MB for a message
     "max.message.bytes" = "104857600"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "invoice-delivery-events" {
   name               = "invoice-delivery-events"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 1MB for a message
+    "max.message.bytes" = "1048588"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "email-delivery-engine-send-failures" {
   name               = "email-delivery-engine-send-failures"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 1MB for a message
+    "max.message.bytes" = "1048588"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "invoice-fulfilment-event-deadletter" {
   name               = "invoice-fulfilment-event-deadletter"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 1MB for a message
+    "max.message.bytes" = "1048588"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "invoice-redelivery-rejections" {
   name               = "invoice-redelivery-rejections"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 1MB for a message
+    "max.message.bytes" = "1048588"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "invoice-diffs" {
   name               = "invoice-diffs"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 1MB for a message
+    "max.message.bytes" = "1048588"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "public-fulfilment-events" {
   name               = "public-fulfilment-events"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
+    "retention.bytes"  = "-1"
+    "retention.ms"     = "-1"
+    # allow max 1MB for a message
+    "max.message.bytes" = "1048588"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "dummy-bill-extracts-historic-model" {
   name               = "dummy-bill-extracts-historic-model"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 100MB for a message
     "max.message.bytes" = "104857600"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "dummy-bill-core-model" {
   name               = "dummy-bill-core-model"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 1MB for a message
+    "max.message.bytes" = "1048588"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "replicated-billing-engine-events" {
   name               = "replicated-billing-engine-events"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
+    "retention.bytes"  = "-1"
+    "retention.ms"     = "-1"
+    # allow max 1MB for a message
+    "max.message.bytes" = "1048588"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "replicated-internal-bill-core-model" {
   name               = "replicated-internal-bill-core-model"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 100MB for a message
     "max.message.bytes" = "104857600"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "replicated-bill-core-model" {
   name               = "replicated-bill-core-model"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
+    "retention.bytes"  = "-1"
+    "retention.ms"     = "-1"
+    # allow max 100MB for a message
     "max.message.bytes" = "104857600"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "replicated-bill-extracts-historic-model" {
   name               = "replicated-bill-extracts-historic-model"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 100MB for a message
     "max.message.bytes" = "104857600"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "budget-plan" {
   name               = "budget-plan"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 100MB for a message
     "max.message.bytes" = "104857600"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "assessment-requests" {
   name               = "assessment-requests"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 1MB for a message
+    "max.message.bytes" = "1048588"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "customer-change" {
   name               = "customer-change"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 1MB for a message
+    "max.message.bytes" = "1048588"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "eqdb-loader" {
   name               = "eqdb-loader"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 1MB for a message
+    "max.message.bytes" = "1048588"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "legacy-data-deadletter" {
   name               = "legacy-data-deadletter"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 1MB for a message
+    "max.message.bytes" = "1048588"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "legacy-accounts-deadletter" {
   name               = "legacy-accounts-deadletter"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 1MB for a message
+    "max.message.bytes" = "1048588"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "account-balance-change-deadletter" {
   name               = "account-balance-change-deadletter"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 1MB for a message
+    "max.message.bytes" = "1048588"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "budget-plan-calculation-requests" {
   name               = "budget-plan-calculation-requests"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 100MB for a message
     "max.message.bytes" = "104857600"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "budget-plan-calculation-deadletter" {
   name               = "budget-plan-calculation-deadletter"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 100MB for a message
     "max.message.bytes" = "104857600"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "budget-plan-fabrication-deadletter" {
   name               = "budget-plan-fabrication-deadletter"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 100MB for a message
     "max.message.bytes" = "104857600"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "budget-plan-change-notification-failures" {
   name               = "budget-plan-change-notification-failures"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 100MB for a message
     "max.message.bytes" = "104857600"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "budget-plan-change-notification-deliveries" {
   name               = "budget-plan-change-notification-deliveries"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 100MB for a message
     "max.message.bytes" = "104857600"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "bill-budget-plan-proximo-reader-deadletter" {
   name               = "bill-budget-plan-proximo-reader-deadletter"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 100MB for a message
     "max.message.bytes" = "104857600"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "billing-comms-preferences-events" {
   name               = "billing-comms-preferences-events"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 100MB for a message
     "max.message.bytes" = "104857600"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "deadletter-billing-comms-preferences-events" {
   name               = "deadletter-billing-comms-preferences-events"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 1MB for a message
+    "max.message.bytes" = "1048588"
+    "cleanup.policy"    = "delete"
   }
 }
-resource "kafka_topic" "account-payment-details_v1" {
+
+resource "kafka_topic" "account-payment-details.v1" {
   name               = "account-payment-details.v1"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 1MB for a message
+    "max.message.bytes" = "1048588"
+    "cleanup.policy"    = "delete"
   }
 }
+
 resource "kafka_topic" "dummy-placeholder" {
   name               = "dummy-placeholder"
   replication_factor = 3
   partitions         = 10
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
-  }
-}
-resource "kafka_topic" "di-filtered-billing-engine-events" {
-  name               = "di-filtered-billing-engine-events"
-  replication_factor = 3
-  partitions         = 10
-  config = {
-    "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
-  }
-}
-resource "kafka_topic" "di-basia-test-2" {
-  name               = "di-basia-test-2"
-  replication_factor = 3
-  partitions         = 10
-  config = {
-    "compression.type" = "zstd"
-    "retention.bytes" = "805306368000"
-    "max.message.bytes" = "104857600"
+    # retain 8GB on each partition
+    "retention.bytes" = "8053063680"
+    # allow max 1MB for a message
+    "max.message.bytes" = "1048588"
+    "cleanup.policy"    = "delete"
   }
 }
