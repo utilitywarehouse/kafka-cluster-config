95d10916e96ae4d8f32481abb7c5365c6cb921ba
Daniel Copaciu <dani.copaciu@gmail.com>
2023-12-05 16:26:57 +0200

    Create topics related to complaints fabricator

---
+++ prod-aws/finance/complaints-fabricator.tf
@@ -0,0 +1,21 @@
+resource "kafka_topic" "complaints-diffs-events" {
+  name               = "complaints-diffs.events"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    "retention.bytes" = "-1"
+    "retention.ms"    = "2592000000" #30 days
+    "cleanup.policy"  = "delete"
+  }
+}
+
+resource "kafka_topic" "complaints-eqdb-loader-events" {
+  name               = "complaints.eqdb-loader.events"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    "retention.bytes" = "-1"
+    "retention.ms"    = "-1" 
+    "cleanup.policy"  = "delete"
+  }
+}
