215a7e87c288e117159912041a0a13a522072ead
Daniel Copaciu <dani.copaciu@gmail.com>
2023-12-06 13:36:21 +0200

    Fix number of partitions

--- dev-aws/finance/disputes-fabricator.tf
+++ dev-aws/finance/disputes-fabricator.tf
@@ -1,7 +1,7 @@
 resource "kafka_topic" "comms-eqdb-loader-events" {
   name               = "comms.eqdb-loader.events"
   replication_factor = 3
-  partitions         = 10
+  partitions         = 1
   config = {
     "retention.bytes" = "-1"
     "retention.ms"    = "2592000000"
