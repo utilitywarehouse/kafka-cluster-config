a9d60a1c0c6a081669f4c0ea6e6154a4b9143a8a
Rob Crowe <282571+rcrowe@users.noreply.github.com>
2024-01-24 16:16:11 +0000

    IAM identitydb topic in prod (#113)
    Same change as [dev](https://github.com/utilitywarehouse/kafka-cluster-config/blob/main/dev-aws/kafka-shared/iam_identitydb.tf) but for prod

---
+++ prod-aws/kafka-shared/iam_identitydb.tf
@@ -0,0 +1,51 @@
+resource "kafka_topic" "iam_identitydb_v1" {
+  name               = "auth.iam-identitydb-v1"
+  replication_factor = 3
+  # MUST be 1 partition as identitydb assumes this to be true
+  partitions         = 1
+  config = {
+    # retain 100MB on each partition
+    "retention.bytes" = "104857600"
+    # keep data for 30 days
+    "retention.ms" = "2592000000"
+    # allow max 5 MB for a message
+    "max.message.bytes" = "5242880"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+module "iam_identitydb_event_forwarder_producer" {
+  source = "../../modules/producer"
+
+  topic = kafka_topic.iam_identitydb_v1.name
+
+  cert_common_name = "auth/iam-identitydb-event-forwarder"
+}
+
+module "iam_identitydb_snapshotter_consumer" {
+  source = "../../modules/consumer"
+
+  topic          = kafka_topic.iam_identitydb_v1.name
+  consumer_group = "iam-identitydb-snapshotter"
+
+  cert_common_name = "auth/iam-identitydb-snapshotter"
+}
+
+module "iam_identitydb_identity_api_consumer" {
+  source = "../../modules/consumer"
+
+  topic          = kafka_topic.iam_identitydb_v1.name
+  consumer_group = "iam-identity-api"
+
+  cert_common_name = "auth/iam-identity-api"
+}
+
+module "iam_identitydb_policy_decision_api_consumer" {
+  source = "../../modules/consumer"
+
+  topic          = kafka_topic.iam_identitydb_v1.name
+  consumer_group = "iam-policy-decision-api"
+
+  cert_common_name = "auth/iam-policy-decision-api"
+}
