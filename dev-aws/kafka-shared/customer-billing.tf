eb03e414a5ee6c82745bfa38040de25225d08028
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2024-02-05 16:20:09 +0200

    kafka-shared: refactor customer-billing to tls-app module

--- dev-aws/kafka-shared/customer-billing.tf
+++ dev-aws/kafka-shared/customer-billing.tf
@@ -2,40 +2,23 @@ resource "kafka_topic" "invoice_fulfillment" {
   name               = "bex.internal.bill_fulfilled"
   replication_factor = 3
   partitions         = 10
-  config = {
+  config             = {
     # keep data for 7 days
-    "retention.ms" = "604800000"
+    "retention.ms"      = "604800000"
     # allow max 1 MB for a message
     "max.message.bytes" = "1048576"
     "compression.type"  = "zstd"
     "cleanup.policy"    = "delete"
   }
-}
-
-module "invoice_fulfillment_producer" {
-  source = "../../modules/producer"
-
-  topic = kafka_topic.invoice_fulfillment.name
-
-  cert_common_name = "customer-billing/invoice-fulfillment"
-}
-
-module "bills_total_api_consumer" {
-  source = "../../modules/consumer"
-
-  topic          = kafka_topic.invoice_fulfillment.name
-  consumer_group = "bex.bills-total-api-reader"
-
-  cert_common_name = "customer-billing/bills-total-api"
 }
 
 resource "kafka_topic" "invoice_fulfillment_deadletter" {
   name               = "bex.internal.accountreadytobefulfilled_deadletter"
   replication_factor = 3
   partitions         = 10
-  config = {
+  config             = {
     # keep data for 14 days
-    "retention.ms" = "1209600000"
+    "retention.ms"      = "1209600000"
     # allow max 1 MB for a message
     "max.message.bytes" = "1048576"
     "compression.type"  = "zstd"
@@ -43,10 +26,17 @@ resource "kafka_topic" "invoice_fulfillment_deadletter" {
   }
 }
 
-module "invoice_fulfillment_deadletter_producer" {
-  source = "../../modules/producer"
-
-  topic = kafka_topic.invoice_fulfillment_deadletter.name
+module "invoice_fulfillment" {
+  source = "../../modules/tls-app"
 
   cert_common_name = "customer-billing/invoice-fulfillment"
+  produce_topics   = [kafka_topic.invoice_fulfillment.name, kafka_topic.invoice_fulfillment_deadletter.name]
 }
+
+module "bills_total_api" {
+  source = "../../modules/tls-app"
+
+  cert_common_name = "customer-billing/bills-total-api"
+  consume_topics   = { (kafka_topic.invoice_fulfillment.name) : "bex.bills-total-api-reader" }
+}
+
