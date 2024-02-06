76527f7cc1da857c8061bdc4bdc2ca41c61364f9
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2024-02-06 09:50:37 +0200

    kafka-shared: refactor customer billing to use the new tls-app module

--- dev-aws/kafka-shared/customer-billing.tf
+++ dev-aws/kafka-shared/customer-billing.tf
@@ -27,16 +27,43 @@ resource "kafka_topic" "invoice_fulfillment_deadletter" {
 }
 
 module "invoice_fulfillment" {
-  source = "../../modules/tls-app"
-
+  source           = "../../modules/tls-app"
   cert_common_name = "customer-billing/invoice-fulfillment"
   produce_topics   = [kafka_topic.invoice_fulfillment.name, kafka_topic.invoice_fulfillment_deadletter.name]
 }
 
 module "bills_total_api" {
-  source = "../../modules/tls-app"
-
+  source           = "../../modules/tls-app"
   cert_common_name = "customer-billing/bills-total-api"
   consume_topics   = { (kafka_topic.invoice_fulfillment.name) : "bex.bills-total-api-reader" }
 }
 
+moved {
+  from = module.invoice_fulfillment_deadletter_producer.kafka_acl.producer_acl
+  to   = module.invoice_fulfillment.kafka_acl.producer_acl["bex.internal.accountreadytobefulfilled_deadletter"]
+}
+
+moved {
+  from = module.invoice_fulfillment_producer.kafka_acl.producer_acl
+  to   = module.invoice_fulfillment.kafka_acl.producer_acl["bex.internal.bill_fulfilled"]
+}
+
+moved {
+  from = module.invoice_fulfillment_producer.kafka_quota.producer_quota
+  to   = module.invoice_fulfillment.kafka_quota.quota
+}
+
+moved {
+  from = module.bills_total_api_consumer.kafka_acl.topic_acl
+  to   = module.bills_total_api.kafka_acl.topic_acl["bex.internal.bill_fulfilled"]
+}
+
+moved {
+  from = module.bills_total_api_consumer.kafka_acl.group_acl
+  to   = module.bills_total_api.kafka_acl.group_acl["bex.internal.bill_fulfilled"]
+}
+
+moved {
+  from = module.bills_total_api_consumer.kafka_quota.consumer_quota
+  to   = module.bills_total_api.kafka_quota.quota
+}
