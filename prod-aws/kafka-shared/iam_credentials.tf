8f33d930b10d03f61f3c695f5060f3f1ee40060e
sbuliarca <sbuliarca@utilitywarehouse.co.uk>
2024-02-06 15:33:57 +0200

    kafka-shared: prod: refactor iam to use the tls-app module (#126)

--- prod-aws/kafka-shared/iam_credentials.tf
+++ prod-aws/kafka-shared/iam_credentials.tf
@@ -14,19 +14,39 @@ resource "kafka_topic" "iam_credentials_v1" {
   }
 }
 
-module "iam_credentials_producer" {
-  source = "../../modules/producer"
-
-  topic = kafka_topic.iam_credentials_v1.name
-
+module "iam_credentials_api" {
+  source           = "../../modules/tls-app"
+  produce_topics   = [kafka_topic.iam_credentials_v1.name]
   cert_common_name = "auth-customer/credentials-api"
 }
 
-module "iam_credentials_consumer" {
-  source = "../../modules/consumer"
-
-  topic          = kafka_topic.iam_credentials_v1.name
-  consumer_group = "indexer-iam-credentials-v1"
-
+module "iam_credentials_indexer" {
+  source           = "../../modules/tls-app"
+  consume_topics   = { (kafka_topic.iam_credentials_v1.name) : "indexer-iam-credentials-v1" }
   cert_common_name = "auth-customer/iam-credentials-v1-indexer"
+}
+
+moved {
+  from = module.iam_credentials_producer.kafka_acl.producer_acl
+  to   = module.iam_credentials_api.kafka_acl.producer_acl["auth-customer.iam-credentials-v1"]
+}
+
+moved {
+  from = module.iam_credentials_producer.kafka_quota.producer_quota
+  to   = module.iam_credentials_api.kafka_quota.quota
+}
+
+moved {
+  from = module.iam_credentials_consumer.kafka_acl.group_acl
+  to   = module.iam_credentials_indexer.kafka_acl.group_acl["auth-customer.iam-credentials-v1"]
+}
+
+moved {
+  from = module.iam_credentials_consumer.kafka_acl.topic_acl
+  to   = module.iam_credentials_indexer.kafka_acl.topic_acl["auth-customer.iam-credentials-v1"]
+}
+
+moved {
+  from = module.iam_credentials_consumer.kafka_quota.consumer_quota
+  to   = module.iam_credentials_indexer.kafka_quota.quota
 }
--- prod-aws/kafka-shared/iam_identitydb.tf
+++ prod-aws/kafka-shared/iam_identitydb.tf
@@ -3,11 +3,11 @@ resource "kafka_topic" "iam_identitydb_v1" {
   replication_factor = 3
   # MUST be 1 partition as identitydb assumes this to be true
   partitions         = 1
-  config = {
+  config             = {
     # retain 100MB on each partition
-    "retention.bytes" = "104857600"
+    "retention.bytes"   = "104857600"
     # keep data for 30 days
-    "retention.ms" = "2592000000"
+    "retention.ms"      = "2592000000"
     # allow max 5 MB for a message
     "max.message.bytes" = "5242880"
     "compression.type"  = "zstd"
@@ -15,42 +15,72 @@ resource "kafka_topic" "iam_identitydb_v1" {
   }
 }
 
-module "iam_identitydb_event_forwarder_producer" {
-  source = "../../modules/producer"
-
-  topic = kafka_topic.iam_identitydb_v1.name
-
+module "iam_identitydb_event_forwarder" {
+  source           = "../../modules/tls-app"
+  produce_topics   = [kafka_topic.iam_identitydb_v1.name]
   cert_common_name = "auth/iam-identitydb-event-forwarder"
 }
 
-module "iam_identitydb_snapshotter_consumer" {
-  source = "../../modules/consumer"
-
-  topic          = kafka_topic.iam_identitydb_v1.name
-  consumer_group = "iam-identitydb-snapshotter"
-
+module "iam_identitydb_snapshotter" {
+  source           = "../../modules/tls-app"
+  consume_topics   = { (kafka_topic.iam_identitydb_v1.name) : "iam-identitydb-snapshotter" }
   cert_common_name = "auth/iam-identitydb-snapshotter"
 }
 
-module "iam_identitydb_identity_api_consumer" {
-  source = "../../modules/consumer"
-
-  topic          = kafka_topic.iam_identitydb_v1.name
-  consumer_group = "iam-identity-api"
-
+module "iam_identity_api" {
+  source           = "../../modules/tls-app"
+  consume_topics   = { (kafka_topic.iam_identitydb_v1.name) : "iam-identity-api" }
   cert_common_name = "auth/iam-identity-api"
 }
 
 module "iam_policy_decision_api" {
-  source = "../../modules/tls-app"
-
+  source           = "../../modules/tls-app"
   cert_common_name = "auth/iam-policy-decision-api"
-
-  produce_topics = [kafka_topic.iam_cerbos_audit_v1.name]
-  consume_topics = { (kafka_topic.iam_identitydb_v1.name) : "iam-policy-decision-api" }
+  produce_topics   = [kafka_topic.iam_cerbos_audit_v1.name]
+  consume_topics   = { (kafka_topic.iam_identitydb_v1.name) : "iam-policy-decision-api" }
 }
 
 moved {
   from = module.iam_policy_decision_api.kafka_quota.producer_quota
   to   = module.iam_policy_decision_api.kafka_quota.quota
+}
+
+moved {
+  from = module.iam_identitydb_snapshotter_consumer.kafka_acl.group_acl
+  to   = module.iam_identitydb_snapshotter.kafka_acl.group_acl["auth.iam-identitydb-v1"]
+}
+
+moved {
+  from = module.iam_identitydb_snapshotter_consumer.kafka_acl.topic_acl
+  to   = module.iam_identitydb_snapshotter.kafka_acl.topic_acl["auth.iam-identitydb-v1"]
+}
+
+moved {
+  from = module.iam_identitydb_snapshotter_consumer.kafka_quota.consumer_quota
+  to   = module.iam_identitydb_snapshotter.kafka_quota.quota
+}
+
+moved {
+  from = module.iam_identitydb_identity_api_consumer.kafka_acl.topic_acl
+  to   = module.iam_identity_api.kafka_acl.topic_acl["auth.iam-identitydb-v1"]
+}
+
+moved {
+  from = module.iam_identitydb_identity_api_consumer.kafka_acl.group_acl
+  to   = module.iam_identity_api.kafka_acl.group_acl["auth.iam-identitydb-v1"]
+}
+
+moved {
+  from = module.iam_identitydb_identity_api_consumer.kafka_quota.consumer_quota
+  to   = module.iam_identity_api.kafka_quota.quota
+}
+
+moved {
+  from = module.iam_identitydb_event_forwarder_producer.kafka_acl.producer_acl
+  to   = module.iam_identitydb_event_forwarder.kafka_acl.producer_acl["auth.iam-identitydb-v1"]
+}
+
+moved {
+  from = module.iam_identitydb_event_forwarder_producer.kafka_quota.producer_quota
+  to   = module.iam_identitydb_event_forwarder.kafka_quota.quota
 }
