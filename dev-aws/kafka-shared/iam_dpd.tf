f0a053205ebd271bc44a8755865addbcaee210a0
Zoltan Bodor <zbodor@utilitywarehouse.co.uk>
2024-02-06 16:35:25 +0200

    Rm empty lines

--- dev-aws/kafka-shared/iam_dpd.tf
+++ dev-aws/kafka-shared/iam_dpd.tf
@@ -15,36 +15,28 @@ resource "kafka_topic" "iam_dpd_v1" {
 }
 
 module "iam_dpd_producer" {
-  source = "../../modules/producer"
-
-  topic = kafka_topic.iam_dpd_v1.name
-
+  source           = "../../modules/producer"
+  topic            = kafka_topic.iam_dpd_v1.name
   cert_common_name = "auth-customer/dpd-mapper"
 }
 
 module "iam_dpd_consumer_di_kafka_source_customer_login_succeeded" {
-  source = "../../modules/consumer"
-
-  topic          = kafka_topic.iam_dpd_v1.name
-  consumer_group = "di-kafka-source-customer-login-succeeded"
-
+  source           = "../../modules/consumer"
+  topic            = kafka_topic.iam_dpd_v1.name
+  consumer_group   = "di-kafka-source-customer-login-succeeded"
   cert_common_name = "auth-customer/di-kafka-source-customer-login-succeeded"
 }
 
 module "iam_dpd_consumer_di_kafka_source_customer_login_failed" {
-  source = "../../modules/consumer"
-
-  topic          = kafka_topic.iam_dpd_v1.name
-  consumer_group = "di-kafka-source-customer-login-failed"
-
+  source           = "../../modules/consumer"
+  topic            = kafka_topic.iam_dpd_v1.name
+  consumer_group   = "di-kafka-source-customer-login-failed"
   cert_common_name = "auth-customer/di-kafka-source-customer-login-failed"
 }
 
 module "iam_dpd_consumer_di_kafka_source_customer_password_reset_failed" {
-  source = "../../modules/consumer"
-
-  topic          = kafka_topic.iam_dpd_v1.name
-  consumer_group = "di-kafka-source-customer-password-reset-failed"
-
+  source           = "../../modules/consumer"
+  topic            = kafka_topic.iam_dpd_v1.name
+  consumer_group   = "di-kafka-source-customer-password-reset-failed"
   cert_common_name = "auth-customer/di-kafka-source-customer-password-reset-failed"
 }
