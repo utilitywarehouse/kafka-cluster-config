f69d4ea84f7c3fc1f2ad25ddad06bc08cf85dd10
Craig Dempsie <cdempsie@uw.co.uk>
2024-02-23 11:49:13 +0000

    Update retention settings, remove comments

--- dev-aws/kafka-shared-msk/energy-budget-plan/energy-budget-plan.tf
+++ dev-aws/kafka-shared-msk/energy-budget-plan/energy-budget-plan.tf
@@ -26,23 +26,15 @@ module "budget_plan_fabricator" {
   cert_common_name = "energy-budget-plan/eqdb-fabricator"
 }
 
-#- max_message_bytes: "104857600" - 100MB !
-#  max_retention_bytes: "805306368000" - 768000MB !
-#  name: budget-plan
-#  partitions: 10
-#  replication_factor: 3
-#  topic_compression: snappy
-#  max_retention_time: "3024000000"
-
 resource "kafka_topic" "budget_plan" {
   name               = "energy-budget-plan.budget-plan"
   replication_factor = 3
   partitions         = 10
   config = {
-    # retain 100MB on each partition - TODO
-    "retention.bytes" = "104857600"
-    # keep data for 5 weeks
-    "retention.ms" = "3024000000"
+    # no limit on retention size, using time based instead
+    "retention.bytes" = "-1"
+    # keep data for 7 years
+    "retention.ms" = "220898482000"
     # allow max 1 MB for a message
     "max.message.bytes" = "1048576"
     "compression.type"  = "zstd"
@@ -50,22 +42,15 @@ resource "kafka_topic" "budget_plan" {
   }
 }
 
-#- max_message_bytes: "104857600" - 100MB !
-#  max_retention_bytes: "805306368000" - 768000MB !
-#  name: customer-change
-#  partitions: 10
-#  replication_factor: 3
-#  topic_compression: snappy
-
 resource "kafka_topic" "customer_change" {
   name               = "energy-budget-plan.customer-change"
   replication_factor = 3
   partitions         = 10
   config = {
-    # retain 100MB on each partition - TODO
-    "retention.bytes" = "104857600"
-    # keep data for 5 weeks - TODO not set in Kafka Topic applier https://github.com/utilitywarehouse/kubernetes-manifests/blob/d632983de4052ff8f33ccdd37ba886a16b322a49/prod-aws/customer-billing/kafka-topic-applier/topics.yaml#L98-L103
-    "retention.ms" = "3024000000"
+    # no limit on retention size, using time based instead
+    "retention.bytes" = "-1"
+    # keep data for 2 years
+    "retention.ms" = "63113852000"
     # allow max 1 MB for a message
     "max.message.bytes" = "1048576"
     "compression.type"  = "zstd"
