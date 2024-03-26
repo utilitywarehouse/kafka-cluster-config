eacf310e8ae4a5340214d223b78a9a1e6a96245d
Catalin Ilea <cilea@utilitywarehouse.co.uk>
2024-03-26 15:39:44 +0100

    Added dev data-infra topics

---
+++ dev-aws/kafka-shared-msk/data-infra/data-infra.tf
@@ -0,0 +1,50 @@
+resource "kafka_topic" "events" {
+  name               = "data-infra.uw.data-infra.product.v1.events"
+  replication_factor = 3
+  partitions         = 15
+  config = {
+    "remote.storage.enable" = "true"
+    # infinite retention
+    "retention.ms" = "-1"
+    # keep data in hot storage for 1 day
+    "local.retention.ms" = "86400000"
+    # allow max 1 MB for a message
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+resource "kafka_topic" "events_send" {
+  name               = "data-infra.uw.data-infra.product.v1.eventsend"
+  replication_factor = 3
+  partitions         = 15
+  config = {
+    "remote.storage.enable" = "true"
+    # infinite retention
+    "retention.ms" = "-1"
+    # keep data in hot storage for 1 day
+    "local.retention.ms" = "86400000"
+    # allow max 1 MB for a message
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+resource "kafka_topic" "snowplow" {
+  name               = "data-infra.uw.data-infra.pubsubbrige.snowplow"
+  replication_factor = 3
+  partitions         = 15
+  config = {
+    "remote.storage.enable" = "true"
+    # 5 days
+    "retention.ms" = "432000000"
+    # keep data in hot storage for 1 day
+    "local.retention.ms" = "86400000"
+    # allow max 1 MB for a message
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
