e585e9cf94a0d2460a7491eb6c4b028973c535e3
Craig Dempsie <cdempsie@uw.co.uk>
2024-03-27 08:43:58 +0000

    Fix parentheses as no longer required

--- dev-aws/kafka-shared-msk/energy-budget-plan/energy-budget-plan.tf
+++ dev-aws/kafka-shared-msk/energy-budget-plan/energy-budget-plan.tf
@@ -76,15 +76,15 @@ module "eqdb_loader_process" {
 
 module "budget_plan_fabricator" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [(kafka_topic.eqdb_loader.name), (kafka_topic.customer_change.name)]
+  consume_topics   = [kafka_topic.eqdb_loader.name, kafka_topic.customer_change.name]
   consume_groups   = ["energy-budget-plan.eqdb-fabricator-loader-v1", "energy-budget-plan.eqdb-fabricator-customer-change-v1"]
-  produce_topics   = [(kafka_topic.customer_change.name), (kafka_topic.fabricator_deadletter.name)]
+  produce_topics   = [kafka_topic.customer_change.name, kafka_topic.fabricator_deadletter.name]
   cert_common_name = "energy-budget-plan/eqdb-fabricator"
 }
 
 module "budget_plan_calculator" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [(kafka_topic.budget_plan.name)]
+  consume_topics   = [kafka_topic.budget_plan.name]
   consume_groups   = ["energy-budget-plan.calculator-v1"]
   produce_topics   = [kafka_topic.budget_plan.name]
   cert_common_name = "energy-budget-plan/calculator"
@@ -92,14 +92,14 @@ module "budget_plan_calculator" {
 
 module "budget_plan_di_kafka_source_requests" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [(kafka_topic.budget_plan.name)]
+  consume_topics   = [kafka_topic.budget_plan.name]
   consume_groups   = ["energy-budget-plan.di-kafka-source-calculation-requests-v1"]
   cert_common_name = "energy-budget-plan/di-kafka-source-calculation-requests"
 }
 
 module "budget_plan_di_kafka_source_recommendation" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [(kafka_topic.budget_plan.name)]
+  consume_topics   = [kafka_topic.budget_plan.name]
   consume_groups   = ["energy-budget-plan.di-kafka-source-recommendations-v1"]
   cert_common_name = "energy-budget-plan/di-kafka-source-recommendations"
 }
