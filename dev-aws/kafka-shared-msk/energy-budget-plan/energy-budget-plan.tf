66227010e305f89443927d76fd46b6ceccfa4177
Craig Dempsie <cdempsie@uw.co.uk>
2024-03-27 14:18:34 +0000

    Adds deadletter for Fabricator (#203)

--- dev-aws/kafka-shared-msk/energy-budget-plan/energy-budget-plan.tf
+++ dev-aws/kafka-shared-msk/energy-budget-plan/energy-budget-plan.tf
@@ -50,6 +50,24 @@ resource "kafka_topic" "customer_change" {
   }
 }
 
+resource "kafka_topic" "fabricator_deadletter" {
+  name               = "energy-budget-plan.fabricator-deadletter"
+  replication_factor = 3
+  partitions         = 3
+  config = {
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data for 2 months
+    "retention.ms" = "5259600000"
+    # keep data in hot storage for 2 days
+    "local.retention.ms" = "172800000"
+    # allow max 1 MB for a message
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
 module "eqdb_loader_process" {
   source           = "../../../modules/tls-app"
   produce_topics   = [kafka_topic.eqdb_loader.name]
@@ -58,15 +76,15 @@ module "eqdb_loader_process" {
 
 module "budget_plan_fabricator" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [(kafka_topic.eqdb_loader.name), (kafka_topic.customer_change.name)]
+  consume_topics   = [kafka_topic.eqdb_loader.name, kafka_topic.customer_change.name]
   consume_groups   = ["energy-budget-plan.eqdb-fabricator-loader-v1", "energy-budget-plan.eqdb-fabricator-customer-change-v1"]
-  produce_topics   = [kafka_topic.customer_change.name]
+  produce_topics   = [kafka_topic.customer_change.name, kafka_topic.fabricator_deadletter.name]
   cert_common_name = "energy-budget-plan/eqdb-fabricator"
 }
 
 module "budget_plan_calculator" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [(kafka_topic.budget_plan.name)]
+  consume_topics   = [kafka_topic.budget_plan.name]
   consume_groups   = ["energy-budget-plan.calculator-v1"]
   produce_topics   = [kafka_topic.budget_plan.name]
   cert_common_name = "energy-budget-plan/calculator"
@@ -74,14 +92,14 @@ module "budget_plan_calculator" {
 
 module "budget_plan_di_kafka_source_requests" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [(kafka_topic.budget_plan.name)]
+  consume_topics   = [kafka_topic.budget_plan.name]
   consume_groups   = ["energy-budget-plan.di-kafka-source-calculation-requests-v1"]
   cert_common_name = "energy-budget-plan/di-kafka-source-calculation-requests"
 }
 
 module "budget_plan_di_kafka_source_recommendation" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [(kafka_topic.budget_plan.name)]
+  consume_topics   = [kafka_topic.budget_plan.name]
   consume_groups   = ["energy-budget-plan.di-kafka-source-recommendations-v1"]
   cert_common_name = "energy-budget-plan/di-kafka-source-recommendations"
 }
