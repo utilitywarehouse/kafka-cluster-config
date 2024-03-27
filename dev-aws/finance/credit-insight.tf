7a1f6f7d8be513df5cdaa0b746f432b33729c4ce
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2024-03-27 15:12:02 +0000

    feat(insights): add experimental topic for insights

--- dev-aws/finance/credit-insight.tf
+++ dev-aws/finance/credit-insight.tf
@@ -30,3 +30,15 @@ resource "kafka_topic" "credit-insight-account-changed-events" {
     "cleanup.policy"  = "delete"
   }
 }
+
+# Experimental
+resource "kafka_topic" "account-insight-events" {
+  name               = "account-insight.events"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    "retention.bytes" = "-1"
+    "retention.ms"    = "604800000" #7 days
+    "cleanup.policy"  = "delete"
+  }
+}
