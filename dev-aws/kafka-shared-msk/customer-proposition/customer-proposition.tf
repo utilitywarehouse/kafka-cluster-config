43ba474b7fb95a423861a4a1c0817b090622e305
mwindlinger <mwindlinger@gmail.com>
2024-03-28 12:59:16 +0000

    Ap 549 uswitch data topic and acls dev (#208)
    * uswitch data topic and ACLs in dev
    
    * add codeowners
    
    * appease the linter
    
    * updates based on PR comments
    
    * udpate CODEOWNERS
    
    * add back newline
    
    * appease the linter

---
+++ dev-aws/kafka-shared-msk/customer-proposition/customer-proposition.tf
@@ -0,0 +1,32 @@
+resource "kafka_topic" "uswitch.data.v1" {
+  name = "customer-proposition.uswitch.data.v1"
+
+  replication_factor = 3
+  partitions         = 15
+
+  # infinte retention
+  config = {
+    "remote.storage.enable" = "true"
+    "retention.bytes"       = "-1"
+    "retention.ms"          = "-1"
+    # keep data locally for 1 hour
+    "local.retention.ms" = "3600000"
+    # allow max 1 MB for a message
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+module "uswitch-data-projector" {
+  source           = "../../../modules/tls-app"
+  produce_topics   = [kafka_topic.uswitch.data.v1]
+  cert_common_name = "customer-proposition/uswitch-data-projector"
+}
+
+module "di-uswitch-orders-kafka-source" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [kafka_topic.uswitch.data.v1]
+  consume_groups   = [customer-proposition.di-uswitch-orders-kafka-source]
+  cert_common_name = "customer-proposition/di-uswitch-orders-kafka-source"
+}
