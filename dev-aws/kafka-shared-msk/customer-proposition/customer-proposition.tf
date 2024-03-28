e50a24804f03188d42d94a05232a3bb0a0c3dff3
Michael Windlinger <mwindlinger@uw.co.uk>
2024-03-28 09:20:26 +0000

    updates based on PR comments

--- dev-aws/kafka-shared-msk/customer-proposition/customer-proposition.tf
+++ dev-aws/kafka-shared-msk/customer-proposition/customer-proposition.tf
@@ -1,5 +1,5 @@
 resource "kafka_topic" "uswitch.data.v1" {
-  name = "customer_proposition.uswitch.data.v1"
+  name = "customer-proposition.uswitch.data.v1"
 
   replication_factor = 3
   partitions         = 15
@@ -9,7 +9,9 @@ resource "kafka_topic" "uswitch.data.v1" {
     "remote.storage.enable" = "true"
     "retention.bytes"       = "-1"
     "retention.ms"          = "-1"
+    # keep data locally for 1 hour
     "local.retention.ms"    = "3600000"
+    # allow max 1 MB for a message
     "max.message.bytes"     = "1048576"
     "compression.type"      = "zstd"
     "cleanup.policy"        = "delete"
@@ -25,5 +27,6 @@ module "uswitch-data-projector" {
 module "di-uswitch-orders-kafka-source" {
   source           = "../../../modules/tls-app"
   consume_topics   = [kafka_topic.uswitch.data.v1]
+  consume_groups   = [customer-proposition.di-uswitch-orders-kafka-source]
   cert_common_name = "customer-proposition/di-uswitch-orders-kafka-source"
 }
