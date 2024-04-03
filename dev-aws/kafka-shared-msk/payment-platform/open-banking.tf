9f13070c9367b05d803ff85cdcb56f530de43222
Daniel Copaciu <dani.copaciu@gmail.com>
2024-04-03 13:15:24 +0300

    Create kafka topics for open banking (#218)
    * Create kafka topics for open banking
    
    * Fix codeowners
    
    * Revert change to codeowners
    
    * Use tiered storage

---
+++ dev-aws/kafka-shared-msk/payment-platform/open-banking.tf
@@ -0,0 +1,67 @@
+resource "kafka_topic" "openbanking-v1-internal-payments" {
+  name               = "payment-platform.openbanking.v1.internal.payments"
+  replication_factor = 3
+  partitions         = 5
+  config = {
+    "compression.type" = "zstd"
+    "retention.bytes"  = "-1"
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data in hot storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data for 30 days
+    "retention.ms"   = "2592000000"
+    "cleanup.policy" = "delete"
+  }
+}
+
+resource "kafka_topic" "openbanking-deadletter-v1-internal-payments" {
+  name               = "payment-platform.openbanking-deadletter.v1.internal.payments"
+  replication_factor = 3
+  partitions         = 5
+  config = {
+    "compression.type" = "zstd"
+    "retention.bytes"  = "-1"
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data in hot storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data for 30 days
+    "retention.ms"   = "2592000000"
+    "cleanup.policy" = "delete"
+  }
+}
+
+resource "kafka_topic" "openbanking-v1-internal-payment-methods" {
+  name               = "payment-platform.openbanking.v1.internal.payment-methods"
+  replication_factor = 3
+  partitions         = 5
+  config = {
+    "compression.type" = "zstd"
+    "retention.bytes"  = "-1"
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data in hot storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data for 30 days
+    "retention.ms"   = "2592000000"
+    "cleanup.policy" = "delete"
+  }
+}
+
+resource "kafka_topic" "openbanking-deadletter-v1-internal-payment-methods" {
+  name               = "payment-platform.openbanking-deadletter.v1.internal.payment-methods"
+  replication_factor = 3
+  partitions         = 5
+  config = {
+    "compression.type" = "zstd"
+    "retention.bytes"  = "-1"
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data in hot storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data for 30 days
+    "retention.ms"   = "2592000000"
+    "cleanup.policy" = "delete"
+  }
+}
