3397a19553eb7215be7b726915d08c01792d7e52
marcinginszt <marcinginszt@gmail.com>
2024-04-11 10:04:35 +0200

    allow test proximo access to shared kafka cluster

---
+++ dev-aws/kafka-shared/dev-enablement/proximo-examples.tf
@@ -0,0 +1,23 @@
+resource "kafka_topic" "proximo_example" {
+  name               = "dev-enablement.proximo-example"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    # retain 100MB on each partition
+    "retention.bytes" = "104857600"
+    # keep data for 2 days
+    "retention.ms" = "172800000"
+    # allow max 1 MB for a message
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+module "example_proximo" {
+  source           = "../../../modules/tls-app"
+  produce_topics   = [kafka_topic.proximo_example.name]
+  consume_topics   = [(kafka_topic.proximo_example.name)]
+  consume_groups   = ["dev-enablement.example-proximo"]
+  cert_common_name = "dev-enablement/example-proximo"
+}
