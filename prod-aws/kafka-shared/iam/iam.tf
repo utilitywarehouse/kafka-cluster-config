207f425b360c9fb534223773b6a766ae42e9fee7
Zoltan Bodor <zbodor@utilitywarehouse.co.uk>
2024-04-12 11:19:54 +0300

    Add iam-creds-comms-processor

--- prod-aws/kafka-shared/iam/iam.tf
+++ prod-aws/kafka-shared/iam/iam.tf
@@ -57,6 +57,13 @@ module "iam_credentials_indexer" {
   cert_common_name = "auth-customer/iam-credentials-v1-indexer"
 }
 
+module "iam_credentials_comms_processor" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [(kafka_topic.iam_credentials_v1.name)]
+  consume_groups   = ["iam.credentials-comms-processor"]
+  cert_common_name = "auth-customer/iam-credentials-comms-processor"
+}
+
 module "iam_customer_auth_provider" {
   source           = "../../../modules/tls-app"
   produce_topics   = [kafka_topic.iam_credentials_v1.name]
