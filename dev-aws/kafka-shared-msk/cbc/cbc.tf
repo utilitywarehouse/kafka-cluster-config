10f3e04d2f2328b897edae66167322222a75c2e1
Luke Gibbard <gibbard27@gmail.com>
2024-04-24 15:27:13 +0100

    fix event spelling

--- dev-aws/kafka-shared-msk/cbc/cbc.tf
+++ dev-aws/kafka-shared-msk/cbc/cbc.tf
@@ -16,7 +16,7 @@ resource "kafka_topic" "FraudEvents" {
   }
 }
 
-resource "kafka_topic" "rating_rvents_v3" {
+resource "kafka_topic" "rating_events_v3" {
   name = "cbc.RatingEvents_v3"
 
   replication_factor = 3
