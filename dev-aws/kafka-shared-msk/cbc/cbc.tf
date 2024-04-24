9c86543a9b6a70168855a1c1ddbd0afe30984c9d
Luke Gibbard <gibbard27@gmail.com>
2024-04-24 15:30:07 +0100

    update module rating topic name

--- dev-aws/kafka-shared-msk/cbc/cbc.tf
+++ dev-aws/kafka-shared-msk/cbc/cbc.tf
@@ -49,69 +49,69 @@ module "cbc_fraud_detection_consumer" {
 
 module "cbc_events_indexer" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [kafka_topic.FraudEvents.name, kafka_topic.RatingEvents_v3.name]
+  consume_topics   = [kafka_topic.FraudEvents.name, kafka_topic.rating_events_v3.name]
   consume_groups   = ["cbc.cbc-events-indexer"]
   cert_common_name = "cbc/cbc-events-indexer"
 }
 
 module "cbc_bigquery_exporter" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [kafka_topic.FraudEvents.name, kafka_topic.RatingEvents_v3.name]
+  consume_topics   = [kafka_topic.FraudEvents.name, kafka_topic.rating_events_v3.name]
   consume_groups   = ["cbc.cbc-bigquery-exporter-v1"]
   cert_common_name = "cbc/cbc-bigquery-exporter"
 }
 
 module "cbc_insights_projector" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [kafka_topic.RatingEvents_v3.name]
+  consume_topics   = [kafka_topic.rating_events_v3.name]
   consume_groups   = ["cbc.cbc-insights-projector"]
   cert_common_name = "cbc/cbc-insights-projector"
 }
 
 module "cbc_transaction_notification_projector" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [kafka_topic.RatingEvents_v3.name]
+  consume_topics   = [kafka_topic.rating_events_v3.name]
   consume_groups   = ["cbc.cbc-transaction-notification-projector-v3"]
   cert_common_name = "cbc/cbc-transaction-notification-projector"
 }
 
 module "cbc_transaction_rater_projector" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [kafka_topic.RatingEvents_v3.name]
+  consume_topics   = [kafka_topic.rating_events_v3.name]
   consume_groups   = ["cbc.cbc-transaction-rater-projector-v1"]
   cert_common_name = "cbc/cbc-transaction-rater-projector"
 }
 
 module "cbc_sodexo_transaction_exporter" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [kafka_topic.RatingEvents_v3.name]
+  consume_topics   = [kafka_topic.rating_events_v3.name]
   consume_groups   = ["cbc.cbc-sodexo-transaction-exporter-v1"]
   cert_common_name = "cbc/cbc-sodexo-transaction-exporter"
 }
 
 module "cbc_retailer_projector" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [kafka_topic.RatingEvents_v3.name]
+  consume_topics   = [kafka_topic.rating_events_v3.name]
   consume_groups   = ["cbc.cbc-retailer-projector-v1"]
   cert_common_name = "cbc/cbc-retailer-projector"
 }
 
 module "cbc_transaction_notification_mapper" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [kafka_topic.RatingEvents_v3.name]
+  consume_topics   = [kafka_topic.rating_events_v3.name]
   consume_groups   = ["cbc.cbc-transaction-notification-mapper-v2"]
   cert_common_name = "cbc/cbc-transaction-notification-mapper"
 }
 
 module "cbc_network_projector" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [kafka_topic.RatingEvents_v3.name]
+  consume_topics   = [kafka_topic.rating_events_v3.name]
   consume_groups   = ["cbc.cbc-network-projector-v4"]
   cert_common_name = "cbc/cbc-network-projector"
 }
 
 module "cbc_retailer_api" {
   source           = "../../../modules/tls-app"
-  produce_topics   = [kafka_topic.RatingEvents_v3.name]
+  produce_topics   = [kafka_topic.rating_events_v3.name]
   cert_common_name = "cbc/cbc-retailer-api"
 }
