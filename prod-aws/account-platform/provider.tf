53a276d5e501d4430c646e7427093ce402dc2b05
Alex Ramin <aramin@uw.co.uk>
2024-04-25 15:23:40 +0200

    ACC-685 Add prod topics

---
+++ prod-aws/account-platform/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    kafka = {
+      source  = "Mongey/kafka"
+      version = ">= 0.7.0"
+    }
+  }
+}
+
+provider "kafka" {
+  bootstrap_servers = [
+    "broker-0.broker.account-platform.svc.cluster.aws:9092",
+    "broker-1.broker.account-platform.svc.cluster.aws:9092",
+    "broker-2.broker.account-platform.svc.cluster.aws:9092",
+    "broker-3.broker.account-platform.svc.cluster.aws:9092",
+    "broker-4.broker.account-platform.svc.cluster.aws:9092"
+  ]
+  tls_enabled = false
+}
