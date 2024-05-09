d168c9e827ee9415a98c5fcbfb5b12ac16d460b4
Alex Ramin <aramin@uw.co.uk>
2024-05-09 12:28:12 +0200

    ACC-684 add backend config

--- dev-aws/account-platform/backend.tf
+++ dev-aws/account-platform/backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket  = "uw-dev-account-platform-kafka-tf-state"
+    key     = "dev-aws/kafka-shared"
+    encrypt = true
+    region  = "eu-west-1"
+  }
 }
