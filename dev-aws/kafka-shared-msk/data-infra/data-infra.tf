5dc4fb29427c17f3e6e6f8b5bd0c8ccec5d144d8
Paulo Simão <102320181+paulosimao-ardanlabs@users.noreply.github.com>
2024-05-13 13:51:33 -0300

    test (di-kafka-source): Creating assets to allow reproducing PLA-1275 issue.

--- dev-aws/kafka-shared-msk/data-infra/data-infra.tf
+++ dev-aws/kafka-shared-msk/data-infra/data-infra.tf
@@ -49,6 +49,23 @@ resource "kafka_topic" "snowplow" {
   }
 }
 
+resource "kafka_topic" "test_pla1275" {
+  name               = "data-infra.uw.data-infra.test.pla1275"
+  replication_factor = 3
+  partitions         = 15
+  config = {
+    "remote.storage.enable" = "true"
+    # 5 days
+    "retention.ms" = "86400000"
+    # keep data in hot storage for 1 day
+    "local.retention.ms" = "86400000"
+    # allow max 1 MB for a message
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
 module "pubsub_bridge" {
   source           = "../../../modules/tls-app"
   produce_topics   = [kafka_topic.snowplow.name]
@@ -60,4 +77,11 @@ module "snowplow_braze_connector" {
   consume_topics   = [(kafka_topic.snowplow.name)]
   consume_groups   = ["data-infra.di-snowplow-braze-conn"]
   cert_common_name = "data-infra/di-snowplow-braze-connector"
+}
+
+module "kafka_source_tls" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [(kafka_topic.test_pla1275.name)]
+  consume_groups   = ["data-infra.di-kafka-source-tls"]
+  cert_common_name = "data-infra/di-kafka-source-tls"
 }
