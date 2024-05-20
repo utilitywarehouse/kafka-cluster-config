808cf17ab8a2208be63b6bd66985e4f2141c9f9a
Victoria Eyres <victoria.eyres@uw.co.uk>
2024-05-20 16:20:35 +0100

    feat(account-legacy.tf): added a new kafka topic for legacy account

---
+++ prod-aws/finance/account-legacy.tf
@@ -0,0 +1,11 @@
+# tflint-ignore: terraform_naming_convention
+resource "kafka_topic" "legacy-account-events-fwd" {
+  name               = "legacy.account.events.fwd"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    "retention.bytes" = "-1"
+    "retention.ms"    = "-1" 
+    "cleanup.policy"  = "delete"
+  }
+}
