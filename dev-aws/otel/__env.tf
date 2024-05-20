5692f83ce4f12e1db81c6ca92d5ff1798a18459c
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2024-05-20 18:29:57 +0300

    otel: add config for dedicated otel cluster

---
+++ dev-aws/otel/__env.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  backend "s3" {}
+
+  required_providers {
+    kafka = {
+      source  = "Mongey/kafka"
+      version = ">= 0.7.0"
+    }
+  }
+}
+
+provider "kafka" {
+  bootstrap_servers = [
+    "kafka-bitnami-0.kafka-bitnami-headless.pubsub.svc.cluster.aws:9092",
+    "kafka-bitnami-1.kafka-bitnami-headless.pubsub.svc.cluster.aws:9092",
+    "kafka-bitnami-2.kafka-bitnami-headless.pubsub.svc.cluster.aws:9092",
+  ]
+}
