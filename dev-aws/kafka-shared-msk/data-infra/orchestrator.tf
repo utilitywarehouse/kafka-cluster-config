e4f8097bc3f85867727acaa2bdfd8c3a0bb0362d
Catalin Ilea <cilea@utilitywarehouse.co.uk>
2024-05-21 13:24:44 +0300

    feat: added data-infra orchestrator resource to dev-aws/kafka-shared-msks (#311)

---
+++ dev-aws/kafka-shared-msk/data-infra/orchestrator.tf
@@ -0,0 +1,95 @@
+resource "kafka_topic" "e2e_proto" {
+  name               = "data-infra.e2e.proto"
+  replication_factor = 3
+  partitions         = 1
+  config = {
+    # this is a test, and we need minimum and non-durable resources
+    "remote.storage.enable" = "false"
+    # 1 day
+    "retention.ms" = "86400000"
+    # allow max 1 MB for a message
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+resource "kafka_topic" "e2e_json" {
+  name               = "data-infra.e2e.json"
+  replication_factor = 3
+  partitions         = 1
+  config = {
+    # this is a test, and we need minimum and non-durable resources
+    "remote.storage.enable" = "false"
+    # 1 day
+    "retention.ms" = "86400000"
+    # allow max 1 MB for a message
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+resource "kafka_topic" "e2e_multi" {
+  name               = "data-infra.e2e.multi"
+  replication_factor = 3
+  partitions         = 1
+  config = {
+    # this is a test, and we need minimum and non-durable resources
+    "remote.storage.enable" = "false"
+    # 1 day
+    "retention.ms" = "86400000"
+    # allow max 1 MB for a message
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+module "orchestrator" {
+  source         = "../../../modules/tls-app"
+  consume_topics = [kafka_topic.events_send.name]
+  consume_groups = [
+    "data-infra.di-orchestrator-job-dev-gcp",
+    "data-infra.di-orchestrator-job-dev-merit",
+    "data-infra.di-orchestrator-job-dev-aws"
+  ]
+  produce_topics = [
+    kafka_topic.e2e_proto.name,
+    kafka_topic.e2e_json.name
+  ]
+  cert_common_name = "data-infra/orchestrator"
+}
+
+module "kafka_source_proto" {
+  source         = "../../../modules/tls-app"
+  consume_topics = [kafka_topic.e2e_proto.name]
+  consume_groups = [
+    "data-infra.di-kafka-source-dev-gcp",
+    "data-infra.di-kafka-source-dev-merit",
+    "data-infra.di-kafka-source-dev-aws"
+  ]
+  cert_common_name = "data-infra/kafka-source"
+}
+
+module "kafka_source_json" {
+  source         = "../../../modules/tls-app"
+  consume_topics = [kafka_topic.e2e_json.name]
+  consume_groups = [
+    "data-infra.di-kafka-json-source-dev-gcp",
+    "data-infra.di-kafka-json-source-dev-merit",
+    "data-infra.di-kafka-json-source-dev-aws"
+  ]
+  cert_common_name = "data-infra/kafka-source-json"
+}
+
+module "kafka_source_multi" {
+  source         = "../../../modules/tls-app"
+  consume_topics = [kafka_topic.e2e_multi.name]
+  consume_groups = [
+    "data-infra.di-kafka-json-source-dev-gcp",
+    "data-infra.di-kafka-json-source-dev-merit",
+    "data-infra.di-kafka-json-source-dev-aws"
+  ]
+  cert_common_name = "data-infra/kafka-source-multi"
+}
