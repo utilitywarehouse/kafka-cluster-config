5f7c1a1983be1744d3b231856d1cf39bd21b9f4a
sbuliarca <sbuliarca@utilitywarehouse.co.uk>
2024-05-22 09:43:44 +0300

    otel: add config for dedicated prod kafka (#313)

---
+++ prod-aws/otel/otel.tf
@@ -0,0 +1,34 @@
+resource "kafka_topic" "otlp_spans" {
+  name               = "otel.otlp_spans"
+  replication_factor = 3
+  partitions         = 200
+  config = {
+    # retain 20GB on each partition
+    "retention.bytes" = "21474836480"
+    # keep data for 2 days
+    "retention.ms" = "172800000"
+    # allow max 128 MB for a message
+    "max.message.bytes" = "134217728"
+    # roll log at 3h max
+    "segment.ms" = "10800000"
+    # max log size of 250 MB
+    "segment.bytes"                  = "262144000"
+    "compression.type"               = "zstd"
+    "cleanup.policy"                 = "delete"
+    "unclean.leader.election.enable" = "false"
+  }
+}
+
+module "otel_collector" {
+  source           = "../../modules/tls-app"
+  produce_topics   = [kafka_topic.otlp_spans.name]
+  cert_common_name = "otel/collector"
+}
+
+module "tempo_distributor" {
+  source           = "../../modules/tls-app"
+  consume_topics   = [kafka_topic.otlp_spans.name]
+  consume_groups   = ["processor-tempo"]
+  cert_common_name = "otel/tempo-distributor"
+}
+
