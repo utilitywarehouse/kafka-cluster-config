da5880ddac09bae2e9eac7dd81ca906cd832020e
Victoria Eyres <victoria.eyres@uw.co.uk>
2024-05-29 08:36:55 +0100

    (finance/debt-payments-page.tf): added bq-deadletter-events topic

--- dev-aws/finance/debt-payments-page.tf
+++ dev-aws/finance/debt-payments-page.tf
@@ -9,3 +9,13 @@ resource "kafka_topic" "bq_staged_events" {
   }
 }
 
+resource "kafka_topic" "bq_deadletter_events" {
+  name               = "bq-deadletter-events"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    "retention.bytes" = "-1"
+    "retention.ms"    = "604800000"
+    "cleanup.policy"  = "delete"
+  }
+}
