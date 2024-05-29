b10d94ea921d0134829399a3b333252f8192c496
Victoria Eyres <victoria.eyres@uw.co.uk>
2024-05-29 09:43:29 +0100

    (finance/debt.tf): move debt related events fron debt payment page to debt.tf

--- dev-aws/finance/debt-payments-page.tf
+++ dev-aws/finance/debt-payments-page.tf
@@ -19,36 +19,3 @@ resource "kafka_topic" "bq_deadletter_events" {
     "cleanup.policy"  = "delete"
   }
 }
-
-resource "kafka_topic" "debt_payment_transaction" {
-  name               = "debt-payment-transaction"
-  replication_factor = 3
-  partitions         = 10
-  config = {
-    "retention.bytes" = "-1"
-    "retention.ms"    = "2592000000"
-    "cleanup.policy"  = "delete"
-  }
-}
-
-resource "kafka_topic" "overdue_balance_with_payments_diffs" {
-  name               = "overdue-balance-with-payments-diffs"
-  replication_factor = 3
-  partitions         = 10
-  config = {
-    "retention.bytes" = "-1"
-    "retention.ms"    = "2592000000"
-    "cleanup.policy"  = "delete"
-  }
-}
-
-resource "kafka_topic" "failed_payment_confirmation" {
-  name               = "failed-payment-confirmation"
-  replication_factor = 3
-  partitions         = 10
-  config = {
-    "retention.bytes" = "-1"
-    "retention.ms"    = "2592000000"
-    "cleanup.policy"  = "delete"
-  }
-}
