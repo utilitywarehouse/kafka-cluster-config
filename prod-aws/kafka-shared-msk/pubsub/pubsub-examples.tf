eb28baf1c22382dd3ab18060bc2030163526e2f5
sbuliarca <sbuliarca@utilitywarehouse.co.uk>
2024-05-31 16:36:28 +0300

    pubsub examples: reduce the number of partitions (#339)

--- prod-aws/kafka-shared-msk/pubsub/pubsub-examples.tf
+++ prod-aws/kafka-shared-msk/pubsub/pubsub-examples.tf
@@ -1,7 +1,7 @@
 resource "kafka_topic" "pubsub_examples" {
   name               = "pubsub.examples"
   replication_factor = 3
-  partitions         = 10
+  partitions         = 3
   config = {
     # retain 100MB on each partition
     "retention.bytes" = "104857600"
