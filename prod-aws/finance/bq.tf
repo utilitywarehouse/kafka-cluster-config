69110d141f79082672383f5b719eda04e7f37176
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2024-06-03 13:58:18 +0100

    chore(prod-aws/finance): create debt payment pages related topics

---
+++ prod-aws/finance/bq.tf
@@ -0,0 +1,21 @@
+resource "kafka_topic" "bq_staged_events" {
+  name               = "bq-staged-events"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    "retention.bytes" = "-1"
+    "retention.ms"    = "2592000000"
+    "cleanup.policy"  = "delete"
+  }
+}
+
+resource "kafka_topic" "bq_deadletter_events" {
+  name               = "bq-deadletter-events"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    "retention.bytes" = "-1"
+    "retention.ms"    = "2592000000"
+    "cleanup.policy"  = "delete"
+  }
+}
