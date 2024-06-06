e51c1992f36cd69105c52402710ead7664846888
Rob Crowe <nobby.crowe@gmail.com>
2024-06-06 14:46:53 +0100

    Grant iam-audit cerbos access

---
+++ dev-aws/kafka-shared-msk/iam/audit.tf
@@ -0,0 +1,7 @@
+# consumes Cerbos audit records & forwards into Audit Ingest API
+module "iam_audit_ingest_input_cerbos" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [(kafka_topic.iam_cerbos_audit_v1.name)]
+  consume_groups   = ["iam.audit-ingest-input-cerbos-v1"]
+  cert_common_name = "iam-audit/ingest-input-cerbos"
+}
