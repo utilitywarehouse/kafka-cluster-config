e25f2ac75002f2f34defc80089f40cb558f215f0
Rob Crowe <nobby.crowe@gmail.com>
2024-06-10 11:25:22 +0100

    ACL for Redpanda Connect to forward IAM audit records

--- dev-aws/kafka-shared-msk/iam/audit.tf
+++ dev-aws/kafka-shared-msk/iam/audit.tf
@@ -5,3 +5,10 @@ module "iam_audit_ingest_input_cerbos" {
   consume_groups   = ["iam.audit-ingest-input-cerbos-v1"]
   cert_common_name = "iam-audit/ingest-input-cerbos"
 }
+
+module "iam_audit_ingest_connect_cerbos" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [(kafka_topic.iam_cerbos_audit_v1.name)]
+  consume_groups   = ["iam.audit-ingest-connect-cerbos-v1"]
+  cert_common_name = "iam-audit/ingest-connect-cerbos"
+}
