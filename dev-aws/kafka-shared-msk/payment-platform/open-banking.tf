16b3976316efed99ec35e9c3971efcdcb056532f
CharlesKP <82518486+CharlesKP@users.noreply.github.com>
2024-06-12 11:13:24 +0200

    remove duplicate topic resources (#360)
    * remove duplicate topic resources
    
    * revert topics under modules
    
    ---------
    
    Co-authored-by: Charles Knighton-Pullin <cknightonpullin@uw.co.uk>

--- dev-aws/kafka-shared-msk/payment-platform/open-banking.tf
+++ dev-aws/kafka-shared-msk/payment-platform/open-banking.tf
@@ -66,23 +66,6 @@ resource "kafka_topic" "openbanking_deadletter_v1_internal_payment_methods" {
   }
 }
 
-resource "kafka_topic" "payment_v1_events" {
-  name               = "payment-platform.payment.v1.events"
-  replication_factor = 3
-  partitions         = 5
-  config = {
-    "compression.type" = "zstd"
-    "retention.bytes"  = "-1"
-    # Use tiered storage
-    "remote.storage.enable" = "true"
-    # keep data in hot storage for 2 days
-    "local.retention.ms" = "172800000"
-    # keep data for 30 days
-    "retention.ms"   = "2592000000"
-    "cleanup.policy" = "delete"
-  }
-}
-
 module "openbanking_apid" {
   source           = "../../../modules/tls-app"
   produce_topics   = [kafka_topic.openbanking_v1_internal_payments.name, kafka_topic.openbanking_v1_internal_payment_methods.name, kafka_topic.payment_v1_events.name]
