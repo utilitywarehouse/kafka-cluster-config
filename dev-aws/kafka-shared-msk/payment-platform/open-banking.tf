9c813793df968ef454a195b7a1c88314a3b1f343
Charles Knighton-Pullin <cknightonpullin@uw.co.uk>
2024-06-12 11:12:10 +0200

    revert topics under modules

--- dev-aws/kafka-shared-msk/payment-platform/open-banking.tf
+++ dev-aws/kafka-shared-msk/payment-platform/open-banking.tf
@@ -68,13 +68,13 @@ resource "kafka_topic" "openbanking_deadletter_v1_internal_payment_methods" {
 
 module "openbanking_apid" {
   source           = "../../../modules/tls-app"
-  produce_topics   = [kafka_topic.openbanking_v1_internal_payments.name, kafka_topic.openbanking_v1_internal_payment_methods.name]
+  produce_topics   = [kafka_topic.openbanking_v1_internal_payments.name, kafka_topic.openbanking_v1_internal_payment_methods.name, kafka_topic.payment_v1_events.name]
   cert_common_name = "payment-platform/openbanking-apid"
 }
 
 module "openbanking_consumerd" {
   source           = "../../../modules/tls-app"
-  produce_topics   = [kafka_topic.openbanking_deadletter_v1_internal_payments.name, kafka_topic.openbanking_deadletter_v1_internal_payment_methods.name]
+  produce_topics   = [kafka_topic.openbanking_deadletter_v1_internal_payments.name, kafka_topic.openbanking_deadletter_v1_internal_payment_methods.name, kafka_topic.payment_v1_events.name]
   consume_topics   = [kafka_topic.openbanking_v1_internal_payments.name, kafka_topic.openbanking_v1_internal_payment_methods.name]
   consume_groups   = ["payment-platform.openbanking_consumerd"]
   cert_common_name = "payment-platform/openbanking-consumerd"
