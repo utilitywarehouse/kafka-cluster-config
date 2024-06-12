89605109dc1eaa3d6cbd97606bd580fd67a27be5
Charles Knighton-Pullin <cknightonpullin@uw.co.uk>
2024-06-12 11:08:20 +0200

    remove duplicate topic resources

--- dev-aws/kafka-shared-msk/payment-platform/open-banking.tf
+++ dev-aws/kafka-shared-msk/payment-platform/open-banking.tf
@@ -66,32 +66,15 @@ resource "kafka_topic" "openbanking_deadletter_v1_internal_payment_methods" {
   }
 }
 
-resource "kafka_topic" "payment_v1_events" {
-  name               = "payment-platform.payment.v1.events"
-  replication_factor = 3
-  partitions         = 5
-  config = {
-    "compression.type" = "zstd"
-    "retention.bytes"  = "-1"
-    # Use tiered storage
-    "remote.storage.enable" = "true"
-    # keep data in hot storage for 2 days
-    "local.retention.ms" = "172800000"
-    # keep data for 30 days
-    "retention.ms"   = "2592000000"
-    "cleanup.policy" = "delete"
-  }
-}
-
 module "openbanking_apid" {
   source           = "../../../modules/tls-app"
-  produce_topics   = [kafka_topic.openbanking_v1_internal_payments.name, kafka_topic.openbanking_v1_internal_payment_methods.name, kafka_topic.payment_v1_events.name]
+  produce_topics   = [kafka_topic.openbanking_v1_internal_payments.name, kafka_topic.openbanking_v1_internal_payment_methods.name]
   cert_common_name = "payment-platform/openbanking-apid"
 }
 
 module "openbanking_consumerd" {
   source           = "../../../modules/tls-app"
-  produce_topics   = [kafka_topic.openbanking_deadletter_v1_internal_payments.name, kafka_topic.openbanking_deadletter_v1_internal_payment_methods.name, kafka_topic.payment_v1_events.name]
+  produce_topics   = [kafka_topic.openbanking_deadletter_v1_internal_payments.name, kafka_topic.openbanking_deadletter_v1_internal_payment_methods.name]
   consume_topics   = [kafka_topic.openbanking_v1_internal_payments.name, kafka_topic.openbanking_v1_internal_payment_methods.name]
   consume_groups   = ["payment-platform.openbanking_consumerd"]
   cert_common_name = "payment-platform/openbanking-consumerd"
