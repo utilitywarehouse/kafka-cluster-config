4e86b1e6ae4cbca6b8bd833868dd2b0627aed5f5
Adrian Ciura <mazxaxz@gmail.com>
2024-06-13 14:17:21 +0200

    added openbanking_crond module and added resources for payments in prod-aws (#368)
    * added openbanking_crond module and added resources for payments in prod-aws
    
    * formatting

--- prod-aws/kafka-shared-msk/payment-platform/open-banking.tf
+++ prod-aws/kafka-shared-msk/payment-platform/open-banking.tf
@@ -66,33 +66,27 @@ resource "kafka_topic" "openbanking_deadletter_v1_internal_payment_methods" {
   }
 }
 
-resource "kafka_topic" "payment_v1_events" {
-  name               = "payment-platform.payment.v1.events"
-  replication_factor = 3
-  partitions         = 15
-  config = {
-    "compression.type" = "zstd"
-    "retention.bytes"  = "3758096384"
-    # Use tiered storage
-    "remote.storage.enable" = "true"
-    # keep data in hot storage for 2 days
-    "local.retention.ms" = "172800000"
-    # keep data for 1 year
-    "retention.ms"   = "31557600000"
-    "cleanup.policy" = "delete"
-  }
-}
-
 module "openbanking_apid" {
-  source           = "../../../modules/tls-app"
-  produce_topics   = [kafka_topic.openbanking_v1_internal_payments.name, kafka_topic.openbanking_v1_internal_payment_methods.name, kafka_topic.payment_v1_events.name]
+  source = "../../../modules/tls-app"
+  produce_topics = [
+    kafka_topic.openbanking_v1_internal_payments.name,
+    kafka_topic.openbanking_v1_internal_payment_methods.name,
+    kafka_topic.payment_v1_events.name
+  ]
   cert_common_name = "payment-platform/openbanking-apid"
 }
 
 module "openbanking_consumerd" {
-  source           = "../../../modules/tls-app"
-  produce_topics   = [kafka_topic.openbanking_deadletter_v1_internal_payments.name, kafka_topic.openbanking_deadletter_v1_internal_payment_methods.name, kafka_topic.payment_v1_events.name]
-  consume_topics   = [kafka_topic.openbanking_v1_internal_payments.name, kafka_topic.openbanking_v1_internal_payment_methods.name]
+  source = "../../../modules/tls-app"
+  produce_topics = [
+    kafka_topic.openbanking_deadletter_v1_internal_payments.name,
+    kafka_topic.openbanking_deadletter_v1_internal_payment_methods.name,
+    kafka_topic.payment_v1_events.name
+  ]
+  consume_topics = [
+    kafka_topic.openbanking_v1_internal_payments.name,
+    kafka_topic.openbanking_v1_internal_payment_methods.name
+  ]
   consume_groups   = ["payment-platform.openbanking_consumerd"]
   cert_common_name = "payment-platform/openbanking-consumerd"
 }
