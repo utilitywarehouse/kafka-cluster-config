3dda08166e11c3cffd9fa8efac8b651ca5a1e195
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2024-06-21 11:04:06 +0100

    feat(fin-841): billing pipeline topics

--- prod-aws/billing/billing-pipeline.tf
+++ prod-aws/billing/billing-pipeline.tf
@@ -8,3 +8,36 @@ resource "kafka_topic" "fwf_load_failures" {
     "retention.ms"    = "604800000" # 7 days
   }
 }
+
+resource "kafka_topic" "bill_extracts_historic_model" {
+  name               = "bill-extracts-historic-model"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    "cleanup.policy"  = "delete"
+    "retention.bytes" = "-1"
+    "retention.ms"    = "604800000" # 7 days
+  }
+}
+
+resource "kafka_topic" "bills_deadletter" {
+  name               = "bills.deadletter"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    "cleanup.policy"  = "delete"
+    "retention.bytes" = "-1"
+    "retention.ms"    = "604800000" # 7 days
+  }
+}
+
+resource "kafka_topic" "billing_engine_events" {
+  name               = "billing-engine-events"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    "cleanup.policy"  = "delete"
+    "retention.bytes" = "-1"
+    "retention.ms"    = "-1"
+  }
+}
