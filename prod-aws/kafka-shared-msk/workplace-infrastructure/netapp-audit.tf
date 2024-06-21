d86e603f927f89cd3fa2dea1b88cee72565985a1
uw-dc <dcadden@uw.co.uk>
2024-06-21 13:00:34 +0100

    define topic for each NetApp virtual server

--- prod-aws/kafka-shared-msk/workplace-infrastructure/netapp-audit.tf
+++ prod-aws/kafka-shared-msk/workplace-infrastructure/netapp-audit.tf
@@ -1,5 +1,18 @@
-resource "kafka_topic" "workplace_infrastructure_netapp_audit_v1" {
-  name = "workplace-infrastructure.netapp-audit-v1"
+resource "kafka_topic" "workplace_infrastructure_netapp_audit_v1_cifs_svm_a" {
+  name = "workplace-infrastructure.netapp-audit-v1.cifs_svm_a"
+  replication_factor = 3
+  partitions = 10
+  config = {
+    "retention.bytes"   = "209715200"  # 200 MiB/Partition
+    "retention.ms"      = "86400000"   # 1 day
+    "max.message.bytes" = "8192"       # 8 kiB/Message
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+resource "kafka_topic" "workplace_infrastructure_netapp_audit_v1_cifs_svm_b" {
+  name = "workplace-infrastructure.netapp-audit-v1.cifs_svm_b"
   replication_factor = 3
   partitions = 10
   config = {
@@ -13,8 +26,16 @@ resource "kafka_topic" "workplace_infrastructure_netapp_audit_v1" {
 
 module "workplace_infrastructure_netapp_audit_publish_to_kafka" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [kafka_topic.workplace_infrastructure.netapp-audit-v1.name]
-  consume_groups   = ["workplace_infrastructure.netapp-audit-v1-consumer"]
-  produce_topics   = [kafka_topic.workplace_infrastructure.netapp-audit-v1.name]
+  consume_topics   = [
+                        kafka_topic.workplace_infrastructure.netapp-audit-v1.cifs_svm_a.name,
+                        kafka_topic.workplace_infrastructure.netapp-audit-v1.cifs_svm_b.name
+                    ]
+  consume_groups   = [
+                        "workplace_infrastructure.netapp-audit-v1-consumer"
+                    ]
+  produce_topics   = [
+                        kafka_topic.workplace_infrastructure.netapp-audit-v1.cifs_svm_a.name,
+                        kafka_topic.workplace_infrastructure.netapp-audit-v1.cifs_svm_b.name
+                    ]
   cert_common_name = "workplace-infrastructure/netapp_audit_publish_to_kafka"
 }
