800133b4d5e94207a85851108736b7c7c8b59cbc
Adrian Ciura <aciura@uw.co.uk>
2024-06-26 10:22:51 +0200

    add cbc integration topics to payment-platform on both prod-aws and dev-aws

--- prod-aws/kafka-shared-msk/payment-platform/payment.tf
+++ prod-aws/kafka-shared-msk/payment-platform/payment.tf
@@ -59,11 +59,29 @@ resource "kafka_topic" "payment_v1_public_events_pp_test" {
   }
 }
 
+resource "kafka_topic" "payment_v1_public_events_cbc_topup_v3" {
+  name               = "payment-platform.payments.v1.public.events.cbc_topup_v3"
+  replication_factor = 3
+  partitions         = 15
+  config = {
+    "compression.type" = "zstd"
+    "retention.bytes"  = "-1"
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data in hot storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data for 30 days
+    "retention.ms"   = "2592000000"
+    "cleanup.policy" = "delete"
+  }
+}
+
 module "payment_query_service_downstream" {
   source = "../../../modules/tls-app"
   produce_topics = [
-    kafka_topic.payment_v1_public_events_pp_test.name
+    kafka_topic.payment_v1_public_events_pp_test.name,
     # integration topics have to go there
+    kafka_topic.payment_v1_public_events_cbc_topup_v3.name
   ]
   consume_topics   = [kafka_topic.payment_v1_events.name]
   consume_groups   = ["payment-platform.payment_query_service_downstream"]
