ea4fdc310c794dfa5ae1c11ffb2ca026425c1aa8
Adrian Ciura <mazxaxz@gmail.com>
2024-06-26 11:25:08 +0200

    add cbc integration topics to payment-platform on both prod-aws and dev-aws (#387)
    * add cbc integration topics to payment-platform on both prod-aws and dev-aws
    
    * change plural of payments to singlar to be consistent

--- dev-aws/kafka-shared-msk/payment-platform/payment.tf
+++ dev-aws/kafka-shared-msk/payment-platform/payment.tf
@@ -43,9 +43,26 @@ module "payment_query_service" {
 # Downstream integrations
 # https://github.com/utilitywarehouse/kubernetes-manifests/blob/master/dev-merit/payment-platform/_configs/platform-integration.yaml
 resource "kafka_topic" "payment_v1_public_events_pp_test" {
-  name               = "payment-platform.payments.v1.public.events.pp_test"
+  name               = "payment-platform.payment.v1.public.events.pp_test"
   replication_factor = 3
   partitions         = 5
+  config = {
+    "compression.type" = "zstd"
+    "retention.bytes"  = "-1"
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data in hot storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data for 30 days
+    "retention.ms"   = "2592000000"
+    "cleanup.policy" = "delete"
+  }
+}
+
+resource "kafka_topic" "payment_v1_public_events_cbc_topup_v3" {
+  name               = "payment-platform.payment.v1.public.events.cbc_topup_v3"
+  replication_factor = 3
+  partitions         = 5 # limiting it to only 5 partitions for dev, can be increased if necessary
   config = {
     "compression.type" = "zstd"
     "retention.bytes"  = "-1"
@@ -62,8 +79,9 @@ resource "kafka_topic" "payment_v1_public_events_pp_test" {
 module "payment_query_service_downstream" {
   source = "../../../modules/tls-app"
   produce_topics = [
-    kafka_topic.payment_v1_public_events_pp_test.name
+    kafka_topic.payment_v1_public_events_pp_test.name,
     # integration topics have to go there
+    kafka_topic.payment_v1_public_events_cbc_topup_v3.name
   ]
   consume_topics   = [kafka_topic.payment_v1_events.name]
   consume_groups   = ["payment-platform.payment_query_service_downstream"]
