a72215ca8c2c81eda0ef0a8ff2b5ec30748935c0
Paulo Simão <102320181+paulosimao-ardanlabs@users.noreply.github.com>
2024-07-04 13:38:13 -0300

    feat: new orch topic for non std envelope (dev and prod) (#406)

--- dev-aws/kafka-shared-msk/data-infra/orchestrator.tf
+++ dev-aws/kafka-shared-msk/data-infra/orchestrator.tf
@@ -62,6 +62,22 @@ resource "kafka_topic" "e2e_multi_dpd" {
   }
 }
 
+resource "kafka_topic" "e2e_non_std_envelope" {
+  name               = "data-infra.e2e.non-std-envelope"
+  replication_factor = 3
+  partitions         = 1
+  config = {
+    # this is a test, and we need minimum and non-durable resources
+    "remote.storage.enable" = "false"
+    # 1 day
+    "retention.ms" = "86400000"
+    # allow max 1 MB for a message
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
 module "orchestrator" {
   source         = "../../../modules/tls-app"
   consume_topics = [kafka_topic.events_end.name]
@@ -120,4 +136,13 @@ module "kafka_source_multi_dpd" {
     "data-infra.di-kafka-source-multi-dpd-dev-aws"
   ]
   cert_common_name = "data-infra/kafka-source-multi-dpd"
+}
+
+module "kafka_source_non_std_envelope" {
+  source         = "../../../modules/tls-app"
+  consume_topics = [kafka_topic.e2e_non_std_envelope.name]
+  consume_groups = [
+    "data-infra.kafka-source-non-std-envelope-dev-gcp",
+  ]
+  cert_common_name = "data-infra/kafka-source-non-std-envelope"
 }
