1645df802177308c8c75fefddfb92e9f829b69b1
Martin Asquino <martin.asquino@gmail.com>
2024-07-05 08:36:24 +0100

    Change retention policy

--- dev-aws/kafka-shared-msk/cbc/cbc.tf
+++ dev-aws/kafka-shared-msk/cbc/cbc.tf
@@ -25,9 +25,9 @@ resource "kafka_topic" "data_product_events_v1" {
   config = {
     "remote.storage.enable" = "true"
     "retention.bytes"       = "-1"
-    "retention.ms"          = "-1"
-    "local.retention.ms"    = "3600000" # keep data locally for 1 hour
-    "max.message.bytes"     = "2097152" # allow max 2MB for a message
+    "retention.ms"          = "2629746000" # keep data for 1 month
+    "local.retention.ms"    = "18000000"   # keep data locally for 5 hours
+    "max.message.bytes"     = "2097152"    # allow max 2MB for a message
     "compression.type"      = "zstd"
     "cleanup.policy"        = "delete"
   }
