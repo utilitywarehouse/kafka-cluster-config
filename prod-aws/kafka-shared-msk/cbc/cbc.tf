78824ffc5a92f9b33a23228b4436bfc5d937b177
martskins <martskins@users.noreply.github.com>
2024-07-08 15:27:38 +0100

    Add data-infra topic and modules to prod (#412)

---
+++ prod-aws/kafka-shared-msk/cbc/cbc.tf
@@ -0,0 +1,31 @@
+resource "kafka_topic" "data_product_events_v1" {
+  name = "cbc.DataProductEvents"
+
+  replication_factor = 3
+  partitions         = 15
+
+  config = {
+    "remote.storage.enable" = "true"
+    "retention.bytes"       = "-1"
+    "retention.ms"          = "2629746000" # keep data for 1 month
+    "local.retention.ms"    = "18000000"   # keep data locally for 5 hours
+    "max.message.bytes"     = "2097152"    # allow max 2MB for a message
+    "compression.type"      = "zstd"
+    "cleanup.policy"        = "delete"
+  }
+}
+
+module "cbc_data_infra_adapter_consumer" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = []
+  consume_groups   = []
+  produce_topics   = [kafka_topic.data_product_events_v1.name]
+  cert_common_name = "cbc/cbc-data-infra-adapter-consumer"
+}
+
+module "cbc_data_infra_exporter" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [kafka_topic.data_product_events_v1.name, ]
+  consume_groups   = ["cbc.cbc-data-infra-exporter-v1"]
+  cert_common_name = "cbc/cbc-data-infra-exporter"
+}
