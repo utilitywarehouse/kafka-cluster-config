039616fadd475c2fbe90703a09a108806f951954
Daniel C <96059559+uw-dc@users.noreply.github.com>
2024-07-15 12:19:30 +0100

    topic names wrong; not sure how I managed that :( (#415)

--- prod-aws/kafka-shared-msk/workplace-infrastructure/netapp-audit.tf
+++ prod-aws/kafka-shared-msk/workplace-infrastructure/netapp-audit.tf
@@ -1,5 +1,5 @@
-resource "kafka_topic" "workplace_infrastructure_netapp_audit_v1_cifs_svm_a" {
-  name               = "workplace-infrastructure.netapp-audit-v1.cifs_svm_a"
+resource "kafka_topic" "workplace_infrastructure_netapp_audit_v1_svm_cifs_a" {
+  name               = "workplace-infrastructure.netapp-audit-v1.svm_cifs_a"
   replication_factor = 3
   partitions         = 10
   config = {
@@ -11,8 +11,8 @@ resource "kafka_topic" "workplace_infrastructure_netapp_audit_v1_cifs_svm_a" {
   }
 }
 
-resource "kafka_topic" "workplace_infrastructure_netapp_audit_v1_cifs_svm_b" {
-  name               = "workplace-infrastructure.netapp-audit-v1.cifs_svm_b"
+resource "kafka_topic" "workplace_infrastructure_netapp_audit_v1_svm_cifs_b" {
+  name               = "workplace-infrastructure.netapp-audit-v1.svm_cifs_b"
   replication_factor = 3
   partitions         = 10
   config = {
@@ -27,15 +27,15 @@ resource "kafka_topic" "workplace_infrastructure_netapp_audit_v1_cifs_svm_b" {
 module "workplace_infrastructure_netapp_audit_publish_to_kafka" {
   source = "../../../modules/tls-app"
   consume_topics = [
-    kafka_topic.workplace_infrastructure_netapp_audit_v1_cifs_svm_a.name,
-    kafka_topic.workplace_infrastructure_netapp_audit_v1_cifs_svm_b.name
+    kafka_topic.workplace_infrastructure_netapp_audit_v1_svm_cifs_a.name,
+    kafka_topic.workplace_infrastructure_netapp_audit_v1_svm_cifs_b.name
   ]
   consume_groups = [
     "workplace-infrastructure.netapp-audit-v1-consumer"
   ]
   produce_topics = [
-    kafka_topic.workplace_infrastructure_netapp_audit_v1_cifs_svm_a.name,
-    kafka_topic.workplace_infrastructure_netapp_audit_v1_cifs_svm_b.name
+    kafka_topic.workplace_infrastructure_netapp_audit_v1_svm_cifs_a.name,
+    kafka_topic.workplace_infrastructure_netapp_audit_v1_svm_cifs_b.name
   ]
   cert_common_name = "corp-netapp-audit/netapp-audit-publish-to-kafka"
 }
