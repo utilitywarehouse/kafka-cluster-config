306a61ba9a0a841a4480dedc7761d2c539c774e8
Vitor Faiante <vfaiante@uw.co.uk>
2024-07-25 22:24:48 +0200

    feat: add genesys eb events es indexer for cc dev-aws (#431)

--- dev-aws/kafka-shared-msk/contact-channels/contact-channels.tf
+++ dev-aws/kafka-shared-msk/contact-channels/contact-channels.tf
@@ -309,3 +309,11 @@ module "agent_state_service" {
   cert_common_name = "contact-channels/agent-state-service"
   consume_topics   = [kafka_topic.interactions_state_events.name]
 }
+
+# Consume from contact-channels.genesys_eb_events (ES Topic Indexer)
+module "genesys_eb_events_es_indexer" {
+  source           = "../../../modules/tls-app"
+  cert_common_name = "contact-channels/genesys-eb-events-indexer"
+  consume_topics   = [kafka_topic.genesys_eb_events.name]
+  consume_groups   = ["contact-channels.genesys-eb-events-indexer"]
+}
