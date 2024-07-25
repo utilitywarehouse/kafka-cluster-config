6a594ad04017274059879c9853406c1a6a30cd2a
Vitor Faiante <vfaiante@uw.co.uk>
2024-07-25 18:53:58 +0100

    chore: re-save

--- dev-aws/kafka-shared-msk/contact-channels/contact-channels.tf
+++ dev-aws/kafka-shared-msk/contact-channels/contact-channels.tf
@@ -310,7 +310,6 @@ module "agent_state_service" {
   consume_topics   = [kafka_topic.interactions_state_events.name]
 }
 
-contact-channels/genesys-eb-events-indexer
 # Consume from contact-channels.genesys_eb_events (ES Topic Indexer)
 module "genesys_eb_events_es_indexer" {
   source           = "../../../modules/tls-app"
