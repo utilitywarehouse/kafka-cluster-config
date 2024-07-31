d0e00d681f9104819a693665e14c3a96eca66f6b
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2024-07-31 10:29:08 +0300

    msk prod: Deny topic deletes for TF applier and Kafka-UI so that we can enable deletes in prod.

--- prod-aws/kafka-shared-msk/pubsub/kafka-ui.tf
+++ prod-aws/kafka-shared-msk/pubsub/kafka-ui.tf
@@ -7,6 +7,16 @@ resource "kafka_acl" "kafka_ui_topic" {
   acl_permission_type = "Allow"
 }
 
+# Deny topic delete to kafka-ui to prevent data loss. Only tf-kafka-config-admin will be able to delete topics in prod manually
+resource "kafka_acl" "kafka_ui_topic_deny_delete" {
+  resource_name       = "*"
+  resource_type       = "Topic"
+  acl_principal       = "User:CN=pubsub/kafka-ui"
+  acl_host            = "*"
+  acl_operation       = "Delete"
+  acl_permission_type = "Deny"
+}
+
 resource "kafka_acl" "kafka_ui_group" {
   resource_name       = "*"
   resource_type       = "Group"
--- prod-aws/kafka-shared-msk/pubsub/tf-applier.tf
+++ prod-aws/kafka-shared-msk/pubsub/tf-applier.tf
@@ -7,6 +7,16 @@ resource "kafka_acl" "tf_applier_topic" {
   acl_permission_type = "Allow"
 }
 
+# Deny topic delete to tf-applier to prevent data loss. Only tf-kafka-config-admin will be able to delete topics in prod manually.
+resource "kafka_acl" "tf_applier_topic_deny_delete" {
+  resource_name       = "*"
+  resource_type       = "Topic"
+  acl_principal       = "User:CN=pubsub/tf-applier"
+  acl_host            = "*"
+  acl_operation       = "Delete"
+  acl_permission_type = "Deny"
+}
+
 resource "kafka_acl" "tf_applier_group" {
   resource_name       = "*"
   resource_type       = "Group"
