f6309c8cc960ca63670c140bdfe910c0b4ef2809
James Tait <32527884+Jarota@users.noreply.github.com>
2024-08-08 14:56:22 +0100

    account-platform: Add import block for existing topic (#453)

--- prod-aws/account-platform/topics.tf
+++ prod-aws/account-platform/topics.tf
@@ -1,3 +1,8 @@
+import {
+  to = kafka_topic.public_account_events
+  id = "public.account.events"
+}
+
 resource "kafka_topic" "public_account_events" {
   config             = {}
   name               = "public.account.events"
