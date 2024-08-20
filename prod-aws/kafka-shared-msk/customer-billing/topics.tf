be0b92cc3f587bd19c5a189b73f2764569306c0b
jturner-uw <104352984+jturner-uw@users.noreply.github.com>
2024-08-20 11:21:14 +0100

    feat: created invoice produced topics (#462)
    * created invoice produced topics
    
    * added modules

--- prod-aws/kafka-shared-msk/customer-billing/topics.tf
+++ prod-aws/kafka-shared-msk/customer-billing/topics.tf
@@ -82,3 +82,37 @@ resource "kafka_topic" "invoice_ready_notifier_deadletter" {
     "cleanup.policy"    = "delete"
   }
 }
+
+resource "kafka_topic" "bex_invoice_api" {
+  name               = "bex.internal.bex_invoice_produced"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    "remote.storage.enable" = "true"
+    # keep data in hot storage for 1 day
+    "local.retention.ms" = "86400000"
+    # keep data for 7 days
+    "retention.ms" = "604800000"
+    # allow max 100 MB for a message
+    "max.message.bytes" = "104857600"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+resource "kafka_topic" "bex_legacy_invoice_api" {
+  name               = "bex.internal.exstream_invoice_produced"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    "remote.storage.enable" = "true"
+    # keep data in hot storage for 1 day
+    "local.retention.ms" = "86400000"
+    # keep data for 7 days
+    "retention.ms" = "604800000"
+    # allow max 100 MB for a message
+    "max.message.bytes" = "104857600"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
