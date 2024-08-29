bf84c2d472d4e9ddc96939172493fabb081ce741
Tomasz Ginszt <63448175+tginszt@users.noreply.github.com>
2024-08-29 15:42:55 +0200

    FPAY-1484: tx-log-writer msk (#473)

---
+++ dev-aws/kafka-shared-msk/payment-platform/payment-enriched-txs.tf
@@ -0,0 +1,26 @@
+resource "kafka_topic" "payment_enriched_txs" {
+  name               = "payment.enriched-txs"
+  replication_factor = 3
+  partitions         = 5
+  config = {
+    "compression.type" = "zstd"
+    "retention.bytes"  = "-1"
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data in hot storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data for 30 days
+    "retention.ms"   = "2592000000"
+    "cleanup.policy" = "delete"
+  }
+}
+
+module "payment_tx_log_writer" {
+  source = "../../../modules/tls-app"
+  consume_topics = [
+    kafka_topic.payment_enriched_txs.name,
+    kafka_topic.notifications_tx.name,
+  ]
+  consume_groups   = ["payment-platform.payment-tx-log-writer"]
+  cert_common_name = "payment-platform/payment-tx-log-writer"
+}
