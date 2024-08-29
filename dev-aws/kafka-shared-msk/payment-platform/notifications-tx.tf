a6d7e2f93a0ff310f0e8de8ea5e8591f6b0a7768
tginszt <tom.ginszt@gmail.com>
2024-08-29 15:30:43 +0200

    FPAY-1484: tx-log-writer msk

---
+++ dev-aws/kafka-shared-msk/payment-platform/notifications-tx.tf
@@ -0,0 +1,16 @@
+resource "kafka_topic" "notifications_tx" {
+  name               = "notifications.tx"
+  replication_factor = 3
+  partitions         = 5
+  config = {
+    "compression.type" = "zstd"
+    "retention.bytes"  = "-1"
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data in hot storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data for 30 days
+    "retention.ms"   = "2592000000"
+    "cleanup.policy" = "delete"
+  }
+}
