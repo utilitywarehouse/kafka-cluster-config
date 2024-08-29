392d4c5d4f9aa500feae7e2f8188d8e99dd62581
Alexander Ramin <aramin@uw.co.uk>
2024-08-29 11:27:07 +0200

    Add compaction for topic (#468)
    * Add compaction for topic
    
    * add topic
    
    ---------
    
    Co-authored-by: Denis Nikulin <denisnikulin@gmail.com>

--- prod-aws/account-platform/topics.tf
+++ prod-aws/account-platform/topics.tf
@@ -3,9 +3,24 @@ import {
   id = "public.account.events"
 }
 
+import {
+  to = kafka_topic.account_exceptions_events
+  id = "account.exceptions.events"
+}
+
 resource "kafka_topic" "public_account_events" {
   config             = {}
   name               = "public.account.events"
+  partitions         = 15
+  replication_factor = 3
+}
+
+resource "kafka_topic" "account_exceptions_events" {
+  config = {
+    "compression.type" = "snappy"
+    "cleanup.policy"   = "compact"
+  }
+  name               = "account.exceptions.events"
   partitions         = 15
   replication_factor = 3
 }
