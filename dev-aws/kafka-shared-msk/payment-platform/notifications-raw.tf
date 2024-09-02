bc1ba6f1231ebd63729f3ae24a1b7ae6f5c3297f
Daniel Copaciu <dani.copaciu@gmail.com>
2024-09-02 15:02:58 +0300

    Fix formatting

--- dev-aws/kafka-shared-msk/payment-platform/notifications-raw.tf
+++ dev-aws/kafka-shared-msk/payment-platform/notifications-raw.tf
@@ -4,14 +4,14 @@ resource "kafka_topic" "notifications_worldpay_raw" {
   partitions         = 15
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "-1"
+    "retention.bytes"  = "-1"
     # Use tiered storage
     "remote.storage.enable" = "true"
     # keep data in hot storage for 2 days
     "local.retention.ms" = "172800000"
     # keep data for 30 days
-    "retention.ms"     = "2592000000"
-    "cleanup.policy"   = "delete"
+    "retention.ms"   = "2592000000"
+    "cleanup.policy" = "delete"
   }
 }
 
@@ -24,7 +24,7 @@ module "payment_worldpay_notification_listener" {
     kafka_topic.notifications_worldpay_raw.name,
     kafka_topic.notifications_tx.name,
   ]
-  consume_groups = ["payment-platform.payment-worldpay-notification-listener"]
+  consume_groups   = ["payment-platform.payment-worldpay-notification-listener"]
   cert_common_name = "payment-platform/payment-worldpay-notification-listener"
 }
 
@@ -34,14 +34,14 @@ resource "kafka_topic" "notifications_stripe_raw" {
   partitions         = 15
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "-1"
+    "retention.bytes"  = "-1"
     # Use tiered storage
     "remote.storage.enable" = "true"
     # keep data in hot storage for 2 days
     "local.retention.ms" = "172800000"
     # keep data for 30 days
-    "retention.ms"     = "2592000000"
-    "cleanup.policy"   = "delete"
+    "retention.ms"   = "2592000000"
+    "cleanup.policy" = "delete"
   }
 }
 
@@ -54,7 +54,7 @@ module "payment_stripe_notification_listener" {
     kafka_topic.notifications_stripe_raw.name,
     kafka_topic.notifications_tx.name,
   ]
-  consume_groups = ["payment-platform.payment-stripe-notification-listener"]
+  consume_groups   = ["payment-platform.payment-stripe-notification-listener"]
   cert_common_name = "payment-platform/payment-stripe-notification-listener"
 }
 
--- dev-aws/kafka-shared-msk/payment-platform/notifications-tx.tf
+++ dev-aws/kafka-shared-msk/payment-platform/notifications-tx.tf
@@ -4,14 +4,14 @@ resource "kafka_topic" "notifications_tx" {
   partitions         = 5
   config = {
     "compression.type" = "zstd"
-    "retention.bytes" = "-1"
+    "retention.bytes"  = "-1"
     # Use tiered storage
     "remote.storage.enable" = "true"
     # keep data in hot storage for 2 days
     "local.retention.ms" = "172800000"
     # keep data for 30 days
-    "retention.ms"     = "2592000000"
-    "cleanup.policy"   = "delete"
+    "retention.ms"   = "2592000000"
+    "cleanup.policy" = "delete"
   }
 }
 
@@ -20,7 +20,7 @@ module "payment_gateway_report_generator" {
   consume_topics = [
     kafka_topic.notifications_tx.name,
   ]
-  consume_groups = ["payment-platform.payment-gateway-report-generator"]
+  consume_groups   = ["payment-platform.payment-gateway-report-generator"]
   cert_common_name = "payment-platform/payment-gateway-report-generator"
 }
 
@@ -29,6 +29,6 @@ module "payment_gateway_reconciler" {
   consume_topics = [
     kafka_topic.notifications_tx.name,
   ]
-  consume_groups = ["payment-platform.payment-gateway-reconciler-for-reconciler-db"]
+  consume_groups   = ["payment-platform.payment-gateway-reconciler-for-reconciler-db"]
   cert_common_name = "payment-platform/payment-gateway-reconciler"
 }
