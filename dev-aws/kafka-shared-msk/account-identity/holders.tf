e441c1ea5ff41e844d5bdbeef032c35c75f56824
Mateusz Tracz <mtracz@uw.co.uk>
2024-09-05 14:45:23 +0100

    Naming

--- dev-aws/kafka-shared-msk/account-identity/holders.tf
+++ dev-aws/kafka-shared-msk/account-identity/holders.tf
@@ -1,4 +1,4 @@
-resource "kafka_topic" "account-identity_Legacy-account-holder-events-compacted" {
+resource "kafka_topic" "account_identity_legacy_account_holder_events_compacted" {
   name               = "account-identity.legacy.account.holder.events.compacted"
   replication_factor = 3
   partitions         = 15
@@ -10,9 +10,9 @@ resource "kafka_topic" "account-identity_Legacy-account-holder-events-compacted"
   }
 }
 
-module "account_identity_update-holders" {
+module "account_identity_update_holders" {
   source           = "../../../modules/tls-app"
-  consume_topics   = [(kafka_topic.account-identity_Legacy-account-holder-events-compacted.name)]
+  consume_topics   = [(kafka_topic.account_identity_legacy_account_holder_events_compacted.name)]
   consume_groups   = ["account-identity.update-holders-projector"]
   cert_common_name = "account-identity/update-holders-projector"
 }
