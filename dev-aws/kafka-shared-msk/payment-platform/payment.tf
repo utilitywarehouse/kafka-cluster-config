a5b705507f9f38e5227ac8077b4e635133307835
Rafal Dekarz <rafdekar@gmail.com>
2024-09-10 11:41:04 +0200

    Adding msk tls module for payment-method.v1.events for payment-api and es indexer (#499)
    * Adding msk tls module for payment-method.v1.events for payment-api and es indexer
    
    * fix

--- dev-aws/kafka-shared-msk/payment-platform/payment.tf
+++ dev-aws/kafka-shared-msk/payment-platform/payment.tf
@@ -15,6 +15,23 @@ resource "kafka_topic" "payment_v1_events" {
   }
 }
 
+resource "kafka_topic" "payment_method_v1_events" {
+  name               = "payment-platform.payment-method.v1.events"
+  replication_factor = 3
+  partitions         = 15
+  config = {
+    "compression.type" = "zstd"
+    "retention.bytes"  = "-1"
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data in hot storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data for 30 days
+    "retention.ms"   = "2592000000"
+    "cleanup.policy" = "delete"
+  }
+}
+
 resource "kafka_topic" "payment_deadletter_v1_events" {
   name               = "payment-platform.payment-deadletter.v1.events"
   replication_factor = 3
@@ -128,6 +145,14 @@ module "es-indexer-payment-v1-events" {
   consume_groups   = ["payment-platform.es-indexer-payment.v1.events"]
   consume_topics   = [kafka_topic.payment_v1_events.name]
   cert_common_name = "payment-platform/es-indexer-payment-v1-events"
+}
+
+# tflint-ignore: terraform_naming_convention
+module "es-indexer-payment-method-v1-events" {
+  source           = "../../../modules/tls-app"
+  consume_groups   = ["payment-platform.es-indexer-payment-method.v1.events"]
+  consume_topics   = [kafka_topic.payment_method_v1_events.name]
+  cert_common_name = "payment-platform/es-indexer-payment-method-v1-events"
 }
 
 module "cbc_topup_processor" {
