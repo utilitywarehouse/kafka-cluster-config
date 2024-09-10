d45498d9a6606e8863a22082f8d7f4e9da0a01ad
Victoria Eyres <victoria.eyres@uw.co.uk>
2024-09-10 11:05:40 +0100

    chore(FIN-1204): create topic for transactions-auditor-diff and module

---
+++ dev-aws/kafka-shared-msk/billing/transactions-auditor-diff-events.tf
@@ -0,0 +1,28 @@
+resource "kafka_topic" "transactions_auditor_diff_events" {
+  name               = "billing.transactions-auditor-diff.events"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    "compression.type" = "zstd"
+    # 805 GB
+    "retention.bytes" = "805306368000"
+    # 105 MB
+    "max.message.bytes" = "104857600"
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data in hot storage for 2 days
+    "local.retention.ms" = "172800000"
+    "retention.ms"       = "-1"
+    "cleanup.policy"     = "delete"
+  }
+}
+
+module "transactions-auditor-api" {
+  source = "../../../modules/tls-app"
+  // consume_topics = [ index? ]
+  produce_topics = [
+    kafka_topic.transactions-auditor-diff.events.name,
+  ]
+  // consume_groups   = ["billing.finance-bigquery-connector"]
+  cert_common_name = "billing/finance-bigquery-connector"
+}
