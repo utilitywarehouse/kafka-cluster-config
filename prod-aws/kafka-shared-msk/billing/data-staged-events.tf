24e5c4679ae0da23369c1ad0227b645f71fabf36
sbuliarca <sbuliarca@utilitywarehouse.co.uk>
2024-09-10 12:27:49 +0300

    DENA-901: billing: throttle finance_tx_log_staging_connector (#500)
    * billing: throttle finance_tx_log_staging_connector
    
    * format

--- prod-aws/kafka-shared-msk/billing/data-staged-events.tf
+++ prod-aws/kafka-shared-msk/billing/data-staged-events.tf
@@ -54,7 +54,9 @@ module "finance_tx_log_staging_connector" {
   produce_topics = [
     kafka_topic.data_staged_events_finance.name,
   ]
-  cert_common_name = "billing/finance-tx-log-staging-connector"
+  #  limit to 150 KB per broker, as the consumer doesn't keep up with the producer and the producer is loading the brokers CPU when not throttled.
+  producer_byte_rate = "153600"
+  cert_common_name   = "billing/finance-tx-log-staging-connector"
 }
 
 module "historical_bigquery_connector" {
