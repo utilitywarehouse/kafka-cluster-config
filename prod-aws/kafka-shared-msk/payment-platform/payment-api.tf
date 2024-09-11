0ac4389047cfcd2aad45fe455577e9bb059c38c8
Rafal Dekarz <rafdekar@gmail.com>
2024-09-11 11:25:26 +0200

    added new consumer for payment method events (#505)

--- prod-aws/kafka-shared-msk/payment-platform/payment-api.tf
+++ prod-aws/kafka-shared-msk/payment-platform/payment-api.tf
@@ -5,3 +5,11 @@ module "payment_api" {
   ]
   cert_common_name = "payment-platform/payment-api"
 }
+
+module "payment_api_consumer_payment_method_cancelled" {
+  source = "../../../modules/tls-app"
+  produce_topics = [
+    kafka_topic.payment_method_v1_events.name,
+  ]
+  cert_common_name = "payment-platform/payment-api-consumer-payment-method-cancelled"
+}
