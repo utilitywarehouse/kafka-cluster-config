6b41e7e08c6e361ab5be36e0e4066785d685af09
Paulo Simão <102320181+paulosimao-ardanlabs@users.noreply.github.com>
2024-09-13 13:45:35 -0300

    feat: PLA-1438: creating infra for eqdb-loader impl.

---
+++ prod-aws/kafka-shared-msk/data-infra/eqdb-loader.tf
@@ -0,0 +1,34 @@
+resource "kafka_topic" "di_eqdbloader_scheduler" {
+  name               = "data-infra.eqdb-loader.schedule"
+  replication_factor = 3
+  partitions         = 1
+  config = {
+    "remote.storage.enable" = "false"
+    # 1 day
+    "local.retention.ms" = "86400000"
+    # allow max 1 MB for a message
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+module "di_eqdbloader_scheduler" {
+  source = "../../../modules/tls-app"
+  produce_topics = [
+    kafka_topic.di_eqdbloader_scheduler.name,
+  ]
+  cert_common_name = "data-infra/di-eqdbloader-scheduler"
+}
+
+module "di_eqdbloader_writer" {
+  source = "../../../modules/tls-app"
+  consume_topics = [
+    kafka_topic.di_eqdbloader_scheduler.name,
+  ]
+  consume_groups = [
+    "data-infra.di-eqdbloader-writer",
+  ]
+
+  cert_common_name = "data-infra/di-eqdbloader-writer"
+}
