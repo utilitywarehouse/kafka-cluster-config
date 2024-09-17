54119f4eb1dbb62b7e24e3503c45c9d6dca5fd83
Mateusz Tracz <mtracz@uw.co.uk>
2024-09-17 14:32:08 +0100

    Legacy account tls apps

--- dev-aws/kafka-shared-msk/account-identity/legacy_account.tf
+++ dev-aws/kafka-shared-msk/account-identity/legacy_account.tf
@@ -201,3 +201,40 @@ module "account_identity_legacy_to_anonymize_relay" {
   produce_topics   = [kafka_topic.account_identity_to_anonymize_events.name]
   cert_common_name = "account-platform/legacy_to_anonymize_relay"
 }
+
+module "account_identity_legacy_account_differ" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [kafka_topic.account_identity_legacy_account_eqdb_events.name, kafka_topic.account_identity_legacy_account_changelog_events.name]
+  consume_groups   = ["account-identity.legacy-account-differ"]
+  produce_topics   = [] # also needs bill analytics topic
+  cert_common_name = "account-platform/legacy_account_differ"
+}
+
+module "account_identity_eqdb_loader" {
+  source           = "../../../modules/tls-app"
+  produce_topics   = [kafka_topic.account_identity_legacy_account_eqdb_events.name]
+  cert_common_name = "account-platform/eqdb_loader"
+}
+
+module "account_identity_legacy_account_mux" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [kafka_topic.account_identity_internal_legacy_account_events.name]
+  consume_groups   = ["account-identity.legacy-account-mux"]
+  produce_topics   = [kafka_topic.account_identity_legacy_account_events.name, kafka_topic.account_identity_legacy_account_created_in_bill_events.name]
+  cert_common_name = "account-platform/legacy_account_mux"
+}
+
+module "account_identity_legacy_account_projector" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [kafka_topic.account_identity_legacy_account_changelog_events.name, kafka_topic.account_identity_legacy_account_events.name]
+  consume_groups   = ["account-identity.legacy-account-projector"]
+  cert_common_name = "account-platform/legacy_account_projector"
+}
+
+module "account_identity_legacy_account_translator" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [kafka_topic.account_identity_legacy_account_changelog_events.name]
+  produce_topics   = [kafka_topic.account_identity_internal_legacy_account_events.name, kafka_topic.account_identity_legacy_account_events_private.name]
+  consume_groups   = ["account-identity.legacy-account-translator"]
+  cert_common_name = "account-platform/legacy_account_translator"
+}
