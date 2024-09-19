d74459614716319cecaf11d1d6bf080b4351de73
Matt Bartlett <matt-bartlett@users.noreply.github.com>
2024-09-19 16:24:38 +0100

    [dev-aws/kafka-shared-msk] Fix cert common name (#537)

--- dev-aws/kafka-shared-msk/customer-support/es-topic-indexer.tf
+++ dev-aws/kafka-shared-msk/customer-support/es-topic-indexer.tf
@@ -1,6 +1,6 @@
 module "es_topic_indexer" {
   source           = "../../../modules/tls-app"
-  cert_common_name = "crm/topic-indexer-msk"
+  cert_common_name = "crm/msk-topic-indexer"
   consume_topics   = [(kafka_topic.vulnerability_v7.name)]
   consume_groups   = ["customer-support.topic-indexer"]
 }
