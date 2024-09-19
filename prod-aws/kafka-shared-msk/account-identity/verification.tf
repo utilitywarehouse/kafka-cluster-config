80fad5004f218a08b0a69eec6eea42646e52d257
Mateusz Tracz <matiqing@gmail.com>
2024-09-19 10:27:53 +0100

    Bootstrap kafka msk for account identity + define verification topic & apps (#534)
    * Bootstrap kafka msk for account identity
    
    * Add verification topic & apps
    
    * Prefix consume group

---
+++ prod-aws/kafka-shared-msk/account-identity/verification.tf
@@ -0,0 +1,34 @@
+module "account_identity_verification_api" {
+  source           = "../../../modules/tls-app"
+  produce_topics   = [kafka_topic.account_identity_verification.name]
+  cert_common_name = "auth-customer/verification-api"
+}
+
+module "account_identity_verification_processor" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [kafka_topic.account_identity_verification.name]
+  consume_groups   = ["account-identity.verification-processor"]
+  cert_common_name = "auth-customer/verification-processor"
+}
+
+module "account_identity_verification_indexer" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [kafka_topic.account_identity_verification.name]
+  consume_groups   = ["account-identity.ai-verification-events-v1"]
+  cert_common_name = "account-platform/ai-verification-events-v1-indexer"
+}
+
+resource "kafka_topic" "account_identity_verification" {
+  config = {
+    "compression.type" = "zstd"
+    # keep data in hot storage for 1 day
+    "local.retention.ms" = "86400000"
+    # enable remote storage
+    "remote.storage.enable" = "true"
+    # retain data for 7 days
+    "retention.ms" = "604800000"
+  }
+  name               = "account-identity.verification.events.v1"
+  partitions         = 3
+  replication_factor = 3
+}
