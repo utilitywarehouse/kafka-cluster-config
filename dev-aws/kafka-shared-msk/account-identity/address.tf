9d645c0a674185c0bd33ae6fb58c0c059dd625d1
Mateusz Tracz <mtracz@uw.co.uk>
2024-09-26 10:23:06 +0100

    Add hot storage

--- dev-aws/kafka-shared-msk/account-identity/address.tf
+++ dev-aws/kafka-shared-msk/account-identity/address.tf
@@ -5,8 +5,11 @@ import {
 
 resource "kafka_topic" "account_identity_address_lookup_analytics_v1" {
   config = {
-
     "compression.type" = "zstd"
+    # keep data in hot storage for 1 day
+    "local.retention.ms" = "86400000"
+    # enable remote storage
+    "remote.storage.enable" = "true"
     # keep data for 7 days
     "retention.ms" = "604800000"
   }
