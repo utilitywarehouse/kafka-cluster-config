e244b897ec37dff335dab1a6152bdcb32917038d
Mateusz Tracz <matiqing@gmail.com>
2024-09-26 10:27:04 +0100

    Add address lookup analytics topic and consumer (#562)
    * Add address lookup analytics topic and consumer
    
    * Add hot storage

---
+++ dev-aws/kafka-shared-msk/account-identity/address.tf
@@ -0,0 +1,19 @@
+import {
+  to = kafka_topic.account_identity_address_lookup_analytics_v1
+  id = "account-identity.address.lookup.analytics.v1"
+}
+
+resource "kafka_topic" "account_identity_address_lookup_analytics_v1" {
+  config = {
+    "compression.type" = "zstd"
+    # keep data in hot storage for 1 day
+    "local.retention.ms" = "86400000"
+    # enable remote storage
+    "remote.storage.enable" = "true"
+    # keep data for 7 days
+    "retention.ms" = "604800000"
+  }
+  name               = "account-identity.address.lookup.analytics.v1"
+  partitions         = 15
+  replication_factor = 3
+}
