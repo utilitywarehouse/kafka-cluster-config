60d6b94b1717f5a1732ffe238f2827797ac39647
George Kimberley <gkimbles@gmail.com>
2024-09-30 12:34:16 +0100

    setup kafka topics

---
+++ prod-aws/kafka-shared-msk/customer-support/es-topic-indexer.tf
@@ -0,0 +1,6 @@
+module "es_topic_indexer" {
+  source           = "../../../modules/tls-app"
+  cert_common_name = "crm/msk-topic-indexer"
+  consume_topics   = [(kafka_topic.vulnerability_v6.name)]
+  consume_groups   = ["customer-support.topic-indexer"]
+}
