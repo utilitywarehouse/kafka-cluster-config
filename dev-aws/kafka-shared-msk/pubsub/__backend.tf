6ef9f1b3d4bbae54be5dbf00be91ab1991cc1983
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2024-10-03 18:00:18 +0300

    Checking a failure in CI

--- dev-aws/kafka-shared-msk/pubsub/__backend.tf
+++ dev-aws/kafka-shared-msk/pubsub/__backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "uw-dev-pubsub-tf-applier-state"
-    key     = "dev-aws/kafka-shared-msk-pubsub"
+    key     = "dev-aws/kafka-shared-msk-should-fail"
     region  = "eu-west-1"
     encrypt = true
   }
