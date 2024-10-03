ad9880a663f69af376507424a070d4640bbe794c
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2024-10-03 18:04:40 +0300

    Revert "Checking a failure in CI"
    This reverts commit 6ef9f1b3d4bbae54be5dbf00be91ab1991cc1983.

--- dev-aws/kafka-shared-msk/pubsub/__backend.tf
+++ dev-aws/kafka-shared-msk/pubsub/__backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "uw-dev-pubsub-tf-applier-state"
-    key     = "dev-aws/kafka-shared-msk-should-fail"
+    key     = "dev-aws/kafka-shared-msk-pubsub"
     region  = "eu-west-1"
     encrypt = true
   }
