0fd8810e800119e3ce1abc4d20a934aeb780dd20
Rafal Dekarz <rafdekar@gmail.com>
2024-10-18 12:16:11 +0200

    added new pq service topics (#632)

--- prod-aws/kafka-shared-msk/payment-platform/payment.tf
+++ prod-aws/kafka-shared-msk/payment-platform/payment.tf
@@ -49,10 +49,27 @@ resource "kafka_topic" "payment_deadletter_v1_events" {
   }
 }
 
+resource "kafka_topic" "payment_method_deadletter_v1_events" {
+  name               = "payment-platform.payment-method-deadletter.v1.events"
+  replication_factor = 3
+  partitions         = 5 # we don't need more than 5 partitions for deadlettered events
+  config = {
+    "compression.type" = "zstd"
+    "retention.bytes"  = "-1"
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data in hot storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data for 30 days
+    "retention.ms"   = "2592000000"
+    "cleanup.policy" = "delete"
+  }
+}
+
 module "payment_query_service" {
   source           = "../../../modules/tls-app"
-  produce_topics   = [kafka_topic.payment_deadletter_v1_events.name, kafka_topic.payment_v1_events.name]
-  consume_topics   = [kafka_topic.payment_v1_events.name]
+  produce_topics   = [kafka_topic.payment_deadletter_v1_events.name, kafka_topic.payment_v1_events.name, kafka_topic.payment_method_deadletter_v1_events.name]
+  consume_topics   = [kafka_topic.payment_v1_events.name, kafka_topic.payment_method_v1_events.name]
   consume_groups   = ["payment-platform.payment_query_service"]
   cert_common_name = "payment-platform/payment-query-service"
 }
