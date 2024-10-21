7b11a52de25a167c2467611a99f10946ee8ed196
Alex Ramin <aramin@uw.co.uk>
2024-10-21 09:43:38 +0200

    Add person test consumer

--- prod-aws/kafka-shared-msk/account-identity/account.tf
+++ prod-aws/kafka-shared-msk/account-identity/account.tf
@@ -164,7 +164,7 @@ module "account_identity_create_account_projector" {
 module "account_identity_update_account_projector" {
   source           = "../../../modules/tls-app"
   consume_topics   = [kafka_topic.account_identity_legacy_account_change_events_compacted.name]
-  consume_groups   = ["account-identity.update-account-projector"]
+  consume_groups   = ["account-identity.update-account-projector", "account-identity.update-account-projector-person-test"]
   cert_common_name = "account-platform/update_account_projector"
 }
 
