9a3254edbaa2335a5d94baf13401fe598facf485
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2024-10-22 16:48:06 +0300

    try compact

--- dev-aws/kafka-shared-msk/pubsub/pubsub-examples.tf
+++ dev-aws/kafka-shared-msk/pubsub/pubsub-examples.tf
@@ -49,13 +49,13 @@ module "es_topic_indexer" {
 }
 
 
-resource "kafka_topic" "test-config" {
-  name               = "pubsub.test-config-sb"
+resource "kafka_topic" "test-config2" {
+  name               = "pubsub.test-config-sb2"
   replication_factor = 3
   partitions         = 3
   config = {
-#     # keep data for 6 hours
-#     "retention.ms" = "21600000"
+    # keep data for 6 hours
+    "retention.ms" = "21600000"
 #     # allow max 1 MB for a message
 #     "max.message.bytes" = "1048576"
     "compression.type"  = "zstd"
