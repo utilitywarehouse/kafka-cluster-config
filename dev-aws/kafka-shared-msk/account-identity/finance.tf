4fe00a86f999b2638308550f4ab9629a0a26870b
sbuliarca <8848332+sbuliarca@users.noreply.github.com>
2024-10-28 12:16:02 +0200

    DENA-671: dev: Remove retention time from compacted topics (#662)
    Retention time is irrelevant for compacted topics.
    See
    https://docs.confluent.io/platform/current/installation/configuration/topic-configs.html#retention-ms

--- dev-aws/kafka-shared-msk/account-identity/finance.tf
+++ dev-aws/kafka-shared-msk/account-identity/finance.tf
@@ -4,8 +4,6 @@ resource "kafka_topic" "account_identity_finance_events_compacted" {
     "compression.type" = "zstd"
     # compaction lag of 7 days
     "max.compaction.lag.ms" = "604800000"
-    # infinite retention
-    "retention.ms" = "-1"
   }
   name               = "account-identity.finance.events.compacted"
   partitions         = 15
