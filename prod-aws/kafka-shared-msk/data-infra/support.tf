396b61c00f995ce8c36e78112a0f552a842186b4
Paulo Simão <102320181+paulosimao-ardanlabs@users.noreply.github.com>
2024-11-06 12:56:05 -0300

    chore: PLA-1499: adding kafka assets to investigate issue.

---
+++ prod-aws/kafka-shared-msk/data-infra/support.tf
@@ -0,0 +1,6 @@
+module "support" {
+  source           = "../../../modules/tls-app"
+  consume_topics = [kafka_topic.events.name]
+  consume_groups = ["data-infra.support"]
+  cert_common_name = "data-infra/support"
+}
