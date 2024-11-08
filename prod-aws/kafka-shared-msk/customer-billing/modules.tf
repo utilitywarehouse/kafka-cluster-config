00fddf3981ee85ac0766ef67249abcc7f2c29dc4
Angelos Gkountis <agkountis@uw.co.uk>
2024-11-08 16:09:03 +0000

    [BEX-1056] - Create module for deadletter queue job for invoice fulflllment. (#692)

--- prod-aws/kafka-shared-msk/customer-billing/modules.tf
+++ prod-aws/kafka-shared-msk/customer-billing/modules.tf
@@ -126,6 +126,14 @@ module "invoice_fulfilment_regeneration_job" {
   produce_topics   = [kafka_topic.internal_bex_bill_regeneration.name]
 }
 
+module "invoice_fulfilment_dlq_job" {
+  source           = "../../../modules/tls-app"
+  cert_common_name = "customer-billing/invoice-fulfilment-dlq-job"
+  consume_topics   = [kafka_topic.internal_invoice_fulfilment_deadletter.name]
+  consume_groups   = ["bex.invoice-fulfilment-dlq-job"]
+  produce_topics   = [kafka_topic.transition_bex_fulfilment_request.name]
+}
+
 module "invoice_api" {
   source           = "../../../modules/tls-app"
   cert_common_name = "customer-billing/invoice-api"
