32f8b58f8952c0bdc55f9f89d1ecc08edf490129
Matt Hughes <mhughes@uw.co.uk>
2024-11-15 11:28:53 +0000

    Remove unused mirroring topics
    These topics were originally created for some mirroring that we've
    stopped[1] so clean them up.
    
    Link: https://github.com/utilitywarehouse/kubernetes-manifests/commit/39a4a3d018c46473cb63b2e481ea2383708cea59 [1]
    Ticket: DENA-1070

--- dev-aws/kafka-shared-msk/payment-platform/card-payment.tf
+++ dev-aws/kafka-shared-msk/payment-platform/card-payment.tf
@@ -15,64 +15,10 @@ resource "kafka_topic" "payment_payment" {
   }
 }
 
-resource "kafka_topic" "payment_method_created" {
-  name               = "payment-platform.payment.method-created"
-  replication_factor = 3
-  partitions         = 15
-  config = {
-    "compression.type" = "zstd"
-    "retention.bytes"  = "3758096384" # keep on each partition 3.5GiB
-    # Use tiered storage
-    "remote.storage.enable" = "true"
-    # keep data in primary storage for 2 days
-    "local.retention.ms" = "172800000"
-    # keep data for 1 year
-    "retention.ms"   = "31557600000"
-    "cleanup.policy" = "delete"
-  }
-}
-
-resource "kafka_topic" "payment_method_verified" {
-  name               = "payment-platform.payment.method-verified"
-  replication_factor = 3
-  partitions         = 15
-  config = {
-    "compression.type" = "zstd"
-    "retention.bytes"  = "3758096384" # keep on each partition 3.5GiB
-    # Use tiered storage
-    "remote.storage.enable" = "true"
-    # keep data in primary storage for 2 days
-    "local.retention.ms" = "172800000"
-    # keep data for 1 year
-    "retention.ms"   = "31557600000"
-    "cleanup.policy" = "delete"
-  }
-}
-
-resource "kafka_topic" "payment_method_linked" {
-  name               = "payment-platform.payment.method-linked"
-  replication_factor = 3
-  partitions         = 15
-  config = {
-    "compression.type" = "zstd"
-    "retention.bytes"  = "3758096384" # keep on each partition 3.5GiB
-    # Use tiered storage
-    "remote.storage.enable" = "true"
-    # keep data in primary storage for 2 days
-    "local.retention.ms" = "172800000"
-    # keep data for 1 year
-    "retention.ms"   = "31557600000"
-    "cleanup.policy" = "delete"
-  }
-}
-
 module "payment_transaction_events_forwarder" {
   source = "../../../modules/tls-app"
   consume_topics = [
     kafka_topic.payment_payment.name,
-    kafka_topic.payment_method_verified.name,
-    kafka_topic.payment_method_linked.name,
-    kafka_topic.payment_method_created.name,
     kafka_topic.notifications_tx.name,
     kafka_topic.payment_analysis_payment_method.name,
   ]
