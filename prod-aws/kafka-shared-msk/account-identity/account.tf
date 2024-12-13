436232b5b810a95a8deddd839d76d25b8893cb59
James Tait <jamesrobtait@gmail.com>
2024-12-13 10:41:31 +0000

    msk/account-identity: Add moved block to properly fix typo

--- prod-aws/kafka-shared-msk/account-identity/account.tf
+++ prod-aws/kafka-shared-msk/account-identity/account.tf
@@ -152,6 +152,11 @@ resource "kafka_topic" "account_identity_account_bill_writes_public" {
   replication_factor = 3
 }
 
+moved {
+  from = kafka_topic.account_identity_accunt_bill_writes_public
+  to   = kafka_topic.account_identity_account_bill_writes_public
+}
+
 module "account_identity_account_api_account_atomic_producer" {
   source           = "../../../modules/tls-app"
   produce_topics   = [kafka_topic.account_identity_account_atomic_v1.name]
