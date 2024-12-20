bf655b52c7dfba598f5e8f5995186fe7a5feb9ec
Filmon Daniels <filmondaniels@gmail.com>
2024-12-20 10:18:09 +0000

    update cg name

--- dev-aws/kafka-shared-msk/customer-proposition/kafka-connect.tf
+++ dev-aws/kafka-shared-msk/customer-proposition/kafka-connect.tf
@@ -1,6 +1,6 @@
 module "kafka_connect_read_service_status_events" {
   source           = "../../../modules/tls-app"
   consume_topics   = ["customer-proposition.service-status.events.v3"]
-  consume_groups   = ["kafka-connect-group"]
+  consume_groups   = ["dev-enablement.kafka-connect-group"]
   cert_common_name = "dev-enablement/kafka-connect"
 }
