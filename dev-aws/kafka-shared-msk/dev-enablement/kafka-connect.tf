6ca62cad893ca4a3c4441a6c11ff01b612acd161
Filmon Daniels <filmondaniels@gmail.com>
2024-12-20 08:34:47 +0000

    add config for kafka connect

---
+++ dev-aws/kafka-shared-msk/dev-enablement/kafka-connect.tf
@@ -0,0 +1,69 @@
+# https://docs.confluent.io/platform/7.8/connect/references/allconfigs.html#distributed-worker-configuration
+resource "kafka_topic" "_connect_configs" {
+  name               = "_connect-configs"
+  partitions         = 1
+  replication_factor = 3
+
+  config = {
+    "cleanup.policy"       = "compact"
+    "compression.type"     = "zstd"
+    "retention.ms"         = "-1"
+  }
+}
+
+resource "kafka_topic" "_connect_offsets" {
+  name               = "_connect-offsets"
+  partitions         = 25
+  replication_factor = 3
+
+  config = {
+    "cleanup.policy"       = "compact"
+    "compression.type"     = "zstd"
+    "retention.ms"         = "-1"
+  }
+}
+
+resource "kafka_topic" "_connect_status" {
+  name               = "_connect-status"
+  partitions         = 5
+  replication_factor = 3
+
+  config = {
+    "cleanup.policy"       = "compact"
+    "compression.type"     = "zstd"
+    "retention.ms"         = "-1"
+  }
+}
+
+
+# https://docs.confluent.io/platform/7.8/connect/security.html#worker-acl-requirements
+
+# Allow Kafka Connect full access to internal topics
+resource "kafka_acl" "kafka_connect_full_internal_topics" {
+  resource_name       = "_connect-*"
+  resource_type       = "Topic"
+  acl_principal       = "User:CN=dev-enablement/kafka-connect"
+  acl_host            = "*"
+  acl_operation       = "All"
+  acl_permission_type = "Allow"
+}
+
+# Allow Kafka Connect to consume only on other topics
+resource "kafka_acl" "kafka_connect_consume_other_topics" {
+  resource_name       = "*"
+  resource_type       = "Topic"
+  acl_principal       = "User:CN=dev-enablement/kafka-connect"
+  acl_host            = "*"
+  acl_operation       = "Read"
+  acl_permission_type = "Allow"
+}
+
+# Needs read/write to register itself as a consumer group
+resource "kafka_acl" "kafka_connect_group_acl" {
+  resource_name       = "*"
+  resource_type       = "Group"
+  acl_principal       = "User:CN=dev-enablement/kafka-connect"
+  acl_host            = "*"
+  acl_operation       = "All"
+  acl_permission_type = "Allow"
+}
