6055d84221cbe17bad9a5450112ab7ea7f646705
Craig Dempsie <cdempsie@uw.co.uk>
2025-01-03 13:44:26 +0000

    Update es-topic-indexer.tf

--- dev-aws/kafka-shared-msk/customer-support/es-topic-indexer.tf
+++ dev-aws/kafka-shared-msk/customer-support/es-topic-indexer.tf
@@ -2,6 +2,7 @@ module "es_topic_indexer" {
   source           = "../../../modules/tls-app"
   cert_common_name = "crm/msk-topic-indexer"
   consume_topics = [
+    kafka_topic.coffee_account_history_v1.name,
     kafka_topic.notes_v2.name,
     kafka_topic.payments_audit_log_v1.name,
     kafka_topic.reminders_v1.name,
