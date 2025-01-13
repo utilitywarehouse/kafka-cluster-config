2068e6f9afa08deb46e7e48cb0c9533f8cc72996
Rafal Dekarz <rafdekar@gmail.com>
2025-01-13 13:41:48 +0100

    feat/FPAY-1744: Add settlements topics to openbanking (#750)

--- dev-aws/kafka-shared-msk/payment-platform/open-banking.tf
+++ dev-aws/kafka-shared-msk/payment-platform/open-banking.tf
@@ -66,11 +66,46 @@ resource "kafka_topic" "openbanking_deadletter_v1_internal_payment_methods" {
   }
 }
 
+resource "kafka_topic" "openbanking_v1_internal_settlements" {
+  name               = "payment-platform.openbanking.v1.internal.settlements"
+  replication_factor = 3
+  partitions         = 5
+  config = {
+    "compression.type" = "zstd"
+    "retention.bytes"  = "-1" # keep on each partition unlimited data
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data in primary storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data for 1 month
+    "retention.ms"   = "2592000000"
+    "cleanup.policy" = "delete"
+  }
+}
+
+resource "kafka_topic" "openbanking_deadletter_v1_internal_settlements" {
+  name               = "payment-platform.openbanking-deadletter.v1.internal.settlements"
+  replication_factor = 3
+  partitions         = 5
+  config = {
+    "compression.type" = "zstd"
+    "retention.bytes"  = "-1" # keep on each partition unlimited data
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data in primary storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data for 1 month
+    "retention.ms"   = "2592000000"
+    "cleanup.policy" = "delete"
+  }
+}
+
 module "openbanking_apid" {
   source = "../../../modules/tls-app"
   produce_topics = [
     kafka_topic.openbanking_v1_internal_payments.name,
     kafka_topic.openbanking_v1_internal_payment_methods.name,
+    kafka_topic.openbanking_v1_internal_settlements.name,
     kafka_topic.payment_v1_events.name
   ]
   cert_common_name = "payment-platform/openbanking-apid"
@@ -81,11 +116,13 @@ module "openbanking_consumerd" {
   produce_topics = [
     kafka_topic.openbanking_deadletter_v1_internal_payments.name,
     kafka_topic.openbanking_deadletter_v1_internal_payment_methods.name,
+    kafka_topic.openbanking_deadletter_v1_internal_settlements.name,
     kafka_topic.payment_v1_events.name
   ]
   consume_topics = [
     kafka_topic.openbanking_v1_internal_payments.name,
-    kafka_topic.openbanking_v1_internal_payment_methods.name
+    kafka_topic.openbanking_v1_internal_payment_methods.name,
+    kafka_topic.openbanking_v1_internal_settlements.name
   ]
   consume_groups   = ["payment-platform.openbanking_consumerd"]
   cert_common_name = "payment-platform/openbanking-consumerd"
@@ -94,7 +131,8 @@ module "openbanking_consumerd" {
 module "openbanking_crond" {
   source = "../../../modules/tls-app"
   produce_topics = [
-    kafka_topic.payment_v1_events.name
+    kafka_topic.payment_v1_events.name,
+    kafka_topic.openbanking_v1_internal_settlements.name
   ]
   consume_groups   = ["payment-platform.openbanking_crond"]
   cert_common_name = "payment-platform/openbanking-crond"
@@ -107,12 +145,14 @@ module "payment_deadletterd" {
     kafka_topic.payment_method_v1_events.name,
     kafka_topic.openbanking_v1_internal_payments.name,
     kafka_topic.openbanking_v1_internal_payment_methods.name,
+    kafka_topic.openbanking_v1_internal_settlements.name
   ]
   consume_topics = [
     kafka_topic.payment_deadletter_v1_events.name,
     kafka_topic.payment_method_deadletter_v1_events.name,
     kafka_topic.openbanking_deadletter_v1_internal_payments.name,
     kafka_topic.openbanking_deadletter_v1_internal_payment_methods.name,
+    kafka_topic.openbanking_deadletter_v1_internal_settlements.name
   ]
   consume_groups   = ["payment-platform.payment-deadletterd"]
   cert_common_name = "payment-platform/payment-deadletterd"
