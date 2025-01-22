946ee6d203fe2866f2697d4e4eb9355b634013f9
marcinginszt <marcinginszt@gmail.com>
2025-01-22 14:49:08 +0100

    add dev-enablement and pilotcd to prod-aws shared msk

---
+++ prod-aws/kafka-shared-msk/dev-enablement/kafka-connect.tf
@@ -0,0 +1,66 @@
+# https://docs.confluent.io/platform/7.8/connect/references/allconfigs.html#distributed-worker-configuration
+resource "kafka_topic" "connect_configs" {
+  name               = "dev-enablement.connect-configs"
+  partitions         = 1
+  replication_factor = 3
+
+  config = {
+    "cleanup.policy"   = "compact"
+    "compression.type" = "zstd"
+
+  }
+}
+
+resource "kafka_topic" "connect_offsets" {
+  name               = "dev-enablement.connect-offsets"
+  partitions         = 25
+  replication_factor = 3
+
+  config = {
+    "cleanup.policy"   = "compact"
+    "compression.type" = "zstd"
+
+  }
+}
+
+resource "kafka_topic" "connect_status" {
+  name               = "dev-enablement.connect-status"
+  partitions         = 5
+  replication_factor = 3
+
+  config = {
+    "cleanup.policy"   = "compact"
+    "compression.type" = "zstd"
+
+  }
+}
+
+
+# https://docs.confluent.io/platform/7.8/connect/security.html#worker-acl-requirements
+
+# Allow Kafka Connect full access to internal topics
+module "kafka_connect_full_internal_topics" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = ["dev-enablement.connect-configs", "dev-enablement.connect-offsets", "dev-enablement.connect-status"]
+  produce_topics   = ["dev-enablement.connect-configs", "dev-enablement.connect-offsets", "dev-enablement.connect-status"]
+  consume_groups   = ["dev-enablement.kafka-connect-group", "dev-enablement.kafka-connect-worker-group"]
+  cert_common_name = "dev-enablement/kafka-connect"
+}
+
+resource "kafka_acl" "kafka_connect_describe_topic_all" {
+  resource_name       = "*"
+  resource_type       = "Topic"
+  acl_principal       = "User:CN=dev-enablement/kafka-connect"
+  acl_host            = "*"
+  acl_operation       = "Describe"
+  acl_permission_type = "Allow"
+}
+
+resource "kafka_acl" "kafka_connect_describe_group_all" {
+  resource_name       = "*"
+  resource_type       = "Group"
+  acl_principal       = "User:CN=dev-enablement/kafka-connect"
+  acl_host            = "*"
+  acl_operation       = "Describe"
+  acl_permission_type = "Allow"
+}
---
+++ prod-aws/kafka-shared-msk/dev-enablement/pilotcd.tf
@@ -0,0 +1,35 @@
+resource "kafka_topic" "dev_enablement_pilotcd_v1" {
+  config = {
+    "cleanup.policy"   = "delete"
+    "compression.type" = "zstd"
+    # keep data in primary storage for 1 day
+    "local.retention.ms" = "86400000"
+    # enable remote storage
+    "remote.storage.enable" = "true"
+    # keep data for 7 days
+    "retention.ms" = "604800000"
+  }
+  name               = "dev-enablement.pilotcd.v1"
+  partitions         = 5
+  replication_factor = 3
+}
+
+module "dev_enablement_pilotcd_api" {
+  source           = "../../../modules/tls-app"
+  produce_topics   = [kafka_topic.dev_enablement_pilotcd_v1.name]
+  cert_common_name = "dev-enablement/pilotcd-api"
+}
+
+module "dev_enablement_pilotcd_worker" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [kafka_topic.dev_enablement_pilotcd_v1.name]
+  consume_groups   = ["dev-enablement.pilotcd.worker"]
+  cert_common_name = "dev-enablement/pilotcd-worker"
+}
+
+module "dev_enablement_mono_releaser" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [kafka_topic.dev_enablement_pilotcd_v1.name]
+  consume_groups   = ["dev-enablement.mono.releaser"]
+  cert_common_name = "dev-enablement/mono-releaser"
+}
