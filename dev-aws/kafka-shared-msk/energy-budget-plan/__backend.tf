c7aa750e70c41f52ea5cd57836d25470c293c206
oliverpauffley <mrpauffley@gmail.com>
2025-01-23 11:03:40 +0000

    feat: remove dead namespace

--- dev-aws/kafka-shared-msk/energy-budget-plan/__backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "uw-dev-pubsub-tf-applier-state"
-    key     = "dev-aws/kafka-shared-msk-energy-budget-plan"
-    region  = "eu-west-1"
-    encrypt = true
-  }
-}
