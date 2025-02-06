aa30ddd20517319f99d815efeaca5a8965300dda
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2025-02-06 11:24:41 +0200

    Revert "Restore empty module"
    This reverts commit 719d6fc7c723be715ab84ce1e87f8c95172aecd5.

--- prod-aws/kafka-shared-msk/energy-budget-plan/__backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "uw-prod-pubsub-tf-applier-state"
-    key     = "prod-aws/kafka-shared-msk-energy-budget-plan"
-    region  = "eu-west-1"
-    encrypt = true
-  }
-}
