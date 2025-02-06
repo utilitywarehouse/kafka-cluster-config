f8e2cd3374f66b089049715f804dd9793d1462f9
oliverpauffley <mrpauffley@gmail.com>
2025-02-06 09:52:45 +0000

    feat: add prod energy-platform

---
+++ prod-aws/kafka-shared-msk/energy-platform/energy-platform.tf
@@ -0,0 +1,17 @@
+resource "kafka_topic" "meter_reads" {
+  name               = "energy-platform.meter.read.events.v2"
+  replication_factor = 3
+  partitions         = 15
+  config = {
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data for 3 months
+    "retention.ms" = "7889238000"
+    # keep data in primary storage for 2 days
+    "local.retention.ms" = "172800000"
+    # allow for a batch of records maximum 1MiB
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
