291f1dfa93f5b1fdb714ed8f161c8e5ca5d58f7b
sbuliarca <8848332+sbuliarca@users.noreply.github.com>
2025-02-20 19:47:49 +0200

    update brokers (#787)
    - **Allow only msk-admin-client first**
    - **Updated brokers after reprovision**
    - **Revert "Allow only msk-admin-client first"**

--- dev-aws/kafka-shared-msk/__env.tf
+++ dev-aws/kafka-shared-msk/__env.tf
@@ -11,8 +11,8 @@ terraform {
 
 provider "kafka" {
   bootstrap_servers = [
-    "b-1.devenablementpubsubmsk.xmhf7r.c8.kafka.eu-west-1.amazonaws.com:9094",
-    "b-2.devenablementpubsubmsk.xmhf7r.c8.kafka.eu-west-1.amazonaws.com:9094",
-    "b-3.devenablementpubsubmsk.xmhf7r.c8.kafka.eu-west-1.amazonaws.com:9094",
+    "b-1.devenablementpubsubmsk.zfikfc.c4.kafka.eu-west-1.amazonaws.com:9094",
+    "b-2.devenablementpubsubmsk.zfikfc.c4.kafka.eu-west-1.amazonaws.com:9094",
+    "b-3.devenablementpubsubmsk.zfikfc.c4.kafka.eu-west-1.amazonaws.com:9094",
   ]
 }
--- dev-aws/kafka-shared-msk/pubsub/brokers.tf
+++ dev-aws/kafka-shared-msk/pubsub/brokers.tf
@@ -1,7 +1,7 @@
 resource "kafka_acl" "brokers_topic" {
   resource_name       = "*"
   resource_type       = "Topic"
-  acl_principal       = "User:CN=*.devenablementpubsubmsk.xmhf7r.c8.kafka.eu-west-1.amazonaws.com"
+  acl_principal       = "User:CN=*.devenablementpubsubmsk.zfikfc.c4.kafka.eu-west-1.amazonaws.com"
   acl_host            = "*"
   acl_operation       = "Describe"
   acl_permission_type = "Allow"
@@ -10,7 +10,7 @@ resource "kafka_acl" "brokers_topic" {
 resource "kafka_acl" "brokers_group" {
   resource_name       = "*"
   resource_type       = "Group"
-  acl_principal       = "User:CN=*.devenablementpubsubmsk.xmhf7r.c8.kafka.eu-west-1.amazonaws.com"
+  acl_principal       = "User:CN=*.devenablementpubsubmsk.zfikfc.c4.kafka.eu-west-1.amazonaws.com"
   acl_host            = "*"
   acl_operation       = "Describe"
   acl_permission_type = "Allow"
