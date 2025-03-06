a7bd134b6a72cd152cb6500d691715926a50bf9a
Daniel Copaciu <dani.copaciu@gmail.com>
2025-03-06 11:03:47 +0200

    Create topics for fixed width file processing (#792)

---
+++ dev-aws/kafka-shared-msk/billing/billing.tf
@@ -0,0 +1,65 @@
+# Topics
+resource "kafka_topic" "fixed_width_file_deadletter" {
+  name               = "billing.fixed-width-file-deadletter"
+  replication_factor = 3
+  partitions         = 2
+  config = {
+    # store data zstd compressed
+    "compression.type" = "zstd"
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data in primary storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data for 1 month
+    "retention.ms" = "2629800000"
+    # delete old data
+    "cleanup.policy" = "delete"
+  }
+}
+
+resource "kafka_topic" "fixed_width_file_load_errors" {
+  name               = "billing.fixed-width-file-load-errors"
+  replication_factor = 3
+  partitions         = 2
+  config = {
+    # store data zstd compressed
+    "compression.type" = "zstd"
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data in primary storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data for 1 month
+    "retention.ms" = "2629800000"
+    # delete old data
+    "cleanup.policy" = "delete"
+  }
+}
+
+resource "kafka_topic" "core_bill_rectifications" {
+  name               = "billing.core-bill-rectifications"
+  replication_factor = 3
+  partitions         = 2
+  config = {
+    # store data zstd compressed
+    "compression.type" = "zstd"
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data in primary storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data for 1 month
+    "retention.ms" = "2629800000"
+    # delete old data
+    "cleanup.policy" = "delete"
+  }
+}
+
+# ACLs
+module "invoice_engine" {
+  source = "../../../modules/tls-app"
+  produce_topics = [
+    kafka_topic.fixed_width_file_deadletter.name,
+    kafka_topic.fixed_width_file_load_errors.name,
+    kafka_topic.core_bill_rectifications.name,
+  ]
+  cert_common_name = "billing/invoice-engine"
+}
