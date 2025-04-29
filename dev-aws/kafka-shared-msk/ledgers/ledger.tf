7f214f3cdcdc658714282f682ae7bb86a31cd85a
Sandor Torok <storok@uw.co.uk>
2025-04-29 12:09:40 +0300

    Added synthetic account balance topic

--- dev-aws/kafka-shared-msk/ledgers/ledger.tf
+++ dev-aws/kafka-shared-msk/ledgers/ledger.tf
@@ -35,12 +35,27 @@ resource "kafka_topic" "transaction_events" {
   }
 }
 
+resource "kafka_topic" "synthetic_account_balance_events" {
+  name               = "ledgers.synthetic-account-balance.events"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    # store data zstd compressed
+    "compression.type" = "zstd"
+    # keep data for 0.5 hours
+    "retention.ms" = "1800000"
+    # delete old data
+    "cleanup.policy" = "delete"
+  }
+}
+
 # ACLs
 module "ledger_api" {
   source = "../../../modules/tls-app"
   produce_topics = [
     kafka_topic.account_balance_events.name,
     kafka_topic.transaction_events.name,
+    kafka_topic.synthetic_account_balance_events.name,
   ]
   cert_common_name = "ledgers/ledger-api"
 }
@@ -51,7 +66,7 @@ module "ledger_operational" {
     kafka_topic.account_balance_events.name,
   ]
   consume_topics = [
-    kafka_topic.account_balance_events.name,
+    kafka_topic.synthetic_account_balance_events.name,
   ]
   consume_groups   = ["ledgers.ledger-operational"]
   cert_common_name = "ledgers/ledger-operational"
