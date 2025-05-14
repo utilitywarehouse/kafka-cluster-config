5f3c02e0da10a410fdc2b2bf04e0b497482c7131
Charles Knighton-Pullin <cknightonpullin@uw.co.uk>
2025-05-14 12:29:56 +0200

    add topic

--- dev-aws/kafka-shared-msk/payment-platform/payment.tf
+++ dev-aws/kafka-shared-msk/payment-platform/payment.tf
@@ -214,6 +214,6 @@ module "cbc_topup_processor" {
 
 module "kafka_debugger" {
   source           = "../../../modules/tls-app"
-  produce_topics   = [kafka_topic.payment_deadletter_v1_events.name]
+  produce_topics   = [kafka_topic.payment_deadletter_v1_events.name, kafka_topic.payment_method_v1_events.name]
   cert_common_name = "payment-platform/kafka-debugger"
 }
