7a556ef6bac89c946efedd1f8e62a40d78dc518e
CharlesKP <82518486+CharlesKP@users.noreply.github.com>
2025-05-26 13:47:26 +0200

    config for deadletterd (#862)
    Co-authored-by: Charles Knighton-Pullin <cknightonpullin@uw.co.uk>

---
+++ prod-aws/kafka-shared-msk/payment-platform/payment-platform-banking.tf
@@ -0,0 +1,11 @@
+module "payment_platform_banking_deadletterd" {
+  source = "../../../modules/tls-app"
+  produce_topics = [
+    kafka_topic.card_deadletter_v1_internal_payment_methods.name,
+  ]
+  consume_topics = [
+    kafka_topic.card_deadletter_v1_internal_payment_methods.name
+  ]
+  consume_groups   = ["payment-platform-banking.payment-platform-banking-deadletterd"]
+  cert_common_name = "payment-platform-banking/payment-platform-banking-deadletterd"
+}
