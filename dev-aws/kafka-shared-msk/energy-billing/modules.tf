3c011de2fa2ef41c0c1d14ad1cdd4a38d3fb00a0
Angelos Gkountis <agkountis@uw.co.uk>
2025-06-02 19:02:48 +0300

    [EPTBP-33] - More linter fix attempts

--- dev-aws/kafka-shared-msk/energy-billing/modules.tf
+++ dev-aws/kafka-shared-msk/energy-billing/modules.tf
@@ -2,15 +2,11 @@ module "energy_platform" {
   source = "../energy-platform"
 }
 
-output "gentrack_billing_events_topic_name" {
-  value = energy_platform.gentrack_billing_events_topic_name
-}
-
 module "billing_adapter" {
   source           = "../../../modules/tls-app"
   cert_common_name = "energy-billing/billing-adapter"
   produce_topics   = [kafka_topic.internal_energy_billing_billing_adapter_retry_1.name]
-  consume_topics   = [energy_platform.gentrack_billing_events_topic_name]
+  consume_topics   = [gentrack_billing_events_topic_name]
   consume_groups   = ["energy-billing.billing-adapter"]
 }
 
