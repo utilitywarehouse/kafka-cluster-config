699003120a83df9a718d80120710f9ab3d9e3e87
Joseph Silcock <joseph.silcock@uw.co.uk>
2025-06-17 14:06:06 +0100

    Use a single kafka topic for all FWF processing events (#879)

--- prod-aws/kafka-shared-msk/billing/billing.tf
+++ prod-aws/kafka-shared-msk/billing/billing.tf
@@ -1,42 +1,6 @@
 # Topics
-resource "kafka_topic" "fixed_width_file_deadletter" {
-  name               = "billing.fixed-width-file-deadletter"
-  replication_factor = 3
-  partitions         = 2
-  config = {
-    # store data zstd compressed
-    "compression.type" = "zstd"
-    # Use tiered storage
-    "remote.storage.enable" = "true"
-    # keep data in primary storage for 7 days
-    "local.retention.ms" = "604800000"
-    # keep data for 1 month
-    "retention.ms" = "2629800000"
-    # delete old data
-    "cleanup.policy" = "delete"
-  }
-}
-
-resource "kafka_topic" "fixed_width_file_load_errors" {
-  name               = "billing.fixed-width-file-load-errors"
-  replication_factor = 3
-  partitions         = 2
-  config = {
-    # store data zstd compressed
-    "compression.type" = "zstd"
-    # Use tiered storage
-    "remote.storage.enable" = "true"
-    # keep data in primary storage for 7 days
-    "local.retention.ms" = "604800000"
-    # keep data for 1 month
-    "retention.ms" = "2629800000"
-    # delete old data
-    "cleanup.policy" = "delete"
-  }
-}
-
-resource "kafka_topic" "core_bill_rectifications" {
-  name               = "billing.core-bill-rectifications"
+resource "kafka_topic" "fixed_width_file_processing_events" {
+  name               = "billing.fixed-width-file-processing-events"
   replication_factor = 3
   partitions         = 2
   config = {
@@ -75,12 +39,19 @@ resource "kafka_topic" "unified_bill_ready_events" {
 module "bill_composition_engine" {
   source = "../../../modules/tls-app"
   produce_topics = [
-    kafka_topic.fixed_width_file_deadletter.name,
-    kafka_topic.fixed_width_file_load_errors.name,
-    kafka_topic.core_bill_rectifications.name,
+    kafka_topic.fixed_width_file_processing_events.name,
     kafka_topic.unified_bill_ready_events.name,
   ]
   cert_common_name = "billing/bill-composition-engine"
+}
+
+module "fixed_width_file_processing_events_indexer" {
+  source = "../../../modules/tls-app"
+  consume_topics = [
+    kafka_topic.fixed_width_file_processing_events.name,
+  ]
+  consume_groups   = ["billing.fixed-width-file-processing-events-indexer"]
+  cert_common_name = "billing/fixed-width-file-processing-events-indexer"
 }
 
 module "unified_bill_ready_events_indexer" {
