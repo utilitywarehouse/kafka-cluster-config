156c8abb3c861e420eb546d290b7815e6323a6c7
MikhailUW <mikhail.molotkov@uw.co.uk>
2025-07-22 13:08:17 +0100

    [Dev] Add `gentrack supply-loss` consumer for meterpoint (#932)

--- dev-aws/kafka-shared-msk/energy-platform/meterpoints.tf
+++ dev-aws/kafka-shared-msk/energy-platform/meterpoints.tf
@@ -6,3 +6,12 @@ module "meterpoint_gentrack_projector" {
   consume_groups   = ["energy-platform.meterpoint-gentrack-projector"]
   cert_common_name = "energy-platform/meterpoint-gentrack-projector"
 }
+
+module "meterpoint_gentrack_supply_loss_projector" {
+  source = "../../../modules/tls-app"
+  consume_topics = [
+    kafka_topic.gentrack_market_interactions_events.name,
+  ]
+  consume_groups   = ["energy-platform.meterpoint-gentrack-projector-supply-loss"]
+  cert_common_name = "energy-platform/meterpoint-gentrack-projector-supply-loss"
+}
