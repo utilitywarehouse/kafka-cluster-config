43364749c826123daaafa5685350bd34d370ec5e
Mikhail Molotkov <mikhail.molotkov@uw.co.uk>
2025-07-22 17:07:08 +0100

    review

--- dev-aws/kafka-shared-msk/energy-platform/meterpoints.tf
+++ dev-aws/kafka-shared-msk/energy-platform/meterpoints.tf
@@ -7,11 +7,11 @@ module "meterpoint_gentrack_projector" {
   cert_common_name = "energy-platform/meterpoint-gentrack-projector"
 }
 
-module "meterpoint_gentrack_supply_loss_projector" {
+module "meterpoints_v2_supply_projector_supply_loss" {
   source = "../../../modules/tls-app"
   consume_topics = [
     kafka_topic.gentrack_market_interactions_events.name,
   ]
-  consume_groups   = ["energy-platform.meterpoint-gentrack-projector-supply-loss"]
-  cert_common_name = "energy-platform/meterpoint-gentrack-projector-supply-loss"
+  consume_groups   = ["energy-platform.meterpoint-v2-supply-projector-supply-loss"]
+  cert_common_name = "energy-platform/meterpoint-v2-supply-projector-supply-loss"
 }
