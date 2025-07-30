855260b3cd773259fc1a7f3f68268a24e9fd13d0
Joseph Silcock <joseph.silcock@softwire.com>
2025-07-30 13:33:10 +0100

    Allow BCE to produce deadletter events

--- prod-aws/kafka-shared-msk/billing/billing.tf
+++ prod-aws/kafka-shared-msk/billing/billing.tf
@@ -78,6 +78,7 @@ module "bill_composition_engine" {
     kafka_topic.fixed_width_file_processing_events.name,
     kafka_topic.bill_reconciliation_error_events.name,
     kafka_topic.unified_bill_ready_events.name,
+    kafka_topic.billing_engine_events_bce_deadletter.name,
   ]
   cert_common_name = "billing/bill-composition-engine"
 }
