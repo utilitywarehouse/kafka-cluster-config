70f5025df4ed2f3534c9dbd71678225602bc0bf5
Rafal Dekarz <rafdekar@gmail.com>
2025-08-05 08:33:48 +0200

    add new consumer group for txlog downstream

--- dev-aws/kafka-shared-msk/payment-platform/payment.tf
+++ dev-aws/kafka-shared-msk/payment-platform/payment.tf
@@ -32,6 +32,23 @@ resource "kafka_topic" "payment_method_v1_events" {
   }
 }
 
+resource "kafka_topic" "payment_method_v2_events" {
+  name               = "payment-platform.payment-method.v2.events"
+  replication_factor = 3
+  partitions         = 15
+  config = {
+    "compression.type" = "zstd"
+    "retention.bytes"  = "-1" # keep on each partition unlimited data
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data in primary storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data for 1 month
+    "retention.ms"   = "2592000000"
+    "cleanup.policy" = "delete"
+  }
+}
+
 resource "kafka_topic" "payment_deadletter_v1_events" {
   name               = "payment-platform.payment-deadletter.v1.events"
   replication_factor = 3
@@ -152,7 +169,7 @@ module "payment_query_service_downstream" {
     kafka_topic.payment_method_v1_events.name,
     kafka_topic.card_v1_internal_payment_methods.name
   ]
-  consume_groups   = ["payment-platform.payment_query_service_downstream"]
+  consume_groups   = ["payment-platform.payment_query_service_downstream","payment-platform.payment_query_service_downstream_txlog"]
   cert_common_name = "payment-platform/payment-query-service-downstream"
 }
 
