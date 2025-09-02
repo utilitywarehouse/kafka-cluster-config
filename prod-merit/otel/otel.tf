b2a868198d1f64703a96ead1282babae6e9ed8e7
ffilippopoulos <ffilippopoulos@uw.co.uk>
2025-09-02 15:32:09 +0100

    prod-merit: Deploy kafka configuration for otel

---
+++ prod-merit/otel/otel.tf
@@ -0,0 +1,70 @@
+resource "kafka_topic" "otlp_spans" {
+  name               = "otel.otlp_spans"
+  replication_factor = 3
+  partitions         = 200
+  config = {
+    # retain 5GB on each partition
+    "retention.bytes" = "5368709120"
+    # keep data for 12 hours
+    "retention.ms" = "43200000"
+    # allow max 128 MB for a message
+    "max.message.bytes" = "134217728"
+    # roll log at 3h max
+    "segment.ms" = "10800000"
+    # max log size of 250 MB
+    "segment.bytes"                  = "262144000"
+    "compression.type"               = "zstd"
+    "cleanup.policy"                 = "delete"
+    "unclean.leader.election.enable" = "false"
+  }
+}
+
+module "otel_collector" {
+  source           = "../../modules/tls-app"
+  produce_topics   = [kafka_topic.otlp_spans.name]
+  cert_common_name = "otel/collector"
+}
+
+module "otel_collector_web" {
+  source           = "../../modules/tls-app"
+  produce_topics   = [kafka_topic.otlp_spans.name]
+  cert_common_name = "otel/collector-web"
+}
+
+module "tempo_distributor" {
+  source = "../../modules/tls-app"
+  consume_topics = [
+    kafka_topic.otlp_spans.name,
+    kafka_topic.otlp_sampled_spans.name,
+  ]
+  consume_groups   = ["processor-tempo"]
+  cert_common_name = "otel/tempo-distributor"
+}
+
+resource "kafka_topic" "otlp_sampled_spans" {
+  name               = "otel.otlp_sampled_spans"
+  replication_factor = 3
+  partitions         = 200
+  config = {
+    # retain 5GB on each partition
+    "retention.bytes" = "5368709120"
+    # keep data for 12 hours
+    "retention.ms" = "43200000"
+    # allow max 128 MB for a message
+    "max.message.bytes" = "134217728"
+    # roll log at 3h max
+    "segment.ms" = "10800000"
+    # max log size of 250 MB
+    "segment.bytes"    = "262144000"
+    "compression.type" = "zstd"
+    "cleanup.policy"   = "delete"
+  }
+}
+
+module "otel_tail_sampling_collector" {
+  source           = "../../modules/tls-app"
+  consume_topics   = [kafka_topic.otlp_spans.name]
+  produce_topics   = [kafka_topic.otlp_sampled_spans.name]
+  consume_groups   = ["otel.tail-sampling-collector"]
+  cert_common_name = "otel/tail-sampling-collector"
+}
