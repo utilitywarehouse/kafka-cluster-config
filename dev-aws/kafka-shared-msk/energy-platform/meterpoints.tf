58273233a6c9a73a504707835abab28216075a87
Zoltán M. Bodor <Nesze@users.noreply.github.com>
2025-09-04 16:47:24 +0300

    Define registration event consumers (#981)

--- dev-aws/kafka-shared-msk/energy-platform/meterpoints.tf
+++ dev-aws/kafka-shared-msk/energy-platform/meterpoints.tf
@@ -15,3 +15,12 @@ module "meterpoints_v2_gentrack_supply_loss_projector" {
   consume_groups   = ["energy-platform.meterpoints-v2-gentrack-supply-loss-projector"]
   cert_common_name = "energy-platform/meterpoints-v2-gentrack-supply-loss-projector"
 }
+
+module "meterpoints_v2_gentrack_registration_consumer" {
+  source = "../../../modules/tls-app"
+  consume_topics = [
+    kafka_topic.gentrack_market_interactions_events.name,
+  ]
+  consume_groups   = ["energy-platform.meterpoints-v2-gentrack-registration-consumer"]
+  cert_common_name = "energy-platform/meterpoints-v2-gentrack-registration-consumer"
+}
