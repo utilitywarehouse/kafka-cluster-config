6cd18f193da19e12bbfc11105d93293e840201dd
Filmon Daniels <filmondaniels@gmail.com>
2025-09-04 15:20:15 +0100

    chore: add consumer for test-account-orchestrator (#979)
    ## Description
    We have a need to be able to listen for `AccountHoldersChangedEvent`s
    before firing requests to update email addresses on test accounts
    
    ref:
    https://github.com/utilitywarehouse/kubernetes-manifests/pull/116278

--- dev-aws/kafka-shared-msk/account-identity/kafka-connect.tf
+++ dev-aws/kafka-shared-msk/account-identity/kafka-connect.tf
@@ -4,3 +4,10 @@ module "kafka_connect_read_legacy_account_events" {
   consume_groups   = ["dev-enablement.kafka-connect-consumer-group"]
   cert_common_name = "dev-enablement/kafka-connect"
 }
+
+module "qe_tao_read_legacy_account_events" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = ["account-identity.legacy.account.events"]
+  consume_groups   = ["dev-enablement.qe-tao-consumer-group"]
+  cert_common_name = "dev-enablement/qe-tao-msk-client"
+}
