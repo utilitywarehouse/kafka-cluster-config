28014e96768473824822af9a13eb165104c35df8
Hector Huertas <hectorhuertas@gmail.com>
2025-09-29 16:03:23 +0200

    Bootstrap sys-msk-exp

---
+++ dev-aws/sys-msk-exp/__env.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    kafka = {
+      source  = "Mongey/kafka"
+      version = ">= 0.7.0"
+    }
+  }
+}
+
+provider "kafka" {
+  bootstrap_servers = [
+    "b-1.sysmskexp.uu5xs6.c6.kafka.eu-west-1.amazonaws.com:9094",
+    "b-2.sysmskexp.uu5xs6.c6.kafka.eu-west-1.amazonaws.com:9094",
+    "b-3.sysmskexp.uu5xs6.c6.kafka.eu-west-1.amazonaws.com:9094",
+  ]
+}
