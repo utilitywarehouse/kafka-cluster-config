c31fcb95e154eac6f6b3de74b5769864dd8edca0
Hector Huertas <hectorhuertas@gmail.com>
2025-10-01 15:28:07 +0200

    Basic configuration for an experimental msk cluster (#1003)

---
+++ dev-aws/sys-msk-exp/brokers.tf
@@ -0,0 +1,17 @@
+resource "kafka_acl" "brokers_topic" {
+  resource_name       = "*"
+  resource_type       = "Topic"
+  acl_principal       = "User:CN=*.sysmskexp.uu5xs6.c6.kafka.eu-west-1.amazonaws.com"
+  acl_host            = "*"
+  acl_operation       = "Describe"
+  acl_permission_type = "Allow"
+}
+
+resource "kafka_acl" "brokers_group" {
+  resource_name       = "*"
+  resource_type       = "Group"
+  acl_principal       = "User:CN=*.sysmskexp.uu5xs6.c6.kafka.eu-west-1.amazonaws.com"
+  acl_host            = "*"
+  acl_operation       = "Describe"
+  acl_permission_type = "Allow"
+}
