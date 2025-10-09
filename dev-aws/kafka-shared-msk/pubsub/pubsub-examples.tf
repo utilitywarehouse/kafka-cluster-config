276a05749ac77b5f78031c1996846bf2575eb656
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2025-10-09 12:22:22 +0300

    Test add new topics in dev & prod

--- dev-aws/kafka-shared-msk/pubsub/pubsub-examples.tf
+++ dev-aws/kafka-shared-msk/pubsub/pubsub-examples.tf
@@ -14,6 +14,22 @@ resource "kafka_topic" "pubsub_examples" {
   }
 }
 
+resource "kafka_topic" "pubsub_examples2" {
+  name               = "pubsub.examples2"
+  replication_factor = 3
+  partitions         = 3
+  config = {
+    # keep on each partition 100MiB
+    "retention.bytes" = "104857600"
+    # keep data for 2 days
+    "retention.ms" = "172800000"
+    # allow for a batch of records maximum 1MiB
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
 module "example_producer" {
   source           = "../../../modules/tls-app"
   produce_topics   = [kafka_topic.pubsub_examples.name]
