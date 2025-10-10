e13131269cbcc7afdcede717340173e19e8972d3
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2025-10-10 12:01:40 +0300

    Updated to `test-` prefix

--- dev-aws/kafka-shared-msk/pubsub/test-kafka-connect-msk-backup.tf
+++ dev-aws/kafka-shared-msk/pubsub/test-kafka-connect-msk-backup.tf
@@ -1,6 +1,6 @@
 # https://docs.confluent.io/platform/7.8/connect/references/allconfigs.html#distributed-worker-configuration
-resource "kafka_topic" "exp_msk_backup_connect_configs" {
-  name               = "pubsub.exp-msk-backup.connect-configs"
+resource "kafka_topic" "test_msk_backup_connect_configs" {
+  name               = "pubsub.test-msk-backup.connect-configs"
   partitions         = 1
   replication_factor = 3
 
@@ -11,8 +11,8 @@ resource "kafka_topic" "exp_msk_backup_connect_configs" {
   }
 }
 
-resource "kafka_topic" "exp_msk_backup_connect_offsets" {
-  name               = "pubsub.exp-msk-backup.connect-offsets"
+resource "kafka_topic" "test_msk_backup_connect_offsets" {
+  name               = "pubsub.test-msk-backup.connect-offsets"
   partitions         = 1
   replication_factor = 3
 
@@ -23,8 +23,8 @@ resource "kafka_topic" "exp_msk_backup_connect_offsets" {
   }
 }
 
-resource "kafka_topic" "exp_msk_backup_connect_status" {
-  name               = "pubsub.exp-msk-backup.connect-status"
+resource "kafka_topic" "test_msk_backup_connect_status" {
+  name               = "pubsub.test-msk-backup.connect-status"
   partitions         = 1
   replication_factor = 3
 
@@ -41,15 +41,15 @@ resource "kafka_topic" "exp_msk_backup_connect_status" {
 # Allow Kafka Connect full access to internal topics
 module "exp_kafka_connect_full_internal_topics" {
   source           = "../../../modules/tls-app"
-  produce_topics   = ["pubsub.exp-msk-backup.connect-configs", "pubsub.exp-msk-backup.connect-offsets", "pubsub.exp-msk-backup.connect-status"]
-  consume_groups   = ["pubsub.exp-msk-backup-kafka-connect", "pubsub.exp-msk-backup-kafka-connect-worker-group", "pubsub.exp-msk-backup-kafka-connect-debug"]
-  cert_common_name = "pubsub/exp-msk-backup-kafka-connect"
+  produce_topics   = ["pubsub.test-msk-backup.connect-configs", "pubsub.test-msk-backup.connect-offsets", "pubsub.test-msk-backup.connect-status"]
+  consume_groups   = ["pubsub.test-msk-backup-kafka-connect", "pubsub.test-msk-backup-kafka-connect-worker-group", "pubsub.test-msk-backup-kafka-connect-debug"]
+  cert_common_name = "pubsub/test-msk-backup-kafka-connect"
 }
 
 resource "kafka_acl" "exp_kafka_connect_read_topic_all" {
   resource_name       = "*"
   resource_type       = "Topic"
-  acl_principal       = "User:CN=pubsub/exp-msk-backup-kafka-connect"
+  acl_principal       = "User:CN=pubsub/test-msk-backup-kafka-connect"
   acl_host            = "*"
   acl_operation       = "Read"
   acl_permission_type = "Allow"
@@ -58,7 +58,7 @@ resource "kafka_acl" "exp_kafka_connect_read_topic_all" {
 resource "kafka_acl" "exp_kafka_connect_describe_group_all" {
   resource_name       = "*"
   resource_type       = "Group"
-  acl_principal       = "User:CN=pubsub/exp-msk-backup-kafka-connect"
+  acl_principal       = "User:CN=pubsub/test-msk-backup-kafka-connect"
   acl_host            = "*"
   acl_operation       = "Describe"
   acl_permission_type = "Allow"
@@ -69,7 +69,7 @@ resource "kafka_acl" "exp_kafka_connect_describe_group_all" {
 # resource "kafka_acl" "exp_kafka_connect_write_topic_all" {
 #   resource_name       = "*"
 #   resource_type       = "Topic"
-#   acl_principal       = "User:CN=pubsub/exp-msk-backup-kafka-connect"
+#   acl_principal       = "User:CN=pubsub/test-msk-backup-kafka-connect"
 #   acl_host            = "*"
 #   acl_operation       = "Write"
 #   acl_permission_type = "Allow"
