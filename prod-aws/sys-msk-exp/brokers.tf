676bdd30d68ae40529ebbdea883dc7d7d5be5c01
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2025-10-15 12:16:52 +0300

    fixed name

--- prod-aws/sys-msk-exp/brokers.tf
+++ prod-aws/sys-msk-exp/brokers.tf
@@ -10,7 +10,7 @@ resource "kafka_acl" "brokers_topic" {
 resource "kafka_acl" "brokers_group" {
   resource_name       = "*"
   resource_type       = "Group"
-  acl_principal       = "User:CN=*.ssysmskexp.hcpvl3.c1.kafka.eu-west-1.amazonaws.com"
+  acl_principal       = "User:CN=*.sysmskexp.hcpvl3.c1.kafka.eu-west-1.amazonaws.com"
   acl_host            = "*"
   acl_operation       = "Describe"
   acl_permission_type = "Allow"
