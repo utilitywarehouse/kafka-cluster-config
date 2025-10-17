1dbd7c5bfbb49aebdc77409a019055fbb5df3435
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2025-10-17 09:43:51 +0300

    Allow restoring old messages

--- prod-aws/sys-msk-exp/restore-topics.tf
+++ prod-aws/sys-msk-exp/restore-topics.tf
@@ -8,10 +8,11 @@ resource "kafka_topic" "challenge_events_v1" {
     "remote.storage.enable" = "true"
     "retention.bytes"       = "-1" # keep on each partition unlimited data
     # tflint-ignore: msk_topic_no_infinite_retention, # infinite retention because ...
-    "retention.ms"       = "-1"       # keep data forever
-    "local.retention.ms" = "18000000" # keep data in primary storage for 5 hours
-    "max.message.bytes"  = "2097152"  # allow for a batch of records maximum 2MiB
-    "compression.type"   = "zstd"
-    "cleanup.policy"     = "delete"
+    "retention.ms"                        = "-1"       # keep data forever
+    "local.retention.ms"                  = "18000000" # keep data in primary storage for 5 hours
+    "max.message.bytes"                   = "2097152"  # allow for a batch of records maximum 2MiB
+    "compression.type"                    = "zstd"
+    "cleanup.policy"                      = "delete"
+    "message.timestamp.difference.max.ms" = "9223372036854775807"
   }
 }
