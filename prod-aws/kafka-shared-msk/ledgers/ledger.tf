7d7a64b140a91866badc72a06262052881ce07e2
Sandor Torok <storok@uw.co.uk>
2025-10-30 16:33:36 +0200

    Configured migrated account events topic + some cleanup

--- prod-aws/kafka-shared-msk/ledgers/ledger.tf
+++ prod-aws/kafka-shared-msk/ledgers/ledger.tf
@@ -37,15 +37,18 @@ resource "kafka_topic" "transaction_events" {
   }
 }
 
-resource "kafka_topic" "synthetic_account_balance_events" {
-  name               = "ledgers.synthetic-account-balance.events"
+resource "kafka_topic" "migrated_account_events" {
+  name               = "ledgers.account.migrated.events"
   replication_factor = 3
   partitions         = 10
   config = {
+    "remote.storage.enable" = "true"
     # store data zstd compressed
     "compression.type" = "zstd"
-    # keep data for 0.5 hours
-    "retention.ms" = "1800000"
+    # keep data in primary storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data for 1 month
+    "retention.ms" = "2629800000"
     # delete old data
     "cleanup.policy" = "delete"
   }
@@ -57,19 +60,19 @@ module "ledger_api" {
   produce_topics = [
     kafka_topic.account_balance_events.name,
     kafka_topic.transaction_events.name,
-    kafka_topic.synthetic_account_balance_events.name,
+    kafka_topic.migrated_account_events.name,
   ]
   cert_common_name = "ledgers/ledger-api"
 }
 
-module "ledger_operational" {
+module "ledger_consumer" {
   source = "../../../modules/tls-app"
   produce_topics = [
-    kafka_topic.account_balance_events.name,
+    kafka_topic.migrated_account_events.name,
   ]
   consume_topics = [
-    kafka_topic.synthetic_account_balance_events.name,
+    kafka_topic.migrated_account_events.name,
   ]
-  consume_groups   = ["ledgers.ledger-operational"]
-  cert_common_name = "ledgers/ledger-operational"
+  consume_groups   = ["ledgers.ledger-consumer"]
+  cert_common_name = "ledgers/ledger-consumer"
 }
