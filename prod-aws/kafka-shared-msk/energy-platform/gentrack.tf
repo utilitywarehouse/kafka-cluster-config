ad92236c9c8420a4d13d7a7fec607e8031f2fbaf
Mat Wright <12665151+web-slinger@users.noreply.github.com>
2025-11-06 11:27:50 +0000

    [dev/prod] create new perms for indexer to consume billing events (#1051)

--- prod-aws/kafka-shared-msk/energy-platform/gentrack.tf
+++ prod-aws/kafka-shared-msk/energy-platform/gentrack.tf
@@ -92,17 +92,27 @@ resource "kafka_topic" "gentrack_meterpoint_events" {
   }
 }
 
+# topic contain proto events from energy-contracts
 module "gentrack_topic_indexer" {
   source = "../../../modules/tls-app"
   consume_topics = [
     kafka_topic.gentrack_meter_read_events.name,
-    kafka_topic.gentrack_billing_events.name,
     kafka_topic.gentrack_migration_events.name,
     kafka_topic.gentrack_market_interactions_events.name,
     kafka_topic.gentrack_meterpoint_events.name
   ]
   consume_groups   = ["energy-platform.gentrack-topic-indexer"]
   cert_common_name = "energy-platform/gentrack-topic-indexer"
+}
+
+# topic contain proto events from protobuf-contracts
+module "gentrack_topic_indexer_uw" {
+  source = "../../../modules/tls-app"
+  consume_topics = [
+    kafka_topic.gentrack_billing_events.name,
+  ]
+  consume_groups   = ["energy-platform.gentrack-topic-indexer-uw"]
+  cert_common_name = "energy-platform/gentrack-topic-indexer-uw"
 }
 
 module "gentrack_adapter_webhook_processor" {
