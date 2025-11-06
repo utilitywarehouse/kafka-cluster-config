8553b5d49c6863eb077152e89870a119e0ce8ae9
Paulo Simão <102320181+paulosimao-ardanlabs@users.noreply.github.com>
2025-11-06 19:25:48 -0300

    chore: pla-1760: creating topics bill integration in msk dev

---
+++ dev-aws/kafka-shared-msk/data-infra/bill-integration.tf
@@ -0,0 +1,127 @@
+resource "kafka_topic" "bill_event_bridge_dlq" {
+  name               = "data-infra.bill_event_bridge.dlq"
+  replication_factor = 3
+  partitions         = 1
+  config = {
+    "remote.storage.enable" = "true"
+    # keep data for 1 month
+    "retention.ms" = "2629800000"
+    # keep data in primary storage for 1 day
+    "local.retention.ms" = "86400000"
+    # allow for a batch of records maximum 1MiB
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+resource "kafka_topic" "bill_integration_bill_telemetry" {
+  name               = "data-infra.bill_integration.bill_telemetry"
+  replication_factor = 3
+  partitions         = 1
+  config = {
+    "remote.storage.enable" = "true"
+    # keep data for 1 month
+    "retention.ms" = "2628000000"
+    # keep data in primary storage for 1 day
+    "local.retention.ms" = "86400000"
+    # allow for a batch of records maximum 1MiB
+    "max.message.bytes" = "2000012"
+    "compression.type"  = "producer"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+resource "kafka_topic" "bill_integration_bill_to_kubernetes" {
+  name               = "data_infra.bill_integration.bill_to_kubernetes"
+  replication_factor = 3
+  partitions         = 15
+  config = {
+    "remote.storage.enable" = "true"
+    # keep data for 1 month
+    "retention.ms" = "2628000000"
+    # keep data in primary storage for 1 day
+    "local.retention.ms" = "86400000"
+    # allow for a batch of records maximum 1MiB
+    "max.message.bytes" = "2000012"
+    "compression.type"  = "producer"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+resource "kafka_topic" "bill_integration_kubernetes_to_bill" {
+  name               = "data-infra.bill_integration.kubernetes_to_bill"
+  replication_factor = 3
+  partitions         = 15
+  config = {
+    "remote.storage.enable" = "true"
+    # keep data for 1 month
+    "retention.ms" = "2628000000"
+    # keep data in primary storage for 1 day
+    "local.retention.ms" = "86400000"
+    # allow for a batch of records maximum 1MiB
+    "max.message.bytes" = "2000012"
+    "compression.type"  = "producer"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+resource "kafka_topic" "bill_integration_kubernetes_to_bill_energy_meter_reading" {
+  name               = "data-infra.bill_integration.kubernetes_to_bill_energy_meter_reading"
+  replication_factor = 3
+  partitions         = 15
+  config = {
+    "remote.storage.enable" = "true"
+    # keep data for 1 month
+    "retention.ms" = "2628000000"
+    # keep data in primary storage for 1 day
+    "local.retention.ms" = "86400000"
+    # allow for a batch of records maximum 1MiB
+    "max.message.bytes" = "2000012"
+    "compression.type"  = "producer"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+resource "kafka_topic" "bill_integration_mm2_test" {
+  name               = "data-infra.bill_integration.mm2_test"
+  replication_factor = 3
+  partitions         = 1
+  config = {
+    "remote.storage.enable" = "true"
+    # keep data for 1 month
+    "retention.ms" = "2628000000"
+    # keep data in primary storage for 1 day
+    "local.retention.ms" = "86400000"
+    # allow for a batch of records maximum 1MiB
+    "max.message.bytes" = "2000012"
+    "compression.type"  = "producer"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+module "di_bill_event_bridge" {
+  source = "../../../modules/tls-app"
+
+  produce_topics = [kafka_topic.bill_event_bridge_dlq.name]
+
+  cert_common_name = "bill-integration/bill-event-bridge"
+}
+
+module "bill_integration_mm2" {
+  source = "../../../modules/tls-app"
+
+  produce_topics = [
+    kafka_topic.bill_integration_mm2_test.name
+  ]
+
+  consume_topics = [
+    kafka_topic.bill_integration_mm2_test.name
+  ]
+
+  consume_groups = [
+    "data-infra.bill-integration.mm2_test"
+  ]
+
+  cert_common_name = "bill-integration/mm2_test"
+}
