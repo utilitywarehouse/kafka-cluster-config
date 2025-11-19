c8c5bbe1bee0bb1bd33a6d2f8a0633cfe66153ea
Paulo Simão <102320181+paulosimao-ardanlabs@users.noreply.github.com>
2025-11-19 11:28:26 -0300

    chore pla-1773: adjusting for beb on msk

--- dev-aws/kafka-shared-msk/data-infra/bill-integration.tf
+++ dev-aws/kafka-shared-msk/data-infra/bill-integration.tf
@@ -91,131 +91,25 @@ resource "kafka_topic" "bill_integration_kubernetes_to_bill_energy_meter_reading
   }
 }
 
-resource "kafka_topic" "bill_integration_mm2_test" {
-  name               = "data-infra.bill_integration.mm2_test"
-  replication_factor = 3
-  partitions         = 1
-  config = {
-    "remote.storage.enable" = "true"
-    # keep data for 1 month
-    "retention.ms" = "2628000000"
-    # keep data in primary storage for 1 day
-    "local.retention.ms" = "86400000"
-    # allow for a batch of records maximum 1.9MiB
-    "max.message.bytes" = "2000012"
-    "compression.type"  = "zstd"
-    "cleanup.policy"    = "delete"
-    # Allow timestamps up to 10 years old
-    "message.timestamp.difference.max.ms" = "315576000000"
-  }
-}
-
-resource "kafka_topic" "bill_integration_mm2_offsets" {
-  name               = "data-infra.bill-integration.mm2-offsets.source.internal"
-  replication_factor = 3
-  partitions         = 1
-  config = {
-    "remote.storage.enable" = "true"
-    # keep data for 1 month
-    "retention.ms" = "2628000000"
-    # keep data in primary storage for 1 day
-    "local.retention.ms" = "86400000"
-    # allow for a batch of records maximum 1.9MiB
-    "max.message.bytes" = "2000012"
-    "compression.type"  = "zstd"
-    "cleanup.policy"    = "delete"
-    # Allow timestamps up to 10 years old
-    "message.timestamp.difference.max.ms" = "315576000000"
-  }
-}
-
-resource "kafka_topic" "bill_integration_mm2_checkpoints" {
-  name               = "data-infra.bill-integration.checkpoints.internal"
-  replication_factor = 3
-  partitions         = 1
-  config = {
-    "remote.storage.enable" = "true"
-    # keep data for 1 month
-    "retention.ms" = "2628000000"
-    # keep data in primary storage for 1 day
-    "local.retention.ms" = "86400000"
-    # allow for a batch of records maximum 1.9MiB
-    "max.message.bytes" = "2000012"
-    "compression.type"  = "zstd"
-    "cleanup.policy"    = "delete"
-    # Allow timestamps up to 10 years old
-    "message.timestamp.difference.max.ms" = "315576000000"
-  }
-}
-
-resource "kafka_topic" "bill_integration_mm2_heartbeats" {
-  name               = "data-infra.bill-integration.heartbeats"
-  replication_factor = 3
-  partitions         = 1
-  config = {
-    "remote.storage.enable" = "true"
-    # keep data for 1 month
-    "retention.ms" = "2628000000"
-    # keep data in primary storage for 1 day
-    "local.retention.ms" = "86400000"
-    # allow for a batch of records maximum 1.9MiB
-    "max.message.bytes" = "2000012"
-    "compression.type"  = "zstd"
-    "cleanup.policy"    = "delete"
-    # Allow timestamps up to 10 years old
-    "message.timestamp.difference.max.ms" = "315576000000"
-  }
-}
 
 
 module "di_bill_event_bridge" {
   source = "../../../modules/tls-app"
 
-  produce_topics = [kafka_topic.bill_event_bridge_dlq.name]
-
-  cert_common_name = "bill-integration/bill-event-bridge"
-}
-
-module "bill_integration_mm2" {
-  source = "../../../modules/tls-app"
-
   produce_topics = [
-    kafka_topic.bill_integration_mm2_test.name,
+    kafka_topic.bill_event_bridge_dlq.name,
     kafka_topic.bill_integration_bill_telemetry.name,
     kafka_topic.bill_integration_bill_to_kubernetes.name,
-    kafka_topic.bill_integration_kubernetes_to_bill.name,
-    kafka_topic.bill_integration_kubernetes_to_bill_energy_meter_reading.name,
-    kafka_topic.bill_integration_mm2_offsets.name,
-    kafka_topic.bill_integration_mm2_checkpoints.name,
-    kafka_topic.bill_integration_mm2_heartbeats.name,
   ]
 
   consume_topics = [
-    kafka_topic.bill_integration_mm2_test.name
+    kafka_topic.bill_integration_kubernetes_to_bill.name,
+    kafka_topic.bill_integration_kubernetes_to_bill_energy_meter_reading.name,
   ]
 
   consume_groups = [
-    "data-infra.bill-integration.mm2_test",
-    "data-infra.gmm-bill-email-connector",
-    "data-infra.gmm-bill-proximo-metrics-exporter",
-    "data-infra.gmm-payment-bill-remove-card-service",
-    "data-infra.gmm-bill-budget-plan-proximo-reader",
-    "data-infra.gmm-bill-event-reconciler-inbound",
-    "data-infra.gmm-bill-event-reconciler-outbound",
-    "data-infra.gmm-bill-letter-connector",
-    "data-infra.gmm-bill-sms-connector",
-    "data-infra.gmm-equinox-proximo",
-    "data-infra.gmm-gentrack-migration-bill-consumer",
-    "data-infra.gmm-kubernetes-to-bill",
-    "data-infra.gmm-mobile-bill-to-kubernetes-kafka2kafka",
-    "data-infra.gmm-order-platform-bill-application-releaser",
-    "data-infra.gmm-uw-bill-telemetry-bq-connector",
-    "data-infra.gmm-payment-ddm-bill-writer",
-    "data-infra.gmm-telecom-bill-to-kubernetes-kafka2kafka",
-    "data-infra.bill-integration.mm2-source",
-    "data-infra.bill-integration.mm2-checkpoints",
-    "data-infra.bill-integration.mm2-heartbeat"
+    "data-infra.bill-integration.di-bill-event-bridge",
   ]
 
-  cert_common_name = "bill-integration/mm2_test"
+  cert_common_name = "bill-integration/bill-event-bridge"
 }
