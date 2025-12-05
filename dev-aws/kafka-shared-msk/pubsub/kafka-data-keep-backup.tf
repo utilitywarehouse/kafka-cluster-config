a4070b19602569916c9fc3a0514b028727a67515
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2025-12-05 16:00:19 +0200

    msk data backup: add the topic for planning a restore

--- dev-aws/kafka-shared-msk/pubsub/kafka-data-keep-backup.tf
+++ dev-aws/kafka-shared-msk/pubsub/kafka-data-keep-backup.tf
@@ -16,9 +16,26 @@ resource "kafka_acl" "msk_data_keep_describe_group_all" {
   acl_permission_type = "Allow"
 }
 
-module "msk_data_keep_group_write" {
+resource "kafka_topic" "plan_restore" {
+  name               = "pubsub.plan-topic-restore"
+  replication_factor = 3
+  partitions         = 15
+  config = {
+    "remote.storage.enable" = "true"
+    "local.retention.ms"    = "86400000" # keep data in primary storage for 1 day
+    # keep data for 3 days
+    "retention.ms" = "259200000"
+    # allow for a batch of records maximum 100MiB
+    "max.message.bytes" = "104857600"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+module "msk_data_keep" {
   source           = "../../../modules/tls-app"
   consume_groups   = ["pubsub.msk-data-keep-backup"]
+  produce_topics   = [kafka_topic.plan_restore.name]
   cert_common_name = "pubsub/msk-data-keep"
 }
 
