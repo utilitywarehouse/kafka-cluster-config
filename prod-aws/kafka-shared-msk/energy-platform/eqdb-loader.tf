aee4d8bd4caaa40339b00e06fa8ebbbfa44699fd
Mat Wright <12665151+web-slinger@users.noreply.github.com>
2025-12-16 16:44:27 +0000

    [prod-msk] eqdb loader topic (#1094)
    Co-authored-by: GITHUB ACTIONS AUTOMATED USER <noreply@uw.co.uk>

---
+++ prod-aws/kafka-shared-msk/energy-platform/eqdb-loader.tf
@@ -0,0 +1,30 @@
+resource "kafka_topic" "eqdb_loader_service" {
+  name               = "energy-platform.eqdb-loader.service"
+  replication_factor = 3
+  partitions         = 1
+  config = {
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data for 7 days
+    "retention.ms" = "604800000"
+    # keep data in primary storage for 2 days
+    "local.retention.ms" = "172800000"
+    # allow for a batch of records maximum 1MiB
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+module "eqdb_loader_service" {
+  source           = "../../../modules/tls-app"
+  produce_topics   = [kafka_topic.eqdb_loader_service.name]
+  cert_common_name = "energy-platform/eqdb-loader.service"
+}
+
+module "service_fabricator" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [kafka_topic.eqdb_loader_service.name]
+  consume_groups   = ["energy-platform.service-fabrication"]
+  cert_common_name = "energy-platform/service-fabrication"
+}
