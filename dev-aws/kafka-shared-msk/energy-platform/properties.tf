4b6c553422a98e03f559b8bac78392867cce6175
patrick <pansbro@me.com>
2026-01-07 14:42:42 +0000

    add property_migration_events topic

---
+++ dev-aws/kafka-shared-msk/energy-platform/properties.tf
@@ -0,0 +1,19 @@
+
+resource "kafka_topic" "property_migration_events" {
+  name               = "energy-platform.propery.migration.events"
+  replication_factor = 3
+  partitions         = 15
+
+  config = {
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data for 6 months
+    "retention.ms" = "15552000000"
+    # keep data in primary storage for 2 days
+    "local.retention.ms" = "172800000"
+    # allow for a batch of records maximum 1MiB
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
