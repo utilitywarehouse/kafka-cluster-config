aa2c2a8582d4f29a70e6002d0b786da3ad0db5fc
patrick ansbro <66091489+pansbro12@users.noreply.github.com>
2026-01-22 13:50:21 +0000

    add property_migration_events topic (#1106)
    Add Kafka topic configuration for energy platform property migration
    events
    
    ---------
    
    Co-authored-by: GITHUB ACTIONS AUTOMATED USER <noreply@uw.co.uk>
    Co-authored-by: Leigh <lawleycobhc@gmail.com>

---
+++ dev-aws/kafka-shared-msk/energy-platform/properties.tf
@@ -0,0 +1,26 @@
+
+resource "kafka_topic" "property_migration_events" {
+  name               = "energy-platform.property.migration.events"
+  replication_factor = 3
+  partitions         = 15
+
+  config = {
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data for 6 months
+    "retention.ms" = "15552000000"
+    # keep data in primary storage for 2 days
+    "local.retention.ms" = "172800000"
+    # allow for a batch of records maximum 1MiB
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+
+module "gentrack_migration_worker" {
+  source           = "../../../modules/tls-app"
+  produce_topics   = [kafka_topic.property_migration_events.name]
+  cert_common_name = "energy-platform/gentrack-migration-worker"
+}
