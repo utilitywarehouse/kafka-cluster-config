98586ccddc3ac05e86f25f8760e027cd853288b0
andreeamei <117647110+andreeamei@users.noreply.github.com>
2026-01-27 10:45:48 +0100

    add consumer to property migration events (#1133)

--- dev-aws/kafka-shared-msk/energy-platform/properties.tf
+++ dev-aws/kafka-shared-msk/energy-platform/properties.tf
@@ -24,3 +24,10 @@ module "gentrack_migration_worker" {
   produce_topics   = [kafka_topic.property_migration_events.name]
   cert_common_name = "energy-platform/gentrack-migration-worker"
 }
+
+module "energy_billing_consumer" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [kafka_topic.property_migration_events.name]
+  consume_groups   = ["energy-billing.budget-plan-events-consumer"]
+  cert_common_name = "energy-billing/budget-plan-events-consumer"
+}
