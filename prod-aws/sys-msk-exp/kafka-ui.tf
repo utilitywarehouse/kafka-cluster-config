c059f2c3d801e4212e24e04d1fea4b986d7983d6
sbuliarca <8848332+sbuliarca@users.noreply.github.com>
2026-01-29 18:15:52 +0200

    sys-msk-exp: allow deletions from dedicated kafka-ui (#1142)

--- prod-aws/sys-msk-exp/kafka-ui.tf
+++ prod-aws/sys-msk-exp/kafka-ui.tf
@@ -7,16 +7,6 @@ resource "kafka_acl" "kafka_ui_topic" {
   acl_permission_type = "Allow"
 }
 
-# Deny topic delete to kafka-ui to prevent data loss. Only tf-kafka-config-admin will be able to delete topics in prod manually
-resource "kafka_acl" "kafka_ui_topic_deny_delete" {
-  resource_name       = "*"
-  resource_type       = "Topic"
-  acl_principal       = "User:CN=pubsub/sys-msk-exp-kafka-ui"
-  acl_host            = "*"
-  acl_operation       = "Delete"
-  acl_permission_type = "Deny"
-}
-
 resource "kafka_acl" "kafka_ui_group" {
   resource_name       = "*"
   resource_type       = "Group"
