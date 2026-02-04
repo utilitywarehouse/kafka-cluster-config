43f914aa4d730590db74d0bffdb973db20b84c51
sbuliarca <8848332+sbuliarca@users.noreply.github.com>
2026-02-04 19:21:15 +0200

    msk prod: keep resources commented out as we'd only use them when doing a restore (#1157)
    - **Revert "msk prod: test a small restore (#1154)"**
    - **msk prod: comment out resources needed for restore**
    - **sys-msk-exp: update topics configs in sys-msk-exp to have it ready
    for restore tests.**
    
    ---------
    
    Co-authored-by: GITHUB ACTIONS AUTOMATED USER <noreply@uw.co.uk>

--- prod-aws/kafka-shared-msk/pubsub/msk-data-keep.tf
+++ prod-aws/kafka-shared-msk/pubsub/msk-data-keep.tf
@@ -22,96 +22,56 @@ module "msk_data_keep_backup" {
   cert_common_name = "pubsub/msk-data-keep-backup"
 }
 
-resource "kafka_topic" "plan_restore_normal" {
-  name               = "pubsub.plan-topic-restore.normal"
-  replication_factor = 3
-  partitions         = 15
-  config = {
-    "remote.storage.enable" = "true"
-    "local.retention.ms"    = "86400000" # keep data in primary storage for 1 day
-    # keep data for 3 days
-    "retention.ms" = "259200000"
-    # allow for a batch of records maximum 100MiB
-    "max.message.bytes" = "104857600"
-    "compression.type"  = "zstd"
-    "cleanup.policy"    = "delete"
-  }
-}
-
-resource "kafka_topic" "plan_restore_large" {
-  name               = "pubsub.plan-topic-restore.large"
-  replication_factor = 3
-  partitions         = 15
-  config = {
-    "remote.storage.enable" = "true"
-    "local.retention.ms"    = "86400000" # keep data in primary storage for 1 day
-    # keep data for 3 days
-    "retention.ms" = "259200000"
-    # allow for a batch of records maximum 100MiB
-    "max.message.bytes" = "104857600"
-    "compression.type"  = "zstd"
-    "cleanup.policy"    = "delete"
-  }
-}
-
-module "msk_data_keep_plan_restore" {
-  source         = "../../../modules/tls-app"
-  produce_topics = [kafka_topic.plan_restore_normal.name, kafka_topic.plan_restore_large.name]
-  consume_topics = [kafka_topic.plan_restore_normal.name, kafka_topic.plan_restore_large.name]
-
-  cert_common_name = "pubsub/msk-data-keep-plan-restore"
-}
-
-resource "kafka_topic" "restore_test_charges_events_v1" {
-  name = "pubsub.restore-test.cbc.ChargesEvents"
-
-  replication_factor = 3
-  partitions         = 15
-
-  config = {
-    "remote.storage.enable" = "true"
-    "retention.bytes"       = "-1" # keep on each partition unlimited data
-    # tflint-ignore: msk_topic_no_infinite_retention, # infinite retention because ...
-    "retention.ms"       = "-1"      # keep data forever
-    "local.retention.ms" = "3600000" # keep data in primary storage for 1 hour
-    "max.message.bytes"  = "2097152" # allow for a batch of records maximum 2MiB
-    "compression.type"   = "zstd"
-    "cleanup.policy"     = "delete"
-  }
-}
-
-resource "kafka_topic" "restore_test_basket_v1" {
-  name               = "pubsub.restore-test.quoting-platform.basket.events.v1"
-  replication_factor = 3
-  partitions         = 15
-
-  # infinte retention
-  config = {
-    "remote.storage.enable" = "true"
-    "retention.bytes"       = "-1" # keep on each partition unlimited data
-    # tflint-ignore: msk_topic_no_infinite_retention, # infinite retention because ...
-    "retention.ms" = "-1" # keep data forever
-    # keep data in primary storage for 1 hour
-    "local.retention.ms" = "3600000"
-    # allow for a batch of records maximum 1MiB
-    "max.message.bytes" = "1048576"
-    "compression.type"  = "zstd"
-    "cleanup.policy"    = "delete"
-  }
-}
-
-
-module "msk_data_keep_restore" {
-  source         = "../../../modules/tls-app"
-  consume_groups = ["pubsub.msk-data-keep-restore.normal", "pubsub.msk-data-keep-restore.large"]
-  consume_topics = [kafka_topic.plan_restore_normal.name, kafka_topic.plan_restore_large.name,
-  kafka_topic.restore_test_charges_events_v1.name, kafka_topic.restore_test_basket_v1.name]
-  produce_topics   = [kafka_topic.restore_test_charges_events_v1.name, kafka_topic.restore_test_basket_v1.name]
-  cert_common_name = "pubsub/msk-data-keep-restore"
-}
-
 # Enable only when we need to restore
+
+# resource "kafka_topic" "plan_restore_normal" {
+#   name               = "pubsub.plan-topic-restore.normal"
+#   replication_factor = 3
+#   partitions         = 40
+#   config = {
+#     "remote.storage.enable" = "true"
+#     "local.retention.ms"    = "86400000" # keep data in primary storage for 1 day
+#     # keep data for 3 days
+#     "retention.ms" = "259200000"
+#     # allow for a batch of records maximum 100MiB
+#     "max.message.bytes" = "104857600"
+#     "compression.type"  = "zstd"
+#     "cleanup.policy"    = "delete"
+#   }
+# }
 #
+# resource "kafka_topic" "plan_restore_large" {
+#   name               = "pubsub.plan-topic-restore.large"
+#   replication_factor = 3
+#   partitions         = 40
+#   config = {
+#     "remote.storage.enable" = "true"
+#     "local.retention.ms"    = "86400000" # keep data in primary storage for 1 day
+#     # keep data for 3 days
+#     "retention.ms" = "259200000"
+#     # allow for a batch of records maximum 100MiB
+#     "max.message.bytes" = "104857600"
+#     "compression.type"  = "zstd"
+#     "cleanup.policy"    = "delete"
+#   }
+# }
+
+# module "msk_data_keep_plan_restore" {
+#   source         = "../../../modules/tls-app"
+#   produce_topics = [kafka_topic.plan_restore_normal.name, kafka_topic.plan_restore_large.name]
+#   consume_topics = [kafka_topic.plan_restore_normal.name, kafka_topic.plan_restore_large.name]
+#
+#   cert_common_name = "pubsub/msk-data-keep-plan-restore"
+# }
+#
+# module "msk_data_keep_restore" {
+#   source         = "../../../modules/tls-app"
+#   consume_groups = ["pubsub.msk-data-keep-restore.normal", "pubsub.msk-data-keep-restore.large"]
+#   consume_topics = [kafka_topic.plan_restore_normal.name, kafka_topic.plan_restore_large.name]
+#
+#   cert_common_name = "pubsub/msk-data-keep-restore"
+# }
+
 # resource "kafka_acl" "msk_data_keep_restore_write_topic_all" {
 #   resource_name       = "*"
 #   resource_type       = "Topic"
