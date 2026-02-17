29e5c555230c2330305f724e6a6a0666fd912777
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2026-02-17 17:01:50 +0200

    msk data keep: prepare a small restore to validate manifests update

--- dev-aws/kafka-shared-msk/pubsub/msk-data-keep.tf
+++ dev-aws/kafka-shared-msk/pubsub/msk-data-keep.tf
@@ -24,53 +24,72 @@ module "msk_data_keep_backup" {
 
 # Enable only when we need to restore
 
-# resource "kafka_topic" "plan_restore_normal" {
-#   name               = "pubsub.plan-topic-restore.normal"
-#   replication_factor = 3
-#   partitions         = 15
-#   config = {
-#     "remote.storage.enable" = "true"
-#     "local.retention.ms"    = "86400000" # keep data in primary storage for 1 day
-#     # keep data for 3 days
-#     "retention.ms" = "259200000"
-#     # allow for a batch of records maximum 100MiB
-#     "max.message.bytes" = "104857600"
-#     "compression.type"  = "zstd"
-#     "cleanup.policy"    = "delete"
-#   }
-# }
-#
-# resource "kafka_topic" "plan_restore_large" {
-#   name               = "pubsub.plan-topic-restore.large"
-#   replication_factor = 3
-#   partitions         = 15
-#   config = {
-#     "remote.storage.enable" = "true"
-#     "local.retention.ms"    = "86400000" # keep data in primary storage for 1 day
-#     # keep data for 3 days
-#     "retention.ms" = "259200000"
-#     # allow for a batch of records maximum 100MiB
-#     "max.message.bytes" = "104857600"
-#     "compression.type"  = "zstd"
-#     "cleanup.policy"    = "delete"
-#   }
-# }
-#
-# module "msk_data_keep_plan_restore" {
-#   source         = "../../../modules/tls-app"
-#   produce_topics = [kafka_topic.plan_restore_normal.name, kafka_topic.plan_restore_large.name]
-#   consume_topics = [kafka_topic.plan_restore_normal.name, kafka_topic.plan_restore_large.name]
-#
-#   cert_common_name = "pubsub/msk-data-keep-plan-restore"
-# }
-#
-# module "msk_data_keep_restore" {
-#   source         = "../../../modules/tls-app"
-#   consume_groups = ["pubsub.msk-data-keep-restore.normal", "pubsub.msk-data-keep-restore.large"]
-#   consume_topics = [kafka_topic.plan_restore_normal.name, kafka_topic.plan_restore_large.name]
-#
-#   cert_common_name = "pubsub/msk-data-keep-restore"
-# }
+resource "kafka_topic" "plan_restore_normal" {
+  name               = "pubsub.plan-topic-restore.normal"
+  replication_factor = 3
+  partitions         = 15
+  config = {
+    "remote.storage.enable" = "true"
+    "local.retention.ms"    = "86400000" # keep data in primary storage for 1 day
+    # keep data for 3 days
+    "retention.ms" = "259200000"
+    # allow for a batch of records maximum 100MiB
+    "max.message.bytes" = "104857600"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+resource "kafka_topic" "plan_restore_large" {
+  name               = "pubsub.plan-topic-restore.large"
+  replication_factor = 3
+  partitions         = 15
+  config = {
+    "remote.storage.enable" = "true"
+    "local.retention.ms"    = "86400000" # keep data in primary storage for 1 day
+    # keep data for 3 days
+    "retention.ms" = "259200000"
+    # allow for a batch of records maximum 100MiB
+    "max.message.bytes" = "104857600"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+module "msk_data_keep_plan_restore" {
+  source         = "../../../modules/tls-app"
+  produce_topics = [kafka_topic.plan_restore_normal.name, kafka_topic.plan_restore_large.name]
+  consume_topics = [kafka_topic.plan_restore_normal.name, kafka_topic.plan_restore_large.name]
+
+  cert_common_name = "pubsub/msk-data-keep-plan-restore"
+}
+
+resource "kafka_topic" "restore_test_topic" {
+  name               = "pubsub.restore-test.energy-platform.gentrack.billing.events"
+  replication_factor = 3
+  partitions         = 15
+  config = {
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data for 1 month
+    "retention.ms" = "2628992000"
+    # keep data in primary storage for 2 days
+    "local.retention.ms" = "172800000"
+    # allow for a batch of records maximum 1MiB
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+module "msk_data_keep_restore" {
+  source           = "../../../modules/tls-app"
+  consume_groups   = ["pubsub.msk-data-keep-restore.normal", "pubsub.msk-data-keep-restore.large"]
+  consume_topics   = [kafka_topic.plan_restore_normal.name, kafka_topic.plan_restore_large.name, kafka_topic.restore_test_topic.name]
+  produce_topics   = [kafka_topic.restore_test_topic.name]
+  cert_common_name = "pubsub/msk-data-keep-restore"
+}
+
 
 #
 # resource "kafka_acl" "msk_data_keep_restore_write_topic_all" {
