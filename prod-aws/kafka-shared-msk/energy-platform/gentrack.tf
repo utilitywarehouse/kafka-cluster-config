bb8fab8eb048a94c5a92780d13f7378009118a66
Andreea <ameirosu@uw.co.uk>
2026-02-24 17:54:22 +0100

    reduce local storage to 2 days

--- prod-aws/kafka-shared-msk/energy-platform/gentrack.tf
+++ prod-aws/kafka-shared-msk/energy-platform/gentrack.tf
@@ -102,8 +102,8 @@ resource "kafka_topic" "gentrack_electronic_payment_events" {
     "remote.storage.enable" = "true"
     # keep data for 6 months
     "retention.ms" = "15552000000"
-    # keep data in primary storage for 28 days
-    "local.retention.ms" = "2419200000"
+    # keep data in primary storage for 2 days
+    "local.retention.ms" = "172800000"
     # allow for a batch of records maximum 1MiB
     "max.message.bytes" = "1048576"
     "compression.type"  = "zstd"
