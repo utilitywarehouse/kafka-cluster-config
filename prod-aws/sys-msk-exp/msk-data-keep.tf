782055a369f8769ba26a52cd0ae668486a7a2e61
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2026-03-27 16:36:49 +0200

    sys-msk-exp prod: done testing

--- prod-aws/sys-msk-exp/msk-data-keep.tf
+++ prod-aws/sys-msk-exp/msk-data-keep.tf
@@ -26,45 +26,45 @@ module "msk_data_keep_backup" {
 # Enable this part when we need to do a FULL cluster restore or we need to test the restore app on the EXISTING DEV cluster.
 # Contains the plan restore topics part.
 ################################################################
-resource "kafka_topic" "plan_restore_normal" {
-  name               = "pubsub.plan-topic-restore.normal"
-  replication_factor = 3
-  partitions         = 40
-  config = {
-    "remote.storage.enable" = "true"
-    "local.retention.ms"    = "86400000" # keep data in primary storage for 1 day
-    # keep data for 3 days
-    "retention.ms" = "259200000"
-    # allow for a batch of records maximum 100MiB
-    "max.message.bytes" = "104857600"
-    "compression.type"  = "zstd"
-    "cleanup.policy"    = "delete"
-  }
-}
-
-resource "kafka_topic" "plan_restore_large" {
-  name               = "pubsub.plan-topic-restore.large"
-  replication_factor = 3
-  partitions         = 40
-  config = {
-    "remote.storage.enable" = "true"
-    "local.retention.ms"    = "86400000" # keep data in primary storage for 1 day
-    # keep data for 3 days
-    "retention.ms" = "259200000"
-    # allow for a batch of records maximum 100MiB
-    "max.message.bytes" = "104857600"
-    "compression.type"  = "zstd"
-    "cleanup.policy"    = "delete"
-  }
-}
-
-module "msk_data_keep_plan_restore" {
-  source         = "../../modules/tls-app"
-  produce_topics = [kafka_topic.plan_restore_normal.name, kafka_topic.plan_restore_large.name]
-  consume_topics = [kafka_topic.plan_restore_normal.name, kafka_topic.plan_restore_large.name]
-
-  cert_common_name = "pubsub/msk-data-keep-plan-restore"
-}
+# resource "kafka_topic" "plan_restore_normal" {
+#   name               = "pubsub.plan-topic-restore.normal"
+#   replication_factor = 3
+#   partitions         = 40
+#   config = {
+#     "remote.storage.enable" = "true"
+#     "local.retention.ms"    = "86400000" # keep data in primary storage for 1 day
+#     # keep data for 3 days
+#     "retention.ms" = "259200000"
+#     # allow for a batch of records maximum 100MiB
+#     "max.message.bytes" = "104857600"
+#     "compression.type"  = "zstd"
+#     "cleanup.policy"    = "delete"
+#   }
+# }
+#
+# resource "kafka_topic" "plan_restore_large" {
+#   name               = "pubsub.plan-topic-restore.large"
+#   replication_factor = 3
+#   partitions         = 40
+#   config = {
+#     "remote.storage.enable" = "true"
+#     "local.retention.ms"    = "86400000" # keep data in primary storage for 1 day
+#     # keep data for 3 days
+#     "retention.ms" = "259200000"
+#     # allow for a batch of records maximum 100MiB
+#     "max.message.bytes" = "104857600"
+#     "compression.type"  = "zstd"
+#     "cleanup.policy"    = "delete"
+#   }
+# }
+#
+# module "msk_data_keep_plan_restore" {
+#   source         = "../../modules/tls-app"
+#   produce_topics = [kafka_topic.plan_restore_normal.name, kafka_topic.plan_restore_large.name]
+#   consume_topics = [kafka_topic.plan_restore_normal.name, kafka_topic.plan_restore_large.name]
+#
+#   cert_common_name = "pubsub/msk-data-keep-plan-restore"
+# }
 
 ################################################################
 # End of the common section for doing a FULL cluster restore or testing the restore app on the EXISTING DEV cluster.
@@ -76,43 +76,43 @@ module "msk_data_keep_plan_restore" {
 # It gives permissions for the restore app fully on all topics
 ################################################################
 
-module "msk_data_keep_restore" {
-  source         = "../../modules/tls-app"
-  consume_groups = ["pubsub.msk-data-keep-restore.normal", "pubsub.msk-data-keep-restore.large"]
-  consume_topics = [kafka_topic.plan_restore_normal.name, kafka_topic.plan_restore_large.name]
-
-  cert_common_name = "pubsub/msk-data-keep-restore"
-}
-
-
-resource "kafka_acl" "msk_data_keep_restore_write_topic_all" {
-  resource_name       = "*"
-  resource_type       = "Topic"
-  acl_principal       = "User:CN=pubsub/msk-data-keep-restore"
-  acl_host            = "*"
-  acl_operation       = "Write"
-  acl_permission_type = "Allow"
-}
-
-# Needed for determining the resume point
-resource "kafka_acl" "msk_data_keep_restore_read_topic_all" {
-  resource_name       = "*"
-  resource_type       = "Topic"
-  acl_principal       = "User:CN=pubsub/msk-data-keep-restore"
-  acl_host            = "*"
-  acl_operation       = "Read"
-  acl_permission_type = "Allow"
-}
-
-resource "kafka_acl" "msk_data_keep_restore_write_groups_all" {
-  resource_name = "*"
-  resource_type = "Group"
-  acl_principal = "User:CN=pubsub/msk-data-keep-restore"
-  acl_host      = "*"
-  # this is non intuitive... we need the Read permission to be able to commit offsets on groups
-  acl_operation       = "Read"
-  acl_permission_type = "Allow"
-}
+# module "msk_data_keep_restore" {
+#   source         = "../../modules/tls-app"
+#   consume_groups = ["pubsub.msk-data-keep-restore.normal", "pubsub.msk-data-keep-restore.large"]
+#   consume_topics = [kafka_topic.plan_restore_normal.name, kafka_topic.plan_restore_large.name]
+#
+#   cert_common_name = "pubsub/msk-data-keep-restore"
+# }
+#
+#
+# resource "kafka_acl" "msk_data_keep_restore_write_topic_all" {
+#   resource_name       = "*"
+#   resource_type       = "Topic"
+#   acl_principal       = "User:CN=pubsub/msk-data-keep-restore"
+#   acl_host            = "*"
+#   acl_operation       = "Write"
+#   acl_permission_type = "Allow"
+# }
+#
+# # Needed for determining the resume point
+# resource "kafka_acl" "msk_data_keep_restore_read_topic_all" {
+#   resource_name       = "*"
+#   resource_type       = "Topic"
+#   acl_principal       = "User:CN=pubsub/msk-data-keep-restore"
+#   acl_host            = "*"
+#   acl_operation       = "Read"
+#   acl_permission_type = "Allow"
+# }
+#
+# resource "kafka_acl" "msk_data_keep_restore_write_groups_all" {
+#   resource_name = "*"
+#   resource_type = "Group"
+#   acl_principal = "User:CN=pubsub/msk-data-keep-restore"
+#   acl_host      = "*"
+#   # this is non intuitive... we need the Read permission to be able to commit offsets on groups
+#   acl_operation       = "Read"
+#   acl_permission_type = "Allow"
+# }
 
 ################################################################
 # End of the section for a FULL cluster restore.
