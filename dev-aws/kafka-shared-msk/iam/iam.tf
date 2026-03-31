f57c6772eacc6d8b9914a07e6c3fcfa792e08b0e
Martin Asquino <martin.asquino@gmail.com>
2026-03-31 12:15:40 +0100

    Add cbc transaction monitoring modules for iam topics

--- dev-aws/kafka-shared-msk/iam/iam.tf
+++ dev-aws/kafka-shared-msk/iam/iam.tf
@@ -307,9 +307,20 @@ module "cbc_fraud_detection_consumer" {
   cert_common_name = "cbc/cbc-fraud-detection-consumer"
 }
 
-
 module "account_identity_login_service" {
   source           = "../../../modules/tls-app"
   cert_common_name = "auth-customer/login-service"
   produce_topics   = [kafka_topic.iam_credentials_v1.name]
+}
+
+module "cbc_transaction_monitoring_processor" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [kafka_topic.iam_credentials_v1_public.name]
+  cert_common_name = "cbc/cbc-transaction-monitoring-processor"
+}
+
+module "cbc_transaction_monitoring_projector" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [kafka_topic.iam_credentials_v1_public.name]
+  cert_common_name = "cbc/cbc-transaction-monitoring-projector"
 }
