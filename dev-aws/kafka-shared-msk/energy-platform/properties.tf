540c22203ffbebf53778853253c0d81355f090d5
Pieter Harvey <pharvey@uw.co.uk>
2026-04-08 10:48:40 +0100

    add topic to dev for property events

--- dev-aws/kafka-shared-msk/energy-platform/properties.tf
+++ dev-aws/kafka-shared-msk/energy-platform/properties.tf
@@ -18,6 +18,25 @@ resource "kafka_topic" "property_migration_events" {
   }
 }
 
+resource "kafka_topic" "property_events" {
+  name               = "energy-platform.property.events"
+  replication_factor = 3
+  partitions         = 15
+
+  config = {
+    # Use tiered storage
+    "remote.storage.enable" = "true"
+    # keep data for 6 months
+    "retention.ms" = "15552000000"
+    # keep data in primary storage for 2 days
+    "local.retention.ms" = "172800000"
+    # allow for a batch of records maximum 1MiB
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
 
 module "gentrack_migration_worker" {
   source           = "../../../modules/tls-app"
@@ -34,3 +53,4 @@ module "energy_billing_consumer" {
   consume_groups   = ["energy-billing.budget-plan-events-consumer"]
   cert_common_name = "energy-billing/budget-plan-events-consumer"
 }
+
