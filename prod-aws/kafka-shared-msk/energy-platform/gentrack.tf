25522749c2bc4a5a0a482a020eb888ee16ebd601
Mikhail Molotkov <mikhail.molotkov@uw.co.uk>
2026-04-23 16:54:13 +0100

    review

--- prod-aws/kafka-shared-msk/energy-platform/gentrack.tf
+++ prod-aws/kafka-shared-msk/energy-platform/gentrack.tf
@@ -178,3 +178,10 @@ module "energy_prepayment_projector" {
   consume_groups   = ["energy-platform.prepayment-projector"]
   cert_common_name = "energy-platform/prepayment-projector"
 }
+
+module "energy_service_gentrack_registration_consumer" {
+  source           = "../../../modules/tls-app"
+  consume_topics   = [kafka_topic.gentrack_market_interactions_events.name]
+  consume_groups   = ["energy-platform.services-gentrack-registration-consumer"]
+  cert_common_name = "energy-platform/services-gentrack-registration-consumer"
+}
