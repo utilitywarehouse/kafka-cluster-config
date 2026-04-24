1a6e704007f6971895f9679fc9d63552a492325e
oliverpauffley <mrpauffley@gmail.com>
2026-04-24 14:23:54 +0100

    create v2 bundle tier topic

--- prod-aws/kafka-shared-msk/customer-proposition/bundle-tier.tf
+++ prod-aws/kafka-shared-msk/customer-proposition/bundle-tier.tf
@@ -33,6 +33,29 @@ resource "kafka_topic" "bundletier_events_v1" {
   }
 }
 
+
+resource "kafka_topic" "bundletier_events_v2" {
+  name = "customer-proposition.public.bundletier.events.v2"
+
+  replication_factor = 3
+  partitions         = 15
+
+  # infinte retention
+  config = {
+    "remote.storage.enable" = "true"
+    "retention.bytes"       = "-1" # keep on each partition unlimited data
+    # tflint-ignore: msk_topic_no_infinite_retention, # infinite retention because ...
+    "retention.ms" = "-1" # keep data forever
+    # keep data in primary storage for 1 hour
+    "local.retention.ms" = "3600000"
+    # allow for a batch of records maximum 1MiB
+    "max.message.bytes" = "1048576"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+
 module "bundletier_event_forwarder" {
   source           = "../../../modules/tls-app"
   produce_topics   = [kafka_topic.bundletier_events_compacted_v1.name, kafka_topic.bundletier_events_v1.name]
