223e613e155872e066185cc2c4ac757e8961127b
Catalin Ilea <cilea@uw.co.uk>
2026-06-17 14:26:22 +0300

    retention 30 days

--- dev-aws/kafka-shared-msk/billing/billing.tf
+++ dev-aws/kafka-shared-msk/billing/billing.tf
@@ -82,8 +82,8 @@ resource "kafka_topic" "billing_bill_core_model" {
     "remote.storage.enable" = "true"
     # keep data in primary storage for 2 days
     "local.retention.ms" = "172800000"
-    # keep data for 7 days
-    "retention.ms" = "604800000"
+    # keep data for 1 month
+    "retention.ms" = "2592000000"
     # delete old data
     "cleanup.policy" = "delete"
   }
