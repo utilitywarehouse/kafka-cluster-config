421363a216d8a7a62f9c76b33b22eb797019b566
Mat Wright <12665151+web-slinger@users.noreply.github.com>
2026-06-24 10:21:09 +0100

    [dev/prod] msk acls for platform property events indexing

--- prod-aws/kafka-shared-msk/energy-platform/indexers.tf
+++ prod-aws/kafka-shared-msk/energy-platform/indexers.tf
@@ -17,7 +17,8 @@ module "gentrack_topic_indexer" {
     kafka_topic.gentrack_market_interactions_events.name,
     kafka_topic.gentrack_meterpoint_events.name,
     kafka_topic.property_migration_events.name,
-    kafka_topic.gentrack_agreement_events.name
+    kafka_topic.gentrack_agreement_events.name,
+    kafka_topic.property_events.name
   ]
   consume_groups   = ["energy-platform.gentrack-topic-indexer"]
   cert_common_name = "energy-platform/gentrack-topic-indexer"
