db0c6428b70d17c8d093c11396ff09addb99f34a
ggeorgiu <ggeorgiu@uw.co.uk>
2026-07-13 17:30:17 +0300

    Add billing transaction log v3 topic

---
+++ dev-aws/kafka-shared-msk/billing/transaction-log.tf
@@ -0,0 +1,19 @@
+resource "kafka_topic" "billing_transaction_log_v3" {
+  name               = "billing.transaction-log-v3"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    # use tiered storage
+    "remote.storage.enable" = "true"
+    # store data zstd compressed
+    "compression.type" = "zstd"
+    # keep data in primary storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data for 1 month
+    "retention.ms" = "2592000000"
+    # delete old data
+    "cleanup.policy" = "delete"
+    # allow for a batch of records maximum 100MiB
+    "max.message.bytes" = "104857600"
+  }
+}
---
+++ prod-aws/kafka-shared-msk/billing/transaction-log.tf
@@ -0,0 +1,20 @@
+resource "kafka_topic" "billing_transaction_log_v3" {
+  name               = "billing.transaction-log-v3"
+  replication_factor = 3
+  partitions         = 10
+  config = {
+    # use tiered storage
+    "remote.storage.enable" = "true"
+    # store data zstd compressed
+    "compression.type" = "zstd"
+    # keep data in primary storage for 2 days
+    "local.retention.ms" = "172800000"
+    # keep data forever 
+    # tflint-ignore: msk_topic_no_infinite_retention, # infinite retention because ...
+    "retention.ms" = "-1"
+    # making sure cleanup policy is not compaction 
+    "cleanup.policy" = "delete"
+    # allow for a batch of records maximum 100MiB
+    "max.message.bytes" = "104857600"
+  }
+}
