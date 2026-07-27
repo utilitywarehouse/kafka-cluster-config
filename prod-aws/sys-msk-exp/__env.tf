d84f0cc4a0beaeaca9040f32cda4252b0eaad556
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2026-07-27 17:12:57 +0300

    prod sys-msk-exp: split into ACL bootstrap and pubsub

--- prod-aws/sys-msk-exp/__env.tf
+++ prod-aws/sys-msk-exp/__env.tf
@@ -11,8 +11,8 @@ terraform {
 
 provider "kafka" {
   bootstrap_servers = [
-    "b-1.sysmskexp.402ohe.c5.kafka.eu-west-1.amazonaws.com:9094",
-    "b-2.sysmskexp.402ohe.c5.kafka.eu-west-1.amazonaws.com:9094",
-    "b-3.sysmskexp.402ohe.c5.kafka.eu-west-1.amazonaws.com:9094",
+    "b-1.sysmskexp.8fj3lx.c11.kafka.eu-west-1.amazonaws.com:9094",
+    "b-2.sysmskexp.8fj3lx.c11.kafka.eu-west-1.amazonaws.com:9094",
+    "b-3.sysmskexp.8fj3lx.c11.kafka.eu-west-1.amazonaws.com:9094",
   ]
 }
---
+++ prod-aws/sys-msk-exp/_kafka-acl-bootstrap/__backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "uw-prod-pubsub-tf-applier-state"
+    # tflint-ignore: msk_module_backend
+    key     = "prod-aws/sys-msk-exp-kafka-acl-bootstrap"
+    region  = "eu-west-1"
+    encrypt = true
+  }
+}
