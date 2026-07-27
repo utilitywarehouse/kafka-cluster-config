0ab2f1231f1c4cfb283249d9bef6b993ed368b43
sbuliarca <8848332+sbuliarca@users.noreply.github.com>
2026-07-27 17:17:42 +0300

    prod sys-msk-exp: split into ACL bootstrap and pubsub (#1377)

---
+++ prod-aws/sys-msk-exp/pubsub/msk-data-keep-restore-full-cluster.tf
@@ -0,0 +1,70 @@
+################################################################
+# Enable this configuration when we need to do a FULL cluster restore.
+# It gives permissions for the restore app fully on all topics
+################################################################
+
+#  Use a variable to enable or disable full cluster restore resources.
+#  This allows us to keep the configuration in place for when we need it, without having to rely on git history to bring it back.
+variable "enable_restore_full_cluster" {
+  type = bool
+
+  description = "Set to true to enable full cluster restore resources, false to disable them."
+  default     = true
+}
+resource "kafka_topic" "plan_restore_full" {
+  count              = var.enable_restore_full_cluster ? 1 : 0
+  name               = "pubsub.plan-topic-restore"
+  replication_factor = 3
+  partitions         = 30
+  config = {
+    "remote.storage.enable" = "true"
+    "local.retention.ms"    = "86400000" # keep data in primary storage for 1 day
+    # keep data for 3 days
+    "retention.ms" = "259200000"
+    # allow for a batch of records maximum 100MiB
+    "max.message.bytes" = "104857600"
+    "compression.type"  = "zstd"
+    "cleanup.policy"    = "delete"
+  }
+}
+
+module "msk_data_keep_plan_restore_full" {
+  count          = var.enable_restore_full_cluster ? 1 : 0
+  source         = "../../../modules/tls-app"
+  produce_topics = ["pubsub.plan-topic-restore"]
+  consume_topics = ["pubsub.plan-topic-restore"]
+
+  cert_common_name = "pubsub/msk-data-keep-plan-restore"
+}
+
+resource "kafka_acl" "msk_data_keep_restore_write_topic_all" {
+  count               = var.enable_restore_full_cluster ? 1 : 0
+  resource_name       = "*"
+  resource_type       = "Topic"
+  acl_principal       = "User:CN=pubsub/msk-data-keep-restore"
+  acl_host            = "*"
+  acl_operation       = "Write"
+  acl_permission_type = "Allow"
+}
+
+# Needed for determining the resume point
+resource "kafka_acl" "msk_data_keep_restore_read_topic_all" {
+  count               = var.enable_restore_full_cluster ? 1 : 0
+  resource_name       = "*"
+  resource_type       = "Topic"
+  acl_principal       = "User:CN=pubsub/msk-data-keep-restore"
+  acl_host            = "*"
+  acl_operation       = "Read"
+  acl_permission_type = "Allow"
+}
+
+resource "kafka_acl" "msk_data_keep_restore_write_groups_all" {
+  count         = var.enable_restore_full_cluster ? 1 : 0
+  resource_name = "*"
+  resource_type = "Group"
+  acl_principal = "User:CN=pubsub/msk-data-keep-restore"
+  acl_host      = "*"
+  # this is non intuitive... we need the Read permission to be able to commit offsets on groups
+  acl_operation       = "Read"
+  acl_permission_type = "Allow"
+}
