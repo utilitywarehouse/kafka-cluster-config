8c9596454b16df76226f6ca1fe93c154669d6467
martskins <martskins@users.noreply.github.com>
2026-09-24 13:56:53 +0100

    cbc: add legacy account events to allowed topics of customer projector (#1433)

--- dev-aws/kafka-shared-msk/cbc/cbc.tf
+++ dev-aws/kafka-shared-msk/cbc/cbc.tf
@@ -1205,7 +1205,8 @@ module "cbc_customer_projector" {
     kafka_topic.order_events_v1.name,
     kafka_topic.verification_events_v1.name,
     kafka_topic.lifecycle_events_v2.name,
-    kafka_topic.customer_events_v1.name
+    kafka_topic.customer_events_v1.name,
+    kafka_topic.legacy_account_events_v2.name
   ]
   consume_groups = [
     "cbc.cbc-customer-api-v1",
